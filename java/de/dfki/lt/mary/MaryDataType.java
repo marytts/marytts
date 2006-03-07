47866cde1959bb5fef40c8712f598d43ce3dca89
Marc Schroeder <marc.schroeder@dfki.de>
2006-03-07 09:22:36 +0000

    another attempt to fix ticket:31 -- removed intelligent logic to get a language-specific version of the example text for data types with null locale.
    git-svn-id: https://mary.opendfki.de/repos/trunk@85 953a6561-930b-0410-b2a6-db37d1b2ae63

--- java/de/dfki/lt/mary/MaryDataType.java
+++ java/de/dfki/lt/mary/MaryDataType.java
@@ -90,37 +90,15 @@ public class MaryDataType
     }
     
     /**
-     * Provide a sensible example text for this data type.
-     * For most data types, this is simply a string provided when the data type
-     * is defined. However, for some language-independent datatypes, no
-     * example text is defined. In these cases, the following logic is applied
-     * to obtain a working example text:
-     * 1. From the list of installed voices, a locale is obtained;
-     * 2. a language-specific version of the current data type is obtained;
-     * 3. the example text of that language-specific version is returned.
+     * Provide the example text for this data type.
+     * This is simply a string provided when the data type
+     * is defined. For some language-independent datatypes, no
+     * example text is defined. In these cases, null is returned
      * @return an example text string, or null if none could be obtained.
      */
     public String exampleText()
     {
-        if (exampleText != null)
-            return exampleText;
-        // No example text in current data type
-        if (getLocale() == null) {
-            Collection voices = Voice.getAvailableVoices();
-            for (Iterator it = voices.iterator(); it.hasNext(); ) {
-                Voice v = (Voice) it.next();
-                Locale locale = v.getLocale();
-                MaryDataType specific = getLanguageSpecificVersion(this, locale);
-                if (specific != null) {
-                    assert specific.getLocale() != null;
-                    return specific.exampleText();
-                } // else, try next voice's locale
-            }
-            // no specific data type for any of the voices' locales
-            return null;
-        } else { // locale, but no example text
-            return null;
-        }
+        return exampleText;
     }
 
     public String toString() { return name; }
