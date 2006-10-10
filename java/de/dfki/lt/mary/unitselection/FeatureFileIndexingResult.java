110538b9c93a843b5e1b22bfcdea5a966b22d34b
Sacha Krstulovic <sakr01@dfki.de>
2006-10-10 15:50:47 +0000

    Separated the QueryResult object from the FeatureFileIndexer and renamed it to FeatureFileIndexingResult so that it can be publicly used to retrieve units.
    git-svn-id: https://mary.opendfki.de/repos/trunk@327 953a6561-930b-0410-b2a6-db37d1b2ae63

---
+++ java/de/dfki/lt/mary/unitselection/FeatureFileIndexingResult.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright 2006 DFKI GmbH.
+ * All Rights Reserved.  Use is subject to license terms.
+ * 
+ * Permission is hereby granted, free of charge, to use and distribute
+ * this software and its documentation without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of this work, and to
+ * permit persons to whom this work is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * 1. The code must retain the above copyright notice, this list of
+ *    conditions and the following disclaimer.
+ * 2. Any modifications must be clearly marked as such.
+ * 3. Original authors' names are not deleted.
+ * 4. The authors' names are not used to endorse or promote products
+ *    derived from this software without specific prior written
+ *    permission.
+ *
+ * DFKI GMBH AND THE CONTRIBUTORS TO THIS WORK DISCLAIM ALL WARRANTIES WITH
+ * REGARD TO THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS, IN NO EVENT SHALL DFKI GMBH NOR THE
+ * CONTRIBUTORS BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL
+ * DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+ * PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS
+ * ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
+ * THIS SOFTWARE.
+ */
+package de.dfki.lt.mary.unitselection;
+
+import de.dfki.lt.mary.unitselection.featureprocessors.FeatureVector;
+
+/**
+ * A helper class to return the query results from the FeatureFileIndexer.
+ * 
+ * @author sacha
+ *
+ */
+public class FeatureFileIndexingResult {
+    public FeatureVector[] v = null;
+    public int level = -1;
+    
+    public FeatureFileIndexingResult( FeatureVector[] setV, int setLevel ) {
+        this.v = setV;
+        this.level = setLevel;
+    }
+    
+    public int[] getUnitIndexes() {
+        int[] ret = new int[v.length];
+        for ( int i = 0; i < v.length; i++ ) {
+            ret[i] = v[i].getUnitIndex();
+        }
+        return( ret );
+    }
+}
