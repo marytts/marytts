c9315a2ff566ce5996d29a1bb088b03667665bd0
Sacha Krstulovic <sakr01@dfki.de>
2006-10-19 19:02:26 +0000

    Added the computation of the distance tables to the CARTBuilder. The acoustic distances between two units are defined as a Mahalanobis distance between Mel-cepstra, cumulated and averaged along the optimal DTW path which aligns the Mel-cepstral frames of the two considered units.   This is not exactly what Alan Black was doing. The pros of our version are that: - it closer to what is done in speech recognition; - it has no "rule of thumb" weights to adjust; - it is reasonably oblivious to unit duration differences (I don't   believe in Black's "duration penalty" for acoustic clustering, maybe   we should discuss this). The cons is that: - it is computationally more expensive, but since it is performed   offline we can probably afford it; - it does not force an explicit F0 penalty, but: a) I believe that   the Mel-cepstra contain some pitch information, though this is arguable   in the case where they come from pitch-synchronous analysis; b) do we really   want to account for pitch info in the preselection, since we plan to do   pitch-correction at a further stage; c) if it appears from experience that   we need a better pitch discrimination in the preselection, we could still   work out and add our own F0 penalty in a future version.
    This commit is untested, because it depends on other parts of the CARTBuilder
    which are not yet complete (file names for the distance matrix export etc.).
    
    
    git-svn-id: https://mary.opendfki.de/repos/trunk@349 953a6561-930b-0410-b2a6-db37d1b2ae63

--- java/de/dfki/lt/mary/unitselection/voiceimport_reorganized/CARTBuilder.java
+++ java/de/dfki/lt/mary/unitselection/voiceimport_reorganized/CARTBuilder.java
@@ -45,6 +45,9 @@ import de.dfki.lt.mary.unitselection.MaryNode;
 import de.dfki.lt.mary.unitselection.cart.CARTWagonFormat;
 import de.dfki.lt.mary.unitselection.featureprocessors.FeatureDefinition;
 import de.dfki.lt.mary.unitselection.featureprocessors.FeatureVector;
+import de.dfki.lt.mary.unitselection.MCepTimelineReader;
+import de.dfki.lt.mary.unitselection.UnitFileReader;
+import de.dfki.lt.mary.unitselection.MCepDatagram;
 
 public class CARTBuilder implements VoiceImportComponent {
     
@@ -77,27 +80,30 @@ public class CARTBuilder implements VoiceImportComponent {
          //read in the feature sequence
          //open the file
          System.out.println("Reading feature sequence ...");
-         String featSeqFile = databaseLayout.featSequenceFileName();
-         BufferedReader buf = new BufferedReader(
-                 new FileReader(new File(featSeqFile)));
-         //each line contains one feature
-         String line = buf.readLine();
-         //collect features in a list
-         List features = new ArrayList();
-         while (line != null){
-             // Skip empty lines and lines starting with #:
-             if (!(line.trim().equals("") || line.startsWith("#"))){
-                 features.add(line.trim());
-             }
-             line = buf.readLine();
-         }
-         //convert list to int array
-         int[] featureSequence = new int[features.size()];
-         for (int i=0;i<features.size();i++){
-             featureSequence[i] = 
-                 featureDefinition.getFeatureIndex((String)features.get(i));
-         }
-         System.out.println(" ... done!"); 
+//         String featSeqFile = databaseLayout.featSequenceFileName();
+//         BufferedReader buf = new BufferedReader(
+//                 new FileReader(new File(featSeqFile)));
+//         //each line contains one feature
+//         String line = buf.readLine();
+//         //collect features in a list
+//         List features = new ArrayList();
+//         while (line != null){
+//             // Skip empty lines and lines starting with #:
+//             if (!(line.trim().equals("") || line.startsWith("#"))){
+//                 features.add(line.trim());
+//             }
+//             line = buf.readLine();
+//         }
+//         //convert list to int array
+//         int[] featureSequence = new int[features.size()];
+//         for (int i=0;i<features.size();i++){
+//             featureSequence[i] = 
+//                 featureDefinition.getFeatureIndex((String)features.get(i));
+//         }
+//         System.out.println(" ... done!"); 
+//       
+         // TODO: THIS IS A HACK:
+         String[] featureSequence = { "mary_phoneme", "mary_stressed" };
          
          //sort the features according to feature sequence
          System.out.println("Sorting features ...");
@@ -255,9 +261,9 @@ public class CARTBuilder implements VoiceImportComponent {
             					FeatureDefinition featDef,
             					String filename) throws FileNotFoundException{
         //open file 
-        PrintWriter out = new PrintWriter(new 
-    			FileOutputStream(new 
-    			        File(filename)));
+        PrintWriter out = new PrintWriter(new
+                BufferedOutputStream(new 
+                        FileOutputStream(filename)));
         //get basic feature info
         int numByteFeats = featDef.getNumberOfByteFeatures();
         int numShortFeats = featDef.getNumberOfShortFeatures();
@@ -299,9 +305,190 @@ public class CARTBuilder implements VoiceImportComponent {
      * @param featureVectors the feature vectors of the units
      * @param filename the filename
      */
-    public void buildAndDumpDistanceTables(FeatureVector[] featureVectors,
-							String filename){
+    public void buildAndDumpDistanceTables (FeatureVector[] featureVectors, String filename) throws FileNotFoundException {
+        
+        System.out.println( "Computing the distance matrix for file[" + filename + "]...");
+        
+        /* Dereference the number of units once and for all */
+        int numUnits = featureVectors.length;
+        /* Load the MelCep timeline and the unit file */
+        MCepTimelineReader tlr = null;
+        try {
+            tlr = new MCepTimelineReader( databaseLayout.melcepTimelineFileName() );
+        }
+        catch ( IOException e ) {
+            throw new RuntimeException( "Failed to read the Mel-Cepstrum timeline [" + databaseLayout.melcepTimelineFileName()
+                    + "] due to the following IOException: ", e );
+        }
+        UnitFileReader ufr = null;
+        try {
+            ufr = new UnitFileReader( databaseLayout.unitFileName() );
+        }
+        catch ( IOException e ) {
+            throw new RuntimeException( "Failed to read the unit file [" + databaseLayout.unitFileName()
+                    + "] due to the following IOException: ", e );
+        }
+        /* Read the Mel Cepstra for each unit, and cumulate
+         * their sufficient statistics in the same loop */
+        double[][][] melCep = new double[numUnits][][];
+        double val = 0;
+        double[] sum = new double[tlr.getOrder()];
+        double[] sumSq = new double[tlr.getOrder()];
+        double[] sigma2 = new double[tlr.getOrder()];
+        double N = 0.0;
+        for ( int i = 0; i < numUnits; i++ ) {
+            /* Read the datagrams for the current unit */
+            MCepDatagram[] dat = null;
+            try {
+                dat = (MCepDatagram[]) tlr.getDatagrams( ufr.getUnit(featureVectors[i].getUnitIndex()), ufr.getSampleRate() );
+            }
+            catch ( IOException e ) {
+                throw new RuntimeException( "Failed to read the datagrams for unit number [" + featureVectors[i].getUnitIndex()
+                        + "] from the Mel-cepstrum timeline due to the following IOException: ", e );
+            }
+            N += (double)(dat.length); // Update the frame counter
+            for ( int j = 0; j < dat.length; j++ ) {
+                melCep[i][j] = dat[j].getCoeffsAsDouble();
+                /* Cumulate the sufficient statistics */
+                for ( int k = 0; k < tlr.getOrder(); k++ ) {
+                    val = melCep[i][j][k];
+                    sum[k] += val;
+                    sumSq[k] += (val*val);
+                }
+            }
+        }
+        /* Finalize the variance calculation */
+        for ( int k = 0; k < tlr.getOrder(); k++ ) {
+            val = sum[k];
+            sigma2[k] = ( sumSq[k] - (val*val)/N ) / N;
+        }
+        /* Compute the unit distance matrix */
+        double[][] dist = new double[numUnits][featureVectors.length];
+        for ( int i = 0; i < numUnits; i++ ) {
+            dist[i][i] = 0.0; // <= Set the diagonal to 0.0
+            for ( int j = 1; j < numUnits; j++ ) {
+                /* Get the DTW distance between the two sequences: */
+                dist[i][j] = dist[j][i] = dtwDist( melCep[i], melCep[j], sigma2 );
+            } 
+        }
+        /* Write the matrix to disk */
+        System.out.print( "Writing the distance matrix to file[" + filename + "]...");
+        PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(filename)));
+        for ( int i = 0; i < numUnits; i++ ) {
+            for ( int j = 0; j < numUnits; j++ ) {
+                out.print( (float)(dist[i][j]) + " " );
+            }
+            out.print("\n");
+        }
+        out.flush();
+        out.close();
+        
+        System.out.println( "Done.");
     }
     
+    
+    /**
+     * Computes an average Mahalanobis distance along the optimal DTW path
+     * between two vector sequences.
+     * 
+     * The DTW constraint used here is:
+     * D(i,j) = min {
+     * D(i-2,j-1) + 2*d(i-1,j) + d(i,j) ;
+     * D(i-1,j-1) + 2*d(i,j) ;
+     * D(i-1,j-2) + 2*d(i,j-1) + d(i,j)
+     * }
+     * 
+     * At the end of the DTW, the cumulated distance is normalized by the number
+     * of local distances cumulated along the optimal path. Hence, the resulting
+     * unit distance is homogeneous to an average having the order of magnitude
+     * of a single Mahalanobis distance, and that for each pair of units.
+     * 
+     * @param seq1 The first sequence of (Mel-cepstrum) vectors.
+     * @param seq2 The second sequence of (Mel-cepstrum) vectors.
+     * @param sigma2 The variance of the vectors.
+     * @return The average Mahalanobis distance along the optimal DTW path.
+     */
+    double dtwDist( double[][] seq1, double[][] seq2, double[] sigma2 ) {
+        double[][] d = new double[seq1.length][seq2.length];
+        double[][] D = new double[seq1.length][seq2.length];
+        int[][] Nd = new int[seq1.length][seq2.length]; // <= Number of cumulated distances, for the final averaging
+        double[] minV = new double[3];
+        int[] minNd = new int[3];
+        int minIdx = 0;
+        /* Fill the local distance matrix */
+        for ( int i = 0; i < seq1.length; i++ ) {
+            for ( int j = 0; j < seq2.length; j++ ) {
+                d[i][j] = mahalanobis( seq1[i],   seq2[j],   sigma2 );
+            }
+        }
+        /* Compute the optimal DTW distance: */
+        /* - 1st row/column: */
+        D[0][0] = 2*d[0][0];
+        for ( int i = 1; i < seq1.length; i++ ) {
+            D[i][0] = d[i][0];
+            D[0][i] = d[0][i];
+            Nd[0][i] = Nd[i][0] = 1;
+        }
+        /* - 2nd row/column: */
+        /* corner: i==1, j==1 */
+        minV[0] = 2*d[0][1] + d[1][1];  minNd[0] = 3;
+        minV[1] = D[0][0] + 2*d[1][1];  minNd[1] = Nd[0][0] + 2;
+        minV[2] = 2*d[1][0] + d[1][1];  minNd[2] = 3;
+        minIdx = minV[0] < minV[1] ? 0 : 1;
+        minIdx = minV[2] < minV[minIdx] ? 2 : minIdx;
+        D[1][1] = minV[minIdx];
+        Nd[1][1] = minNd[minIdx];
+        /* 2nd row: j==1 ; 2nd col: i==1 */
+        for ( int i = 2; i < seq1.length; i++ ) {
+            /* Row: */
+            minV[0] = D[i-2][0] + 2*d[i-1][1] + d[i][1];  minNd[0] = Nd[i-2][0] + 3;
+            minV[1] = D[i-1][0] + 2*d[i][1];              minNd[1] = Nd[i-1][0] + 2;
+            minV[2] = 2*d[i][0] + d[i][1];                minNd[2] = 3;
+            minIdx = minV[0] < minV[1] ? 0 : 1;
+            minIdx = minV[2] < minV[minIdx] ? 2 : minIdx;
+            D[i][1] = minV[minIdx];
+            Nd[i][1] = minNd[minIdx];
+            /* Column: */
+            minV[0] = 2*d[0][i] + d[1][i];                minNd[0] = 3;
+            minV[1] = D[0][i-1] + 2*d[1][i];              minNd[1] = Nd[0][i-1] + 2;
+            minV[2] = D[0][i-2] + 2*d[1][i-1] + d[1][i];  minNd[2] = Nd[0][i-2] + 3;
+            minIdx = minV[0] < minV[1] ? 0 : 1;
+            minIdx = minV[2] < minV[minIdx] ? 2 : minIdx;
+            D[1][i] = minV[minIdx];
+            Nd[1][i] = minNd[minIdx];
+        }
+        /* - Rest of the matrix: */
+        for ( int i = 2; i < seq1.length; i++ ) {
+            for ( int j = 2; j < seq2.length; j++ ) {
+                minV[0] = D[i-2][j-1] + 2*d[i-1][j] + d[i][j];  minNd[0] = Nd[i-2][j-1] + 3;
+                minV[1] = D[i-1][j-1] + 2*d[i][j];              minNd[1] = Nd[i-1][j-1] + 2;
+                minV[2] = D[i-1][j-2] + 2*d[i][j-1] + d[i][j];  minNd[0] = Nd[i-1][j-2] + 3;
+                minIdx = minV[0] < minV[1] ? 0 : 1;
+                minIdx = minV[2] < minV[minIdx] ? 2 : minIdx;
+                D[i][j] = minV[minIdx];
+                Nd[i][j] = minNd[minIdx];
+            }
+        }
+        /* Return */
+        return( D[seq1.length-1][seq2.length-1] / (double)(Nd[seq1.length-1][seq2.length-1]) );
+    }
+    
+    /**
+     * Mahalanobis distance between two feature vectors.
+     * 
+     * @param v1 A feature vector.
+     * @param v2 Another feature vector.
+     * @param sigma2 The variance of the distribution of the considered feature vectors.
+     * @return The mahalanobis distance between v1 and v2.
+     */
+    double mahalanobis( double[] v1, double[] v2, double[] sigma2 ) {
+        double sum = 0.0;
+        double diff = 0.0;
+        for ( int i = 0; i < v1.length; i++ ) {
+            diff = v1[i] - v2[i];
+            sum += ( (diff*diff) / sigma2[i] );
+        }
+        return( sum );
+    }
     
 }
