e0b0871cb7d410c10ab8841f98c5743201dd7775
Anna Hunecke <anhu01@dfki.de>
2006-11-20 09:37:26 +0000

    Modified LabelFeatureAligner to automatically remove unneccessary pauses in features; modified LabelComputers to print the unit index instead of the dummy number
    git-svn-id: https://mary.opendfki.de/repos/trunk@422 953a6561-930b-0410-b2a6-db37d1b2ae63

--- java/de/dfki/lt/mary/unitselection/voiceimport/HalfPhoneUnitLabelComputer.java
+++ java/de/dfki/lt/mary/unitselection/voiceimport/HalfPhoneUnitLabelComputer.java
@@ -1,13 +1,6 @@
 package de.dfki.lt.mary.unitselection.voiceimport;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.util.StringTokenizer;
 
 /**
@@ -30,7 +23,9 @@ public class HalfPhoneUnitLabelComputer extends UnitLabelComputer
         // We will create exactly two half phones for every phone:
         String[] halfPhoneLabels = new String[2*phoneLabels.length];
         float startTime = 0;
+        int unitIndex = 0;
         for (int i=0; i<phoneLabels.length; i++) {
+            unitIndex++;
             StringTokenizer st = new StringTokenizer(phoneLabels[i]);
             String endTimeString = st.nextToken();
             String dummyNumber = st.nextToken();
@@ -40,8 +35,9 @@ public class HalfPhoneUnitLabelComputer extends UnitLabelComputer
             float duration = endTime - startTime;
             assert duration > 0 : "Duration is not > 0 for phone "+i+" ("+phone+")";
             float midTime = startTime + duration/2;
-            String leftUnitLine = midTime + " " + dummyNumber + " " + phone + "_L";
-            String rightUnitLine = endTime + " " + dummyNumber + " " + phone + "_R";
+            String leftUnitLine = midTime + " " + unitIndex + " " + phone + "_L";
+            unitIndex++;
+            String rightUnitLine = endTime + " " + unitIndex + " " + phone + "_R";
             halfPhoneLabels[2*i] = leftUnitLine;
             halfPhoneLabels[2*i+1] = rightUnitLine;
             startTime = endTime;
