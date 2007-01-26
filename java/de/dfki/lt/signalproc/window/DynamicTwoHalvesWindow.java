02510d55819c6aaa0cad047095fb827ba87c5eaf
Marc Schroeder <marc.schroeder@dfki.de>
2007-01-26 10:11:13 +0000

    visualise frequency response of an asymmetric two-halves window
    git-svn-id: https://mary.opendfki.de/repos/trunk@462 953a6561-930b-0410-b2a6-db37d1b2ae63

--- java/de/dfki/lt/signalproc/window/DynamicTwoHalvesWindow.java
+++ java/de/dfki/lt/signalproc/window/DynamicTwoHalvesWindow.java
@@ -1,5 +1,9 @@
 package de.dfki.lt.signalproc.window;
 
+import de.dfki.lt.signalproc.display.FunctionGraph;
+import de.dfki.lt.signalproc.display.LogSpectrum;
+import de.dfki.lt.signalproc.util.MathUtils;
+
 public class DynamicTwoHalvesWindow extends DynamicWindow {
 
     public DynamicTwoHalvesWindow(int windowType) {
@@ -26,4 +30,36 @@ public class DynamicTwoHalvesWindow extends DynamicWindow {
         w.apply(data, off, data, off, len, len);
     }
 
+    public static void main(String[] args)
+    {
+        int samplingRate = Integer.getInteger("samplingrate", 1).intValue();
+        int windowLengthMs = Integer.getInteger("windowlength.ms", 0).intValue();
+        int windowLength = Integer.getInteger("windowlength.samples", 512).intValue();
+        float asymmetry = 1.5f; // length of right window half relative to left half
+        // If both are given, use window length in milliseconds:
+        if(windowLengthMs != 0) windowLength = windowLengthMs * samplingRate / 1000;
+        int leftHalfLength = windowLength/2;
+        int rightHalfLength = (int) (leftHalfLength*asymmetry);
+        windowLength = leftHalfLength + rightHalfLength;
+        int fftSize = Math.max(4096, MathUtils.closestPowerOfTwoAbove(windowLength));
+        int windowType = Window.HANN;
+        Window wLeft = Window.get(windowType, 2*leftHalfLength);
+        Window wRight = Window.get(windowType, 2*rightHalfLength);
+        double window[] = new double[windowLength];
+        System.arraycopy(wLeft.window, 0, window, 0, leftHalfLength);
+        System.arraycopy(wRight.window, rightHalfLength, window, leftHalfLength, rightHalfLength);
+        
+        FunctionGraph timeGraph = new FunctionGraph(0, 1./samplingRate, window);
+        timeGraph.showInJFrame("Asymmetric "+wLeft.toString() + " in time domain", true, false);
+        double[] fftSignal = new double[fftSize];
+        // fftSignal should integrate to one, so normalise amplitudes:
+        double sum = MathUtils.sum(window);
+        for (int i=0; i<window.length; i++) {
+            fftSignal[i] = window[i] / sum;
+        }
+        LogSpectrum freqGraph = new LogSpectrum(fftSignal, samplingRate);
+        freqGraph.showInJFrame("Asymmetric "+wLeft.toString() + " log frequency response", true, false);
+    }
+
+    
 }
