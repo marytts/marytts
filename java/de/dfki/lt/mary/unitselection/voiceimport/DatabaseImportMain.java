51a702dda1f183d80f9d666fc7c2a3fe29dd6860
Marc Schroeder <marc.schroeder@dfki.de>
2007-02-08 18:54:04 +0000

    Added a tool for interactively exploring the phone labels of database recordings, and to extract sections of wav+lab files.
    git-svn-id: https://mary.opendfki.de/repos/trunk@472 953a6561-930b-0410-b2a6-db37d1b2ae63

--- java/de/dfki/lt/mary/unitselection/voiceimport/DatabaseImportMain.java
+++ java/de/dfki/lt/mary/unitselection/voiceimport/DatabaseImportMain.java
@@ -313,6 +313,7 @@ public class DatabaseImportMain extends JFrame
                 new MRPALabelConverter ( db ),
                 
                 new FestvoxTextfileConverter( db, bnl ),
+                new LabelledFilesInspector(db, bnl),
                 new UnitLabelComputer( db, bnl ),
                 new HalfPhoneUnitLabelComputer( db, bnl ),
                 new UnitFeatureComputer( db, bnl ),                
