423307a71c58f100811b1ee6b1c7b9c71b4f1cb7
Sacha Krstulovic <sakr01@dfki.de>
2007-02-09 17:18:41 +0000

    Minor modifs.
    git-svn-id: https://mary.opendfki.de/repos/trunk@473 953a6561-930b-0410-b2a6-db37d1b2ae63

--- java/de/dfki/lt/mary/gizmos/TxtToUnitfeats_HTS_BITS.java
+++ java/de/dfki/lt/mary/gizmos/TxtToUnitfeats_HTS_BITS.java
@@ -18,29 +18,38 @@ public class TxtToUnitfeats_HTS_BITS {
     public static void main(String[] args) throws IOException {
         BasenameList basenames = new BasenameList( "gen_text", ".txt" );
         String text;
+        String inText;
         if (System.getProperty("server.host") == null) {
             System.setProperty("server.host", "localhost");
         }
         if (System.getProperty("server.port") == null) {
             System.setProperty("server.port", "59125");
         }
+        //System.setProperty("server.host", "cling");
+        //System.setProperty("server.port", "59125");
         MaryClient maryClient = new MaryClient();
         for (int i=0; i<basenames.getLength(); i++) {
+            //System.out.println( "IN: " + basenames.getName(i) );
+            inText = FileUtils.getFileAsString(new File( "gen_text/" + basenames.getName(i) + ".txt" ), "UTF-8");
+            System.out.println( "IMPORTING: " + inText );
             text = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n" +
             "<maryxml version=\"0.4\"\n" +
             "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n" +
             "xmlns=\"http://mary.dfki.de/2002/MaryXML\"\n" +
-            "xml:lang=\"" + "en" + "\">\n" +
+            "xml:lang=\"" + "de" + "\">\n" +
             "<boundary duration=\"100\"/>\n"
-            + FileUtils.getFileAsString(new File( "gen_text/" + basenames.getName(i) + ".txt" ), "UTF-8")
+            + inText
             + "</maryxml>";
             String inputFormat = "RAWMARYXML";
+            //String inputFormat = "TEXT_DE";
+            //text = inText;
             String outputFormat = "TARGETFEATURES";
             OutputStream os = new BufferedOutputStream(new FileOutputStream(new File( "unitfeats/gen/" + basenames.getName(i) + ".feats" )));
             maryClient.process(text, inputFormat, outputFormat, null, null, os);
             os.flush();
             os.close();
             System.out.println( "    " + basenames.getName(i) );
+            System.out.flush();
         }
         System.out.println("Finished computing the unit features.");
     }
