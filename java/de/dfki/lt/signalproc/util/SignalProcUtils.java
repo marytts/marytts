b27d8df382108ddbf879674a9a587bce6c206e68
Oytun Turk <oytun_turk@dfki.de>
2007-06-28 07:49:51 +0000

    General purpose signal processing functions
    git-svn-id: https://mary.opendfki.de/repos/trunk@622 953a6561-930b-0410-b2a6-db37d1b2ae63

---
+++ java/de/dfki/lt/signalproc/util/SignalProcUtils.java
@@ -0,0 +1,37 @@
+package de.dfki.lt.signalproc.util;
+
+public class SignalProcUtils {
+    
+    public static int getLPOrder(int fs){
+        int P = (int)(fs/1000.0f+2);
+        
+        if (P%2==1)
+            P+=1;
+        
+        return P;
+    }
+    
+    public static int getDFTSize(int fs){
+        int dftSize;
+        
+        if (fs<8000)
+            dftSize = 128;
+        else if (fs<16000)
+            dftSize = 256;
+        else if (fs<22050)
+            dftSize = 512;
+        else if (fs<32000)
+            dftSize = 1024;
+        else if (fs<44100)
+            dftSize = 2048;
+        else
+            dftSize = 4096;
+        
+        return dftSize;
+    }
+    
+    public static int halfSpectrumSize(int fftSize)
+    {
+        return (int)(Math.floor(fftSize/2.0+1.5));
+    }
+}
