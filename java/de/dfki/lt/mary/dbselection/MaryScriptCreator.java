1f952661448abdce2f92f459a17cff1fe0764330
Marc Schroeder <marc.schroeder@dfki.de>
2007-10-12 14:01:00 +0000

    adding little tool to convert single-file recording scripts into collection of .txt files in text/ folder, for recording tool
    git-svn-id: https://mary.opendfki.de/repos/trunk@830 953a6561-930b-0410-b2a6-db37d1b2ae63

---
+++ java/de/dfki/lt/mary/dbselection/MaryScriptCreator.java
@@ -0,0 +1,46 @@
+package de.dfki.lt.mary.dbselection;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.text.DecimalFormat;
+
+public class MaryScriptCreator {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) throws Exception 
+    {
+        // input: one line ( dummy "text" )
+        // output: file named voice0001.txt containing text
+        String prefix = System.getProperty("prefix", "prompt");
+        File outDir = new File(System.getProperty("outdir", "./text"));
+        String inputEncoding = System.getProperty("encoding", "UTF-8");
+        if (!outDir.exists()) outDir.mkdir();
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in, inputEncoding));
+        String line;
+        DecimalFormat f = new DecimalFormat("0000");
+        int i=0;
+        while ((line = in.readLine()) != null) {
+            i++;
+            // all we do is search for the first and last quotation marks; the rest is ignored
+            //int first = line.indexOf('"');
+            //int last = line.lastIndexOf('"');
+            int first = line.indexOf(" ");
+            int last = line.length();
+            if (first == -1 || last == -1 || last <= first) {
+                System.err.println("Line no. "+i+" has no space -- skipping: "+line);
+            }
+            File outFile = new File(outDir, prefix+f.format(i)+".txt");
+            PrintWriter out = new PrintWriter(new OutputStreamWriter(new FileOutputStream(outFile), "UTF-8"));
+            out.println(line.substring(first+1, last));
+            out.close();
+        }
+        
+    }
+
+}
