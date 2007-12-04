b77d03e7ef171dae1590a25d00c4ecede4beb6a3
Marc Schroeder <marc.schroeder@dfki.de>
2007-12-04 09:53:22 +0000

    Added DFKI Copyright notice to all Redstart source files.
    git-svn-id: https://mary.opendfki.de/repos/trunk@954 953a6561-930b-0410-b2a6-db37d1b2ae63

--- java/de/dfki/lt/mary/recsessionmgr/gui/PromptTableModel.java
+++ java/de/dfki/lt/mary/recsessionmgr/gui/PromptTableModel.java
@@ -1,12 +1,31 @@
-/*
- * PromptTableModel.java
+/**
+ * Copyright 2007 DFKI GmbH.
+ * All Rights Reserved.  Use is subject to license terms.
+ * 
+ * Permission is hereby granted, free of charge, to use and distribute
+ * this software and its documentation without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of this work, and to
+ * permit persons to whom this work is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * 1. The code must retain the above copyright notice, this list of
+ *    conditions and the following disclaimer.
+ * 2. Any modifications must be clearly marked as such.
+ * 3. Original authors' names are not deleted.
+ * 4. The authors' names are not used to endorse or promote products
+ *    derived from this software without specific prior written
+ *    permission.
  *
- * Created on June 27, 2007, 4:29 PM
- *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
+ * DFKI GMBH AND THE CONTRIBUTORS TO THIS WORK DISCLAIM ALL WARRANTIES WITH
+ * REGARD TO THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS, IN NO EVENT SHALL DFKI GMBH NOR THE
+ * CONTRIBUTORS BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL
+ * DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+ * PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS
+ * ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
+ * THIS SOFTWARE.
  */
-
 package de.dfki.lt.mary.recsessionmgr.gui;
 
 import javax.swing.table.AbstractTableModel;
