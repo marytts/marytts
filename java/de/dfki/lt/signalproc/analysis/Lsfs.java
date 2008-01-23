9109dc82bf828c8fe287fd1170e6d9098b1774e6
Oytun Turk <oytun_turk@dfki.de>
2008-01-23 17:06:28 +0000

    Various updates due to new voice adaptation module
    git-svn-id: https://mary.opendfki.de/repos/trunk@1057 953a6561-930b-0410-b2a6-db37d1b2ae63

--- java/de/dfki/lt/signalproc/analysis/Lsfs.java
+++ java/de/dfki/lt/signalproc/analysis/Lsfs.java
@@ -31,7 +31,7 @@ package de.dfki.lt.signalproc.analysis;
 
 import java.io.IOException;
 
-import de.dfki.lt.signalproc.util.LEDataInputStream;
+import de.dfki.lt.signalproc.util.MaryRandomAccessFile;
 
 /**
  * @author oytun.turk
@@ -41,28 +41,109 @@ public class Lsfs {
     public double[][] lsfs;
     public LsfFileHeader params;
     
+    public Lsfs()
+    {
+        this("");
+    }
+    
     public Lsfs(String lsfFile)
     {
+        readLsfFile(lsfFile);
+    }
+    
+    public void readLsfFile(String lsfFile)
+    {
+        lsfs = null;
         params = new LsfFileHeader();
         
-        LEDataInputStream stream = null;
-        try {
-            stream = params.readLsfHeader(lsfFile, true);
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        
-        if (stream != null)
+        if (lsfFile!="")
         {
+            MaryRandomAccessFile stream = null;
             try {
-                lsfs = LineSpectralFrequencies.readLsfs(stream, params);
+                stream = params.readLsfHeader(lsfFile, true);
             } catch (IOException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
             }
+
+            if (stream != null)
+            {
+                try {
+                    lsfs = LineSpectralFrequencies.readLsfs(stream, params);
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
         }
-        else
-            lsfs = null;
+    }
+    
+    public void writeLsfFile(String lsfFile)
+    {   
+        if (lsfFile!="")
+        {
+            MaryRandomAccessFile stream = null;
+            try {
+                stream = params.writeLsfHeader(lsfFile, true);
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+            if (stream != null)
+            {
+                try {
+                    LineSpectralFrequencies.writeLsfs(stream, lsfs);
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    public void allocate()
+    {
+        allocate(params.numfrm, params.lpOrder);
+    }
+    
+    public void allocate(int numEntries, int lpOrder)
+    {
+        lsfs = null;
+        params.numfrm = 0;
+        params.lpOrder = 0;
+        
+        if (numEntries>0)
+        {
+            lsfs = new double[numEntries][];
+            params.numfrm = numEntries;
+            
+            if (lpOrder>0)
+            {
+                params.lpOrder = lpOrder;
+                
+                for (int i=0; i<numEntries; i++)
+                    lsfs[i] = new double[lpOrder];
+            }
+        }
+    }
+    
+    public static void main(String[] args)
+    {
+        Lsfs l1 = new Lsfs();
+        l1.params.lpOrder = 5;
+        l1.params.numfrm = 1;
+        l1.allocate();
+        l1.lsfs[0][0] = 1.5;
+        l1.lsfs[0][1] = 2.5;
+        l1.lsfs[0][2] = 3.5;
+        l1.lsfs[0][3] = 4.5;
+        l1.lsfs[0][4] = 5.5;
+
+
+        String lsfFile = "d:\\1.lsf";
+        l1.writeLsfFile(lsfFile);
+        Lsfs l2 = new Lsfs(lsfFile);
+
+        System.out.println("Test of class Lsfs completed...");
     }
 }
