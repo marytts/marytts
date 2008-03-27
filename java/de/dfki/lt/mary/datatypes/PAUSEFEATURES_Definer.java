67a8377fed6b3fbb5b1d671d8f12631b4b96d4cc
Benjamin Roth <bero01@dfki.de>
2008-03-27 17:35:00 +0000

    definer for pausefeatures
    git-svn-id: https://mary.opendfki.de/repos/trunk@1126 953a6561-930b-0410-b2a6-db37d1b2ae63

---
+++ java/de/dfki/lt/mary/datatypes/PAUSEFEATURES_Definer.java
@@ -0,0 +1,17 @@
+package de.dfki.lt.mary.datatypes;
+
+import de.dfki.lt.mary.MaryDataType;
+
+/**
+ * 
+ * @author benjaminroth
+ *
+ */
+
+public class PAUSEFEATURES_Definer extends MaryDataType{
+    static {
+        define("PAUSEFEATURES", null, false, true, PLAIN_TEXT, null, null,
+                         "(no example text)");
+
+    }
+}
