aca673c7c39b58b937347d59895dcfda8831325c
Marc Schroeder <marc.schroeder@dfki.de>
2008-04-29 06:41:36 +0000

    updated message in self-healing process -- restart is not required anymore.
    git-svn-id: https://mary.opendfki.de/repos/trunk@1233 953a6561-930b-0410-b2a6-db37d1b2ae63

--- java/de/dfki/lt/mary/MaryProperties.java
+++ java/de/dfki/lt/mary/MaryProperties.java
@@ -447,8 +447,7 @@ public class MaryProperties
         if (missing.contains("voice")) {
             int answer = JOptionPane.showConfirmDialog(null,
                     problem+"\n"+
-                    "Would you like to download `"+ component +"'\nusing the MARY Voice Installer?\n"
-                    + "After installation, please re-start the MARY server.",
+                    "Would you like to download `"+ component +"'\nusing the MARY Voice Installer?",
                     "Dependency problem",
                     JOptionPane.YES_NO_OPTION);
             if (answer == JOptionPane.YES_OPTION) {
