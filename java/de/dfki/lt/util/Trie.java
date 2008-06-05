3d56134c3e62af9b69eadd8b27a0de3ed3df6308
Benjamin Roth <bero01@dfki.de>
2008-06-05 15:20:16 +0000

    Trie is now made more generic. It can have all types of arc symbols, not only strings, or input output pairs. Input/Output pairs are now best treated with the inherited class TransducerTrie.
    git-svn-id: https://mary.opendfki.de/repos/trunk@1294 953a6561-930b-0410-b2a6-db37d1b2ae63

--- java/de/dfki/lt/util/Trie.java
+++ java/de/dfki/lt/util/Trie.java
@@ -62,20 +62,13 @@ import de.dfki.lt.mary.modules.phonemiser.PhonemeSet;
  * that allows for an even more efficient storage and lookup.
  * 
  * We are very thankful to Andreas Eisele who had the idea of using tries and
- * transducers for our purposes and who provided us with a c-program 
+ * transducers for our purposes and who provided us with c-code 
  * this particular implemention is based on.
- * 
- * The symbols used in this trie are strings being a concatenation of an input
- * symbol, a delimiter (':') and an output symbol. To get the transducer 
- * representation first compute the minimization of the trie, then write the
- * transducer to disk. 
- * 
- * See main method for example usage.
  * 
  * @author benjaminroth
  *
  */
-public class Trie {
+public class Trie<Symbol> {
 
     // class to store nodes of a trie
     class TrieNode{
@@ -84,27 +77,22 @@ public class Trie {
         private Map<Integer,TrieNode> labelId2node = new HashMap<Integer, TrieNode>();
         
         // true if this state marks the end of an entry
-        private boolean isFinal = false;
+        protected boolean isFinal = false;
         
         // id for transducer representation. -1 means no id asigned.
         private int id = -1;
         
         // pointer to mother node - needed for minimization
         private TrieNode backPointer = null;
-        
-        // back and forth mapping from labels to ids
-        private Map<String, Integer> label2id;
-        private List<String> labels;
-       
+              
         /**
          * This constructs a TrieNode and specifies its predecessor.
          * 
          * @param predecessor
          */
-        public TrieNode(TrieNode predecessor, Map<String, Integer> label2idMap, List<String> labels){
+        public TrieNode(TrieNode predecessor, Map<Symbol, Integer> label2idMap, List<Symbol> labels){
             this.backPointer = predecessor;
-            this.label2id = label2idMap;
-            this.labels = labels;
+
         }
         
         /**
@@ -117,7 +105,7 @@ public class Trie {
          *  this node.
          * @return the final node of this entry.
          */
-        protected TrieNode add(String[] entry, int index){
+        protected TrieNode add(Symbol[] entry, int index){
             
             if (index == entry.length){
                 // index points to the end of the word: already everything entered.
@@ -126,11 +114,11 @@ public class Trie {
                 return this;
             }
             
-            Integer labelId = this.label2id.get(entry[index]);
+            Integer labelId = label2id.get(entry[index]);
             if (null == labelId){
-                labelId = this.labels.size();
-                this.labels.add(entry[index]);
-                this.label2id.put(entry[index], labelId);
+                labelId = labels.size();
+                labels.add(entry[index]);
+                label2id.put(entry[index], labelId);
             }
                 
             // add rest of the entry to successors
@@ -139,7 +127,7 @@ public class Trie {
             
             if ( null == successor ){
                 
-                successor = new TrieNode(this, this.label2id, this.labels);
+                successor = new TrieNode(this, label2id, labels);
                 this.labelId2node.put(labelId, successor);
             }
             
@@ -213,7 +201,7 @@ public class Trie {
             
             for (Integer lId : this.labelId2node.keySet()){
                 
-                String l = labels.get(lId);
+                String l = labels.get(lId).toString();
                 
                 sb.append("\n");
                 sb.append("|-" + l);
@@ -230,26 +218,26 @@ public class Trie {
     }
     
     // node that point to the beginning of all words
-    private TrieNode root;
+    protected TrieNode root;
     
     // list of nodes that mark the end of a entry (word)
-    private List<TrieNode> finalNodes;
+    protected List<TrieNode> finalNodes;
     
     // mapping from nodes to representatives in a minimized transducer view.
     // mapping is done via list indices
-    private List<TrieNode> reprs = null;
+    protected List<TrieNode> reprs = null;
     
     // back and forth mapping from labels to ids
-    private Map<String, Integer> label2id;
-    private List<String> labels;
+    protected Map<Symbol, Integer> label2id;
+    protected List<Symbol> labels;
     
     /**
      * Standard constructor for a trie.
      */
     public Trie() {
         
-        this.label2id = new HashMap<String, Integer>();
-        this.labels = new ArrayList<String>();
+        this.label2id = new HashMap<Symbol, Integer>();
+        this.labels = new ArrayList<Symbol>();
         
         this.root = new TrieNode(null, label2id, labels);
         
@@ -262,7 +250,7 @@ public class Trie {
      * 
      * @param entry
      */
-    public void add(String[] entry){
+    public void add(Symbol[] entry){
         // add the entry and remember backpointer to final node
         this.finalNodes.add(this.root.add(entry, 0));
     }
@@ -332,195 +320,5 @@ public class Trie {
         return sb.toString();
         
     }
-    
-    public void writeFST(DataOutputStream out) throws IOException{
         
-        if (null == this.reprs)
-            throw new IllegalStateException("Cannot write transducer. First compute minimization of trie.");
-        
-        // compute arc offsets
-        int[] arcOffsets = new int[this.reprs.size()+1];
-            
-        // first has offset one (consider additional start arc)
-        arcOffsets[0] = 1;
-           
-        for (int i=0; i< reprs.size(); i++){
-           arcOffsets[i+1] = arcOffsets[i] + reprs.get(i).getArcMap().size();
-           
-           // if final, consider the added "final arc"
-           if (reprs.get(i).isFinal){
-               arcOffsets[i+1] += 1;
-           }
-        }
-        
-        // write number of arcs
-        out.writeInt(arcOffsets[arcOffsets.length-1]);
-        
-        // write starting arc:
-        //      pointing to start node offset - empty label - final
-        int startArc = arcOffsets[root.getId()] | 1 << 20 | 1 <<31;
-        
-        out.writeInt(startArc);
-                
-        // write arcs, final nodes have final arc as last with same empty label as start
-        // dont forget to add one
-        for (TrieNode repr : reprs){
-            
-            List<Integer> arcVals = new ArrayList<Integer>();
-            
-            for (Integer labelId : repr.getArcMap().keySet()){
-                int targetId = repr.getArcMap().get(labelId).getId();
-                arcVals.add( arcOffsets[targetId] | (labelId + 2) << 20 );
-            }
-            
-            // if final, consider the added "final arc"
-            if (repr.isFinal){
-                arcVals.add( arcOffsets[repr.getId()] | 0 << 20 | 1 << 31 );
-                
-            } else {
-                // mark last of the arcs as "last"
-                int last = arcVals.size() -1;
-                arcVals.set(last, arcVals.get(last) | 1 << 31);                
-            }
-            
-            for (Integer val : arcVals){ out.writeInt(val); }
-            
-         }
-        
-        // compute label offsets
-        int[] labelOffsets = new int[this.labels.size()*2];
-            
-        // first has offset two (input and output of empty arc)
-        labelOffsets[0] = 4;
-           
-        for (int i=0; i< labels.size(); i++){
-            
-            // TODO: make split symbol argument
-            String[] ioSym = labels.get(i).split(":",2); 
-            
-            // offset of outS determined by offset of inS
-            labelOffsets[i*2+1] = labelOffsets[i*2];
-            // offset increased by length of inS
-            labelOffsets[i*2+1] += ioSym[0].getBytes().length;// TODO: use encoding
-            // additionally increased by one because of stop byte
-            labelOffsets[i*2+1] += 1;
-                
-            if (i+1<labels.size()){
-                // offset of next inS determined by this outS
-                labelOffsets[(i+1)*2] = labelOffsets[i*2+1];
-                labelOffsets[(i+1)*2] += ioSym[1].getBytes().length;// TODO: use encoding
-                labelOffsets[(i+1)*2] += 1;
-            }
-            
-        }
-        
-        // write number of pairs
-        out.writeInt(labels.size() + 2);
-        
-        // write empty label id/offset
-        out.writeShort(0);
-        out.writeShort(1);
-        out.writeShort(2);
-        out.writeShort(3);
-        
-        // write pair offsets
-        for (int i=0; i< labels.size(); i++){
-            out.writeShort( labelOffsets[i*2] );
-            out.writeShort(labelOffsets[i*2+1]);
-        }
-        
-        // write first two pairs: just empty symbols
-        out.writeByte(0);
-        out.writeByte(0);
-        out.writeByte(0);
-        out.writeByte(0);
-
-        // write pairs
-        for (int i=0; i< labels.size(); i++){
-            
-            // TODO: make split symbol argument
-            String[] ioSym = labels.get(i).split(":",2); 
-                       
-            out.write(ioSym[0].getBytes());// TODO: use encoding
-            out.writeByte(0);
-            out.write(ioSym[1].getBytes());// TODO: use encoding
-            out.writeByte(0);
-        }
-        
-    }
-    
-
-
-public static void main(String[] args) throws IOException, SAXException, ParserConfigurationException {
-    // example usage
-    
-    // specify phone set definition
-    String  phFileLoc = "phoneme-list-engb.xml";
-
-    // specify location of lexicon you want to encode
-    BufferedReader lexReader = new BufferedReader(
-            new InputStreamReader(
-            new FileInputStream("sampa-lexicon.txt"),"ISO-8859-1"));
-    
-    // specify location of output
-    String fstLocation = "lexicon.fst";
-    
- 
-    
-    // initialize trainer 
-    AlignerTrainer at = new AlignerTrainer(PhonemeSet.getPhonemeSet(phFileLoc), Locale.ENGLISH);
-    
-    System.out.println("reading lexicon...");
-    
-    // read lexicon for training
-    at.readSampaLexicon(lexReader, true);
-    
-    System.out.println("...done!");
-
-    System.out.println("aligning...");
-    // make some alignment iterations
-    for ( int i = 0 ; i < 8 ; i++ ){
-        System.out.println(" iteration " + i);
-        at.alignIteration();
-        
-    }
-    System.out.println("...done!");
-    
-    Trie t = new Trie();
-    
-    System.out.println("entering alignments in trie...");
-    for (int i = 0; i<at.lexiconSize(); i++){
-        t.add(at.getAlignment(i));
-    }
-    System.out.println("...done!");
-
-    System.out.println("minimizing trie...");
-    t.computeMinimization();
-    System.out.println("...done!");
-    
-    System.out.println("writing transducer to disk...");
-    File of = new File(fstLocation);
-
-    
-    DataOutputStream os = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(of)));
-    
-    t.writeFST(os);
-    os.flush();
-    os.close();
-    System.out.println("...done!");
-
-    
-    System.out.println("looking up test words...");
-    FSTLookup fst = new FSTLookup(fstLocation);
-
-    System.out.println(" zoroastrians -> " + Arrays.toString(fst.lookup("zoroastrians")));
-    System.out.println(" the -> " + Arrays.toString(fst.lookup("the")));
-    System.out.println(" thanks -> " + Arrays.toString(fst.lookup("thanks")));
-    System.out.println(" xylophone -> " + Arrays.toString(fst.lookup("xylophone")));
-
-    
-    System.out.println("...done!");
-    
-    }
-    
 }
