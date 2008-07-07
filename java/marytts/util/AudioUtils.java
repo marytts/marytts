e80c25dae2624d1c0c4edb5f047b2d32075add89
Oytun Turk <oytun_turk@dfki.de>
2008-07-07 09:33:03 +0000

    git-svn-id: https://mary.opendfki.de/repos/trunk@1352 953a6561-930b-0410-b2a6-db37d1b2ae63

--- java/marytts/util/AudioUtils.java
+++ java/marytts/util/AudioUtils.java
@@ -29,54 +29,20 @@
 
 package marytts.util;
 
-import java.util.Arrays;
+
+import javax.sound.sampled.AudioInputStream;
+
+import marytts.util.audio.AudioDoubleDataSource;
+
+
 
 /**
- * This is a wrapper class that can hold two double arrays, one of which is 
- * interpreted as containing the real values, the other the imaginary values
- * of the complex numbers.
  * @author Marc Schr&ouml;der
  *
  */
-public class ComplexArray
-{
-    public double [] real;
-    public double [] imag;
+public class AudioUtils {
 
-    public ComplexArray(int len)
-    {
-        init(len);
-    }
-    
-    public ComplexArray(double[] realVals, double[] imagVals)
-    {
-        if (realVals!=null && imagVals!=null && realVals.length==imagVals.length)
-        {
-            init(realVals.length);
-            System.arraycopy(realVals, 0, real, 0, realVals.length);
-            System.arraycopy(imagVals, 0, imag, 0, imagVals.length);
-        }
-    }
-
-    public ComplexArray(ComplexArray c)
-    {
-        this(c.real, c.imag);
-    }
-
-    public void init(int len)
-    {
-        if (len>0)
-        {
-            real = new double[len];
-            imag = new double[len];
-
-            Arrays.fill(real, 0.0);
-            Arrays.fill(imag, 0.0);
-        }
-        else
-        {
-            real = null;
-            imag = null;
-        }
+    public static double[] getSamplesAsDoubleArray(AudioInputStream ais) {
+        return new AudioDoubleDataSource(ais).getAllData();
     }
 }
