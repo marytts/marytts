6b4f9082fdff215ad7f8ac3bb55b6ca3b1547eaf
Oytun Turk <oytun_turk@dfki.de>
2008-10-07 09:38:22 +0000

    Ticket #192: Functionality added to discard silence in the beginning and at the end of recordings with support for leaving specified amounts of silence
    git-svn-id: https://mary.opendfki.de/repos/trunk@1476 953a6561-930b-0410-b2a6-db37d1b2ae63

--- java/marytts/tools/voiceimport/EndpointDetector.java
+++ java/marytts/tools/voiceimport/EndpointDetector.java
@@ -7,9 +7,22 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.util.*;
 
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
 import marytts.client.MaryClient;
 import marytts.datatypes.MaryDataType;
+import marytts.signalproc.analysis.EnergyAnalyser;
+import marytts.util.data.BufferedDoubleDataSource;
+import marytts.util.data.DoubleDataSource;
+import marytts.util.data.audio.AudioDoubleDataSource;
+import marytts.util.data.audio.DDSAudioInputStream;
 import marytts.util.io.FileUtils;
+import marytts.util.signal.SignalProcUtils;
+import marytts.util.string.PrintfFormat;
 
 
 
@@ -53,7 +66,6 @@ public class EndpointDetector extends VoiceImportComponent
              props.put(OUTPUTWAVDIR, db.getProp(db.ROOTDIR)
                      +"outputwav"
                      +System.getProperty("file.separator"));
-             
          } 
          return props;
      }
@@ -91,12 +103,19 @@ public class EndpointDetector extends VoiceImportComponent
         // Automatically collect all ".wav" files from given directory
         bnlist = new BasenameList(inputWavDir+File.separator,waveExt);
         
+        /* TO DO
+         * Get these two values from the interface
+         */
+        double minimumStartSilenceInSeconds = 1.0;
+        double minimumEndSilenceInSeconds = 1.0;
+        //
+        
         System.out.println( "Removing endpoints for "+ bnlist.getLength() + " wave files" );
         for (int i=0; i<bnlist.getLength(); i++) {
             percent = 100*i/bnlist.getLength();
             String inputFile = inputWavDir + File.separator + bnlist.getName(i) + waveExt;
             String outputFile = outputWavDir + File.separator + bnlist.getName(i) + waveExt;
-            removeEndponits(inputFile,outputFile);
+            removeEndponits(inputFile,outputFile,minimumStartSilenceInSeconds,minimumEndSilenceInSeconds);
             System.out.println( "    " + bnlist.getName(i) );
         }
         System.out.println("...Done.");
@@ -109,17 +128,83 @@ public class EndpointDetector extends VoiceImportComponent
      * @param inputFile 
      * @param outputFile
      * @throws IOException
+     * @throws  
      */
-    public void removeEndponits(String inputFile, String outputFile) throws IOException
+    public void removeEndponits(String inputFile, String outputFile,
+                                double minimumStartSilenceInSeconds,
+                                double minimumEndSilenceInSeconds) throws IOException
     {
-        /**TODO
+        /*
          * Add corresponding module to remove endpoints
          * 1. identify and remove end points
-         * 2. store as output wavefile 
+         * 2. make sure at least some desired amount of silence in the beginning and at the end
+         * 3. store as output wavefile 
          */
         
+        AudioInputStream ais = null;
+        try {
+            ais = AudioSystem.getAudioInputStream(new File(inputFile));
+        } catch (UnsupportedAudioFileException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         
-        
+        if (ais!=null)
+        {
+            if (!ais.getFormat().getEncoding().equals(AudioFormat.Encoding.PCM_SIGNED)) {
+                ais = AudioSystem.getAudioInputStream(AudioFormat.Encoding.PCM_SIGNED, ais);
+            }
+            if (ais.getFormat().getChannels() > 1) {
+                throw new IllegalArgumentException("Can only deal with mono audio signals");
+            }
+            int samplingRate = (int) ais.getFormat().getSampleRate();
+            DoubleDataSource signal = new AudioDoubleDataSource(ais);
+            int framelength = (int)(0.01 /*seconds*/ * samplingRate);
+            EnergyAnalyser ea = new EnergyAnalyser(signal, framelength, framelength, samplingRate);
+            //double[][] speechStretches = ea.getSpeechStretches();
+            double[][] speechStretches = ea.getSpeechStretchesUsingEnergyHistory();
+            int numStretches = speechStretches.length;
+            int speechStartIndex = (int)(samplingRate*speechStretches[0][0]);
+            int speechEndIndex = (int)(samplingRate*speechStretches[numStretches-1][1]);
+            speechStartIndex = Math.min(0, speechStartIndex);
+
+            int silStartLen = Math.max(0, (int)(samplingRate*minimumStartSilenceInSeconds));
+            double[] silStart = SignalProcUtils.getWhiteNoise(silStartLen, 1e-20);
+            int silEndLen = Math.max(0, (int)(samplingRate*minimumEndSilenceInSeconds));
+            double[] silEnd = SignalProcUtils.getWhiteNoise(silEndLen, 1e-20);
+
+            double[] y = null;
+            if (speechEndIndex-speechStartIndex+silStartLen+silEndLen>0)
+                y = new double[speechEndIndex-speechStartIndex+silStartLen+silEndLen];
+            else
+                throw new Error("No output samples to write for " + inputFile);
+
+            double[] x = signal.getAllData();
+
+            int start = 0;
+            if (silStartLen>0)
+            {
+                System.arraycopy(silStart, 0, y, start, silStartLen);
+                start += silStartLen;
+            }
+
+            if (speechEndIndex-speechStartIndex>0)
+            {
+                System.arraycopy(x, speechStartIndex, y, start, speechEndIndex-speechStartIndex);
+                start += (speechEndIndex-speechStartIndex);
+            }
+
+            if (silEndLen>0)
+            {
+                System.arraycopy(silEnd, 0, y, start, silEndLen);
+                start += silEndLen;
+            }
+
+            DDSAudioInputStream outputAudio = new DDSAudioInputStream(new BufferedDoubleDataSource(y), ais.getFormat());
+            AudioSystem.write(outputAudio, AudioFileFormat.Type.WAVE, new File(outputFile));
+        }
+        else
+            throw new Error("Cannot open input file " + inputFile);
     }
     
     /**
