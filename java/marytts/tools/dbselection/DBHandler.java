5b0153d452b4b5920300cca50d80424d2cbc7a4b
Marcela Charfuelan <Marcela.Charfuelan@dfki.de>
2008-10-27 09:27:17 +0000

    fixed PreparedStatement problem.
    git-svn-id: https://mary.opendfki.de/repos/trunk@1519 953a6561-930b-0410-b2a6-db37d1b2ae63

--- java/marytts/tools/dbselection/DBHandler.java
+++ java/marytts/tools/dbselection/DBHandler.java
@@ -35,6 +35,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.PreparedStatement;
 import java.util.Properties;
 
 /**
