f4bf14de6dcc147ce3780662fa67cde5a6997f72
Oytun Turk <oytun_turk@dfki.de>
2008-11-14 15:43:21 +0000

    Client-server protocol with HTTP requests test release: HTTP based client/server and web browser support is now fully functional. The latest version of the code is checked in as version 1570.
    Here are the steps to test the new protocol:
    
    Server: 
    Either
    - change "server=..." field in marybase.config to "server=http"
    or
    - give Dserver=http as a command line/run target argument to override marybase.config value
    P.S.: To return to the old socket-based server, set "server=socket" (Dserver=socket)
          To use command line server, set "server=commandline" (Dserver=commandline)
    ---------
    Java GUI client:
    - Run MaryGUIHttpClient with parameters identical to MaryGUIClient (Duplicate run target for MaryGUIClient in Eclipse by right-clicking on it, then change the main class to marytts.client.http.MaryGUIHttpClient)
    --------------
    Web browser client:
    - Open a browser page and type "localhost:59125" in the address bar (after starting the server, of course)
    
    Please try the web browser client in different operating systems/browsers and let me know about the outcome (both problems and success:).
    
    I have already faced several problems (all under Windows Vista):
    - Opera does not play audio
    - Internet Explorer does not display Mary TTS icon
    
    In Mozilla/Firefox, it seems to work fine.
    I have not tried Netscape at all.
    
    
    
    
    git-svn-id: https://mary.opendfki.de/repos/trunk@1570 953a6561-930b-0410-b2a6-db37d1b2ae63

--- java/marytts/server/http/MaryHttpServer.java
+++ java/marytts/server/http/MaryHttpServer.java
@@ -473,7 +473,6 @@ public class MaryHttpServer {
                     }
                     
                     response.setStatusCode(status);
-                    
                     return;
                 }
                 
