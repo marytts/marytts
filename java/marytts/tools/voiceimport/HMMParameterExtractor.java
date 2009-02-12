7e77f0f3fad5cd9d0a70a64e56450cc7d50558d1
Sathish Chandra Pammi <sach01@dfki.de>
2009-02-12 10:43:28 +0000

    Modifications in HMM parameter generation voice import component: more config. settings for output generation. for example, useGV or not
    git-svn-id: https://mary.opendfki.de/repos/trunk@1757 953a6561-930b-0410-b2a6-db37d1b2ae63

--- java/marytts/tools/voiceimport/HMMParameterExtractor.java
+++ java/marytts/tools/voiceimport/HMMParameterExtractor.java
@@ -39,7 +39,7 @@ import marytts.util.MaryUtils;
 public class HMMParameterExtractor extends VoiceImportComponent {
 
     public final String MARYBASE = "HMMParameterExtractor.maryBase";
-    public final String LOCALE = "HMMParameterExtractor.locale";
+    public final String VOICECONFIG = "HMMParameterExtractor.voiceConfigFile";
     public final String VOICENAME = "HMMParameterExtractor.voiceName";
     public final String PHONEFEATS = "HMMParameterExtractor.phonefeaturesDir";
     public final String OUTHMMDIR = "HMMParameterExtractor.outputDir";
@@ -47,7 +47,7 @@ public class HMMParameterExtractor extends VoiceImportComponent {
     public final String PLAB = "HMMParameterExtractor.printLab";
     public final String PWAVE = "HMMParameterExtractor.printWave";
     public final String PSLAB = "HMMParameterExtractor.PrintStateLab";
-    //public final String CONFIGFILE = "HMMParameterExtractor.configFile";
+    public final String USEGV   = "HMMParameterExtractor.useGV";
     
     private DatabaseLayout db;    
     private String sCostDirectory;
@@ -58,14 +58,21 @@ public class HMMParameterExtractor extends VoiceImportComponent {
     private String wavExt = ".wav";
     private String hplabExt = ".hplab";
     protected int percent = 0;
+    private HTSEngine hmm_tts;
+    private HMMData htsData;
     
     protected void setupHelp() {
         props2Help = new TreeMap();
         props2Help.put(MARYBASE,"Mary Base.  ex: /home/user/MARY400/ ");
-        props2Help.put(LOCALE,"locale ex: english"); 
+        props2Help.put(VOICECONFIG,"locale ex: english"); 
         props2Help.put(VOICENAME, "Voice name ex: slt-arctic");
         props2Help.put(PHONEFEATS, "Phonefeatures directory");
-        props2Help.put(OUTHMMDIR, "Output directory to store hmm generated parameters"); 
+        props2Help.put(OUTHMMDIR, "Output directory to store hmm generated parameters");
+        props2Help.put(PPARAMETERS, "Generate parameters like MFCC, PITCH files?");
+        props2Help.put(PLAB, "Generate HMM Label files?");
+        props2Help.put(PWAVE, "Generate HMM WAVE files?");
+        props2Help.put(PSLAB, "Generate HMM State Label files?");
+        props2Help.put(USEGV, "Use Global variance(GV) in parameter generation?");
     }
     
     public SortedMap<String, String> getDefaultProps(DatabaseLayout db) {
@@ -73,7 +80,7 @@ public class HMMParameterExtractor extends VoiceImportComponent {
         if (props == null){
             props = new TreeMap();
             props.put(MARYBASE,db.getProp(db.MARYBASE));
-            props.put(LOCALE,"english"); 
+            props.put(VOICECONFIG,"english-hsmm-slt.config"); 
             props.put(VOICENAME, "hsmm-slt");
             props.put(PHONEFEATS,db.getProp(db.ROOTDIR)+File.separator+"phonefeatures");
             props.put(OUTHMMDIR, db.getProp(db.ROOTDIR)+File.separator+"sCost"+File.separator+"hmmparams"); 
@@ -81,6 +88,7 @@ public class HMMParameterExtractor extends VoiceImportComponent {
             props.put(PLAB,"true");
             props.put(PWAVE,"false");
             props.put(PSLAB,"false");
+            props.put(USEGV,"false");
         }
         return props;
     }
@@ -114,10 +122,39 @@ public class HMMParameterExtractor extends VoiceImportComponent {
             System.out.print("Created successfully.\n");
         }
         
+        
+        /* For initialise provide the name of the hmm voice and the name of its configuration file,
+         * also indicate the name of your MARY_BASE directory.*/
+        String MaryBase = getProp(MARYBASE);
+        //String locale = getProp(VOICECONFIG);
+        String voice = getProp(VOICENAME);
+        String configFile = getProp(VOICECONFIG);
+        // directory where the context features of each file are
+        String contextFeaDir = getProp(PHONEFEATS);
+        // the output dir has to be created already
+        String outputDir = getProp(OUTHMMDIR); 
+        // Create a htsengine object
+        hmm_tts = new HTSEngine();
+        
+        // Create and set HMMData
+        htsData = new HMMData();
+        htsData.initHMMData(voice, MaryBase, configFile);
+           
+        // Settings for using GV
+        if(getProp(USEGV).equals("true")){
+            htsData.setUseGV(true);
+        }
+        else{
+            htsData.setUseGV(false);
+        }
+        
+        //Settings for mixed excitation
+        htsData.setUseMixExc(true);
+        
         // Now process all files, one by one
         for (int i=0; i<bnl.getLength(); i++) {
             percent = 100*i/bnl.getLength();
-            generateParameters(bnl.getName(i));
+            generateParameters(bnl.getName(i), contextFeaDir, outputDir);
             boolean success = createHalfPhoneLab(bnl.getName(i));
             if(!success) return false;
         }
@@ -134,102 +171,75 @@ public class HMMParameterExtractor extends VoiceImportComponent {
      * @param args
      * @throws IOException
      */
-    public void generateParameters(String file) throws IOException, InterruptedException{
+    public void generateParameters(String file, String contextFeaDir, String outputDir) throws IOException, InterruptedException{
        
-      int i, j;  
-      /* For initialise provide the name of the hmm voice and the name of its configuration file,
-       * also indicate the name of your MARY_BASE directory.*/
-      String MaryBase = getProp(MARYBASE);
-      String locale = getProp(LOCALE);
-      String voice = getProp(VOICENAME);
-      String configFile = locale + "-" + voice + ".config";
-      
-      // directory where the context features of each file are
-      String contextFeaDir = getProp(PHONEFEATS);
-      
-      // the output dir has to be created already
-      String outputDir = getProp(OUTHMMDIR); 
-      
-      // Create a htsengine object
-      HTSEngine hmm_tts = new HTSEngine();
-      
-      // Create and set HMMData
-      HMMData htsData = new HMMData();
-      htsData.initHMMData(voice, MaryBase, configFile);
-      float fperiodmillisec = ((float)htsData.getFperiod() / (float)htsData.getRate()) * 1000;
-      float fperiodsec = ((float)htsData.getFperiod() / (float)htsData.getRate());
-         
-      // Settings for using GV, mixed excitation
-      htsData.setUseGV(true);
-      htsData.setUseMixExc(true);
-   
-      /* generate files out of HMMs */
-      String feaFile, parFile, durStateFile, durFile, mgcModifiedFile, outWavFile;
-      try {
-        
-        /* The utterance model, um, is a Vector (or linked list) of Model objects. 
-         * It will contain the list of models for the current label file. */
-        HTSUttModel um = new HTSUttModel();
-        HTSParameterGeneration pdf2par = new HTSParameterGeneration();
-        HTSVocoder par2speech = new HTSVocoder();
-        AudioInputStream ais;
-        
-        
-        /* Process label file of Mary context features and creates UttModel um.   */
-        feaFile      = contextFeaDir + file + pfeatExt;
-        hmm_tts.processUttFromFile(feaFile, um, htsData);
+        float fperiodmillisec = ((float)htsData.getFperiod() / (float)htsData.getRate()) * 1000;
+        float fperiodsec = ((float)htsData.getFperiod() / (float)htsData.getRate());
+        /* generate files out of HMMs */
+        String feaFile, parFile, durStateFile, durFile, mgcModifiedFile, outWavFile;
+        try {
           
-        
-        if(getProp(PLAB).equals("true")){
-            /* save realised durations in a lab file */
-            FileWriter outputStream;
-            durFile      = outputDir + file + labExt;   /* realised durations */
-            outputStream = new FileWriter(durFile);
-            outputStream.write(hmm_tts.getRealisedDurations());
-            outputStream.close();
+          /* The utterance model, um, is a Vector (or linked list) of Model objects. 
+           * It will contain the list of models for the current label file. */
+          HTSUttModel um = new HTSUttModel();
+          HTSParameterGeneration pdf2par = new HTSParameterGeneration();
+          HTSVocoder par2speech = new HTSVocoder();
+          AudioInputStream ais;
+          
+          
+          /* Process label file of Mary context features and creates UttModel um.   */
+          feaFile      = contextFeaDir + file + pfeatExt;
+          hmm_tts.processUttFromFile(feaFile, um, htsData);
+            
+          
+          if(getProp(PLAB).equals("true")){
+              /* save realised durations in a lab file */
+              FileWriter outputStream;
+              durFile      = outputDir + file + labExt;   /* realised durations */
+              outputStream = new FileWriter(durFile);
+              outputStream.write(hmm_tts.getRealisedDurations());
+              outputStream.close();
+          }
+          if(getProp(PSLAB).equals("true")){
+              /* save realised durations at state label in a slab file */
+              float totalDur=0;
+              int numStates=htsData.getCartTreeSet().getNumStates();
+              durStateFile = outputDir + file + slabExt;  /* state level realised durations */
+              FileWriter outputStream = new FileWriter(durStateFile);
+              outputStream.write("#\n");
+              for(int i=0; i<um.getNumModel(); i++){
+                for(int j=0; j<numStates; j++){
+                    totalDur += (um.getUttModel(i).getDur(j)*fperiodsec);
+                    if(j < (numStates-1))
+                      outputStream.write(totalDur + " 0 " + um.getUttModel(i).getPhoneName() + "\n");
+                    else
+                      outputStream.write(totalDur + " 1 " + um.getUttModel(i).getPhoneName() + "\n");   
+                  }         
+              }
+              outputStream.close();
+          }
+          
+          if(getProp(PPARAMETERS).equals("true")){
+              /* Generate sequence of speech parameter vectors, generate parameters out of sequence of pdf's */ 
+              boolean debug = true;  /* with debug=true it saves the generated parameters  
+                                        f0 and mfcc in parFile.f0 and parFile.mfcc in Mary format. */
+              parFile      = outputDir + file;            /* generated parameters mfcc and f0, Mary format */
+              pdf2par.htsMaximumLikelihoodParameterGeneration(um, htsData, parFile, debug);
+          }
+          
+          if(getProp(PWAVE).equals("true")){
+              /* Synthesize speech waveform, generate speech out of sequence of parameter */
+              ais = par2speech.htsMLSAVocoder(pdf2par, htsData);
+              outWavFile   = outputDir + file + wavExt;   /* generated wav file */
+              System.out.println("saving to file: " + outWavFile);
+              File fileOut = new File(outWavFile);
+              if (AudioSystem.isFileTypeSupported(AudioFileFormat.Type.WAVE,ais)) {
+                AudioSystem.write(ais, AudioFileFormat.Type.WAVE, fileOut);
+              }
+          }
+        } catch (Exception e) {
+            System.err.println("Exception: " + e.getMessage());
         }
-        
-
-        if(getProp(PSLAB).equals("true")){
-            /* save realised durations at state label in a slab file */
-            float totalDur=0;
-            int numStates=htsData.getCartTreeSet().getNumStates();
-            durStateFile = outputDir + file + slabExt;  /* state level realised durations */
-            FileWriter outputStream = new FileWriter(durStateFile);
-            outputStream.write("#\n");
-            for(i=0; i<um.getNumModel(); i++){
-              for(j=0; j<numStates; j++){
-                  totalDur += (um.getUttModel(i).getDur(j)*fperiodsec);
-                  if(j < (numStates-1))
-                    outputStream.write(totalDur + " 0 " + um.getUttModel(i).getPhoneName() + "\n");
-                  else
-                    outputStream.write(totalDur + " 1 " + um.getUttModel(i).getPhoneName() + "\n");   
-                }         
-            }
-            outputStream.close();
-        }
-        
-        if(getProp(PPARAMETERS).equals(true)){
-            /* Generate sequence of speech parameter vectors, generate parameters out of sequence of pdf's */ 
-            boolean debug = true;  /* with debug=true it saves the generated parameters  
-                                      f0 and mfcc in parFile.f0 and parFile.mfcc in Mary format. */
-            parFile      = outputDir + file;            /* generated parameters mfcc and f0, Mary format */
-            pdf2par.htsMaximumLikelihoodParameterGeneration(um, htsData, parFile, debug);
-        }
-        
-        if(getProp(PWAVE).equals(true)){
-            /* Synthesize speech waveform, generate speech out of sequence of parameter */
-            ais = par2speech.htsMLSAVocoder(pdf2par, htsData);
-            outWavFile   = outputDir + file + wavExt;   /* generated wav file */
-            System.out.println("saving to file: " + outWavFile);
-            File fileOut = new File(outWavFile);
-            if (AudioSystem.isFileTypeSupported(AudioFileFormat.Type.WAVE,ais)) {
-              AudioSystem.write(ais, AudioFileFormat.Type.WAVE, fileOut);
-            }
-        }
-      } catch (Exception e) {
-          System.err.println("Exception: " + e.getMessage());
-      }
       
     }  /* main method */
     
