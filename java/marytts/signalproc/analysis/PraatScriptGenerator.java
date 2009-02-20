442f7fa28808f782d766be54557d5a6abe463681
Oytun Turk <oytun_turk@dfki.de>
2009-02-20 08:52:55 +0000

    Duration scaling for HNM (still experimental, only harmonics and noise part are being handled, transients not yet)
    git-svn-id: https://mary.opendfki.de/repos/trunk@1776 953a6561-930b-0410-b2a6-db37d1b2ae63

--- java/marytts/signalproc/analysis/PraatScriptGenerator.java
+++ java/marytts/signalproc/analysis/PraatScriptGenerator.java
@@ -60,19 +60,43 @@ public class PraatScriptGenerator {
             OutputStreamWriter out = new OutputStreamWriter(fos, "UTF-8");
 
             String outputFile;
+            
+            //Harmonicity analysis
             for (int i=0; i<wavFiles.length; i++)
             {
-                out.write("Read from file... " + wavFiles[i] + System.getProperty("line.separator"));
-                
-                //Harmonicity analysis
+                out.write("Read from file... " + wavFiles[i] + System.getProperty("line.separator"));    
                 out.write("To Harmonicity (cc)... 0.01 75 0.1 1.0" + System.getProperty("line.separator"));
                 outputFile = StringUtils.modifyExtension(wavFiles[i], "hcc");
                 out.write("Write to text file... " + outputFile + System.getProperty("line.separator"));
                 out.write("select all" + System.getProperty("line.separator"));
                 out.write("Remove" + System.getProperty("line.separator"));
-                //
                 
-                //Jitter/shimmer analysis
+                out.write("Read from file... " + wavFiles[i] + System.getProperty("line.separator"));    
+                out.write("To Harmonicity (ac)... 0.01 75 0.1 4.5" + System.getProperty("line.separator"));
+                outputFile = StringUtils.modifyExtension(wavFiles[i], "hac");
+                out.write("Write to text file... " + outputFile + System.getProperty("line.separator"));
+                out.write("select all" + System.getProperty("line.separator"));
+                out.write("Remove" + System.getProperty("line.separator"));
+                
+                out.write("Read from file... " + wavFiles[i] + System.getProperty("line.separator"));    
+                out.write("To Harmonicity (gne)... 500 4500 1000 80" + System.getProperty("line.separator"));
+                outputFile = StringUtils.modifyExtension(wavFiles[i], "hgne");
+                out.write("Write to text file... " + outputFile + System.getProperty("line.separator"));
+                out.write("select all" + System.getProperty("line.separator"));
+                out.write("Remove" + System.getProperty("line.separator"));
+                
+                System.out.println("Added harmonicity file " + String.valueOf(i+1) + " of " + String.valueOf(wavFiles.length));
+            }
+            out.close();
+            //
+            
+            //Jitter/shimmer analysis
+            scriptFile = args[2];
+            fos = new FileOutputStream(scriptFile); 
+            out = new OutputStreamWriter(fos, "UTF-8");
+            for (int i=0; i<wavFiles.length; i++)
+            {
+                out.write("Read from file... " + wavFiles[i] + System.getProperty("line.separator"));  
                 out.write("snd = selected (\"Sound\")" + System.getProperty("line.separator"));
                 out.write("To Pitch (cc)... 0 60 15 yes 0.03 0.45 0.01 0.35 0.14 600" + System.getProperty("line.separator"));
                 out.write("pit = selected (\"Pitch\")" + System.getProperty("line.separator"));
@@ -104,15 +128,14 @@ public class PraatScriptGenerator {
                 out.write("# write values to file" + System.getProperty("line.separator"));
                 outputFile = StringUtils.modifyExtension(wavFiles[i], "shi" + System.getProperty("line.separator"));
                 out.write("fileappend \"shimmerOut.txt\" 'fn$''tab$''shim_local''tab$''shim_local_db''tab$''apq3''tab$''apq5''tab$''apq11''tab$''dda''newline$'" + System.getProperty("line.separator"));
-                //
                 
-                System.out.println("Added file " + String.valueOf(i+1) + " of " + String.valueOf(wavFiles.length));
+                System.out.println("Added jitter/shimmer file " + String.valueOf(i+1) + " of " + String.valueOf(wavFiles.length));
                 
                 out.write("select all" + System.getProperty("line.separator"));
                 out.write("Remove" + System.getProperty("line.separator"));
             }
-            
             out.close();
+            //
         }
     }
 
