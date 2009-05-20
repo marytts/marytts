cb95881f57e97abfe0b2635306e0ab821fb43eb4
Marc Schroeder <marc.schroeder@dfki.de>
2009-05-20 12:42:26 +0000

    maryserver scripts removed endorsed dirs, updated version of log4j jar
    git-svn-id: https://mary.opendfki.de/repos/trunk@1909 953a6561-930b-0410-b2a6-db37d1b2ae63

--- bin/maryserver.bat
+++ bin/maryserver.bat
@@ -37,6 +37,6 @@ set MARY_BASE=$INSTALL_PATH
 set SHPROT_BASE=%MARY_BASE%\lib\modules\shprot
 set PATH=%MARY_BASE%\lib\windows;%PATH%
 
-set CLASSPATH="%MARY_BASE%\java\mary-common.jar;%MARY_BASE%\java\log4j-1.2.8.jar"
-java -ea -Xms40m -Xmx512m -cp %CLASSPATH% "-Dmary.base=%MARY_BASE%" "-Djava.endorsed.dirs=%MARY_BASE%\lib\endorsed" %1 %2 %3 %4 %5 %6 %7 %8 %9 marytts.server.Mary
+set CLASSPATH="%MARY_BASE%\java\mary-common.jar;%MARY_BASE%\java\log4j-1.2.15.jar"
+java -ea -Xms40m -Xmx512m -cp %CLASSPATH% "-Dmary.base=%MARY_BASE%" %1 %2 %3 %4 %5 %6 %7 %8 %9 marytts.server.Mary
 
