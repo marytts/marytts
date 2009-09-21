d8d7d72f7894e0a5a096984babd60b46e1f3965a
Marc Schroeder <marc.schroeder@dfki.de>
2009-09-21 08:54:55 +0000

    elements of a future component installer
    git-svn-id: https://mary.opendfki.de/repos/trunk@2113 953a6561-930b-0410-b2a6-db37d1b2ae63

---
+++ java/marytts/tools/installvoices/VoiceUpdateListener.java
@@ -0,0 +1,23 @@
+/*
+ * VoiceUpdateListener.java
+ *
+ * Created on 21. September 2009, 10:27
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package marytts.tools.installvoices;
+
+/**
+ *
+ * @author marc
+ */
+public interface VoiceUpdateListener
+{
+    /**
+     * Take note of the fact that the current language has changed and the list of voices needs updating.
+     */
+    public void updateVoices(LanguageComponentDescription currentLanguage);
+    
+}
