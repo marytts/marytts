6cb817019eb9cfaad03dcba9758c6990d70d32a3
Marc Schroeder <marc.schroeder@dfki.de>
2009-09-23 18:06:21 +0000

    increasing max memory, in case people install many unit selection languages.
    workaround for "unknown protocol c" observed on Windows Vista. (needs testing)
    
    git-svn-id: https://mary.opendfki.de/repos/trunk@2140 953a6561-930b-0410-b2a6-db37d1b2ae63

--- bin/maryserver.bat
+++ bin/maryserver.bat
@@ -32,11 +32,15 @@
 ::#########################################################################
 :: Set the Mary base installation directory in an environment variable:
 set BINDIR=%~dp0%
-set MARY_BASE=%BINDIR%\..
 
-set SHPROT_BASE=%MARY_BASE%\lib\modules\shprot
+::set MARY_BASE=%BINDIR%\..
+
+:: Workaround for weird bug on Windows Vista, "unknown protocol c" somewhere in JTok:
+cd "%BINDIR%\.."
+set MARY_BASE=.
+
 set PATH=%MARY_BASE%\lib\windows;%PATH%
 
 set CLASSPATH="%MARY_BASE%\java\mary-common.jar;%MARY_BASE%\java\log4j-1.2.15.jar"
-java -ea -Xms40m -Xmx512m -cp %CLASSPATH% "-Dmary.base=%MARY_BASE%" %1 %2 %3 %4 %5 %6 %7 %8 %9 marytts.server.Mary
+java -ea -Xms40m -Xmx1g -cp %CLASSPATH% "-Dmary.base=%MARY_BASE%" %1 %2 %3 %4 %5 %6 %7 %8 %9 marytts.server.Mary
 
