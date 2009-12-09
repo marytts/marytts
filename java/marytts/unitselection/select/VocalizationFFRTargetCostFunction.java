ea92dff64b89b8f52a82e27497f36c0afa9ded08
Sathish Chandra Pammi <sach01@dfki.de>
2009-12-09 12:15:38 +0000

    a FFRTargetCostFunction for a backchannel
    git-svn-id: https://mary.opendfki.de/repos/trunk@2195 953a6561-930b-0410-b2a6-db37d1b2ae63

---
+++ java/marytts/unitselection/select/VocalizationFFRTargetCostFunction.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright 2006 DFKI GmbH.
+ * All Rights Reserved.  Use is subject to license terms.
+ *
+ * This file is part of MARY TTS.
+ *
+ * MARY TTS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package marytts.unitselection.select;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import marytts.features.FeatureDefinition;
+import marytts.features.FeatureProcessorManager;
+import marytts.features.FeatureVector;
+import marytts.features.TargetFeatureComputer;
+import marytts.features.VocalizationFeatureProcessorManager;
+import marytts.features.VocalizationTargetFeatureComputer;
+import marytts.nonverbal.BackchannelFeatureFileReader;
+import marytts.server.MaryProperties;
+import marytts.signalproc.display.Histogram;
+import marytts.unitselection.data.FeatureFileReader;
+import marytts.unitselection.data.Unit;
+import marytts.unitselection.weightingfunctions.WeightFunc;
+import marytts.unitselection.weightingfunctions.WeightFunctionManager;
+
+import org.apache.log4j.Logger;
+
+
+public class VocalizationFFRTargetCostFunction extends FFRTargetCostFunction 
+{
+        
+    public VocalizationFFRTargetCostFunction()
+    {
+    }
+
+
+    public void load(BackchannelFeatureFileReader ffr) //FeatureFileReader
+    throws IOException
+    {
+        this.featureDefinition = ffr.getFeatureDefinition();
+        this.featureVectors = ffr.getFeatureVectors();
+        
+        weightFunction = new WeightFunc[featureDefinition.getNumberOfContinuousFeatures()];
+        WeightFunctionManager wfm = new WeightFunctionManager();
+        int nDiscreteFeatures = featureDefinition.getNumberOfByteFeatures()+featureDefinition.getNumberOfShortFeatures();
+        for ( int i = 0; i < weightFunction.length; i++ ) {
+            String weightFunctionName = featureDefinition.getWeightFunctionName(nDiscreteFeatures+i);
+            if ( "".equals( weightFunctionName ) )
+                weightFunction[i] = wfm.getWeightFunction( "linear" );
+            else
+                weightFunction[i] = wfm.getWeightFunction(weightFunctionName);
+        }
+        
+        rememberWhichWeightsAreNonZero();
+    }
+
+
+    
+}
+
