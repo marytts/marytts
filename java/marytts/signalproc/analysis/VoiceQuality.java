54f5923b88b8e825f46933471fd8681bb449027e
Marcela Charfuelan <Marcela.Charfuelan@dfki.de>
2010-04-09 13:25:24 +0000

    VoiceQuality: a wrapper class for frame based voice quality parameters
    git-svn-id: https://mary.opendfki.de/repos/trunk@2313 953a6561-930b-0410-b2a6-db37d1b2ae63

--- java/marytts/signalproc/analysis/VoiceQuality.java
+++ java/marytts/signalproc/analysis/VoiceQuality.java
@@ -3,7 +3,13 @@ package marytts.signalproc.analysis;
 import java.io.IOException;
 
 import marytts.util.io.MaryRandomAccessFile;
+import marytts.util.MaryUtils;
 
+/**
+ * A wrapper class for frame based voice quality parameters
+ *
+ * @author Marcela Charfuelan
+ */
 public class VoiceQuality {
 
   public double [][] vq;
@@ -19,10 +25,46 @@ public class VoiceQuality {
       readVqFile(vqFile);
   }
   
-  public VoiceQuality(int numVqParams, int numFrames)
+  /**
+   * VoiceQuality object containing various vq mesures
+   * @param numVqParams number of vq parameters per frame
+   * @param Fs sampling rate
+   * @param skipSize skip size in seconds
+   * @param winSize window size in seconds
+   */
+  public VoiceQuality(int numVqParams, int Fs, float skipSize, float winSize)
   {
-     params = new VoiceQualityFileHeader();    
-     vq = new double[numVqParams][numFrames];
+     params = new VoiceQualityFileHeader();   
+     params.dimension = numVqParams;     
+     params.samplingRate = 16000;
+     params.skipsize = skipSize;  // in seconds
+     params.winsize = winSize;  // in seconds
+     
+  }
+  
+  public void allocate(int numFramesVq, double[][] par){
+    params.numfrm = numFramesVq;
+    vq = new double[params.dimension][numFramesVq];
+    for(int i=0; i<params.dimension; i++)
+      for(int j=0; j<numFramesVq; j++)
+        vq[i][j] = par[i][j];        
+    
+  }
+  
+  public void printPar(){
+    System.out.println("Features Read:\nframe\tOQG\tGOG\tSKG\tRCG\tIC");
+    for(int i=0; i<params.numfrm; i++)
+      System.out.format("%d\t%.3f %.3f %.3f %.3f %.3f \n", 
+          i+1, vq[0][i], vq[1][i], vq[2][i], vq[3][i], vq[4][i]); 
+  }
+  
+  public void printMeanStd(){
+    System.out.println("Mean +- Standard deviation:");
+    System.out.format("OQG: %.3f +- %.3f\n", MaryUtils.mean(vq[0]), MaryUtils.stdDev(vq[0]));
+    System.out.format("GOG: %.3f +- %.3f\n", MaryUtils.mean(vq[1]), MaryUtils.stdDev(vq[1]));
+    System.out.format("SKG: %.3f +- %.3f\n", MaryUtils.mean(vq[2]), MaryUtils.stdDev(vq[2]));
+    System.out.format("RCG: %.3f +- %.3f\n", MaryUtils.mean(vq[3]), MaryUtils.stdDev(vq[3]));
+    System.out.format("IC:  %.3f +- %.3f\n", MaryUtils.mean(vq[4]), MaryUtils.stdDev(vq[4]));
   }
   
   public void readVqFile(String vqFile)
@@ -92,12 +134,12 @@ public class VoiceQuality {
   {
       double[][] vqs = null;
       
-      if (stream!=null && params.numfrm>0 && params.dimension>0)
+      if (stream!=null && params.dimension>0 && params.dimension>0)
       {
-          vqs = new double[params.numfrm][];
+          vqs = new double[params.dimension][];
           
           for (int i=0; i<vqs.length; i++)
-              vqs[i] = stream.readDouble(params.dimension);
+              vqs[i] = stream.readDouble(params.numfrm);
           
           stream.close();
       }
