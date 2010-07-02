2cadf5a48edbef2391c71ffe95722fe1feaa9b6a
Ingmar Steiner <ingmar.steiner@inria.fr>
2010-07-02 15:11:08 +0000

    improved AbstractTimelineMaker to use FloatArrayDatagrams
    timeline files are now 50% smaller, writing is faster. DataFrame inner class is obsolete and has been removed again.
    
    
    git-svn-id: https://mary.opendfki.de/repos/branches/vq_unitselection@2412 953a6561-930b-0410-b2a6-db37d1b2ae63

--- java/marytts/tools/voiceimport/AbstractDataFile.java
+++ java/marytts/tools/voiceimport/AbstractDataFile.java
@@ -48,7 +48,7 @@ abstract class AbstractDataFile {
 
     protected int frameDuration;
 
-    protected ArrayList<DataFrame> dataFrames;
+    protected Datagram[] datagrams;
 
     /**
      * main constructor
@@ -87,111 +87,50 @@ abstract class AbstractDataFile {
     }
 
     /**
-     * get array of {@link Datagram}s constructed from the frames of this; no special requirements for their total duration
+     * get datagrams; no special requirements for their total duration
      * 
-     * @return Datagrams array
-     * @throws IOException
+     * @return datagrams
      */
-    public Datagram[] getDatagrams() throws IOException {
-        int expectedTotalDuration = frameDuration * numFrames;
-        return getDatagrams(expectedTotalDuration);
+    public Datagram[] getDatagrams() {
+        return datagrams;
     }
 
     /**
-     * get array of {@link Datagram}s constructed from the frames of this; if the total duration of all Datagrams is <i>longer</i>
-     * than <b>forcedDuration</b>, excess Datagrams will be silently dropped, and the duration of the last included one is
-     * shortened to match forcedDuration. If the total duration of all Datagrams is <i>shorter</i> than <b>forcedDuration</b>, a
-     * final "filler" Datagram is appended which contains no data, but whose duration increases the total duration to match
-     * <b>forcedDuration</b>.
+     * Get datagrams; if the total duration of all Datagrams is <i>longer</i> than <b>forcedDuration</b>, excess Datagrams will be
+     * silently dropped, and the duration of the last included one is shortened to match forcedDuration. If the total duration of
+     * all Datagrams is <i>shorter</i> than <b>forcedDuration</b>, a final "filler" Datagram is appended which contains no data,
+     * but whose duration increases the total duration to satisfy <b>forcedDuration</b>.
      * 
      * @param forcedDuration
      *            of all Datagrams (in samples)
-     * @return Datagrams array
-     * @throws IOException
+     * @return datagrams
      */
-    public Datagram[] getDatagrams(int forcedDuration) throws IOException {
+    public Datagram[] getDatagrams(int forcedDuration) {
         // initialize datagrams as a List:
-        ArrayList<Datagram> datagrams = new ArrayList<Datagram>(numFrames);
+        ArrayList<Datagram> datagramList = new ArrayList<Datagram>(numFrames);
         int durationMismatch = forcedDuration;
 
         // iterate over all data frames:
-        for (DataFrame dataFrame : dataFrames) {
-            durationMismatch -= dataFrame.duration;
+        for (Datagram datagram : datagrams) {
+            durationMismatch -= datagram.getDuration();
             if (durationMismatch < 0) {
                 // if forcedDuration is exceeded, adjust the final frame's duration and break out of loop:
-                dataFrame.duration += durationMismatch;
-                datagrams.add(dataFrame.toDatagram());
+                datagram.setDuration(datagram.getDuration() + durationMismatch);
+                datagramList.add(datagram);
                 break;
             }
-            datagrams.add(dataFrame.toDatagram());
+            datagramList.add(datagram);
         }
 
         // if total duration of all Datagrams is less than forcedDuration, pad with empty filler:
         if (durationMismatch > 0) {
             byte[] nothing = new byte[] {};
             Datagram filler = new Datagram(durationMismatch, nothing);
-            datagrams.add(filler);
+            datagramList.add(filler);
         }
 
         // return datagrams as array:
-        Datagram[] datagramArray = datagrams.toArray(new Datagram[datagrams.size()]);
+        Datagram[] datagramArray = datagramList.toArray(new Datagram[datagramList.size()]);
         return datagramArray;
-    }
-
-    /**
-     * Class to wrap a data frame and convert between this and a {@link Datagram}.
-     * 
-     * @author steiner
-     * 
-     */
-    protected class DataFrame {
-
-        private int duration;
-
-        private float[] data;
-
-        /**
-         * main constructor
-         * 
-         * @param duration
-         *            in samples
-         * @param data
-         *            in this frame
-         */
-        public DataFrame(int duration, float[] data) {
-            this.duration = duration;
-            this.data = data;
-        }
-
-        /**
-         * constructor to convert from a {@link Datagram}
-         * 
-         * @param datagram
-         *            to wrap
-         * @throws IOException
-         * @throws ClassNotFoundException
-         */
-        public DataFrame(Datagram datagram) throws IOException, ClassNotFoundException {
-            this.duration = (int) datagram.getDuration();
-            byte[] byteArray = datagram.getData();
-            ByteArrayInputStream inputStream = new ByteArrayInputStream(byteArray);
-            ObjectInputStream input = new ObjectInputStream(inputStream);
-            this.data = (float[]) input.readObject();
-        }
-
-        /**
-         * provide this data frame as a {@link Datagram}
-         * 
-         * @return Datagram
-         * @throws IOException
-         */
-        public Datagram toDatagram() throws IOException {
-            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-            ObjectOutputStream output = new ObjectOutputStream(outputStream);
-            output.writeObject(data);
-            byte[] byteArray = outputStream.toByteArray();
-            Datagram datagram = new Datagram((long) duration, byteArray);
-            return datagram;
-        }
     }
 }
