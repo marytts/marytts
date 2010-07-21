d028605bbcf17f87be5abc1b36375e18e2491044
Sathish Chandra Pammi <sach01@dfki.de>
2010-07-21 01:58:17 +0000

    Now HMMDurationF0Modeller consider 'prosody' element modifications like 'pitch', 'contour'. For better F0 modification, 'contour' modifications are calculated on first order polynomial fit. Though new methods are generic and reusable for other 'acoustic models', for some practical reasons I added to HMMDurationF0Modeller. We could easily add to other models as well.
    git-svn-id: https://mary.opendfki.de/repos/trunk@2451 953a6561-930b-0410-b2a6-db37d1b2ae63

--- java/marytts/modules/HMMDurationF0Modeller.java
+++ java/marytts/modules/HMMDurationF0Modeller.java
@@ -4,12 +4,17 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.Iterator;
 import java.util.Vector;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.sound.sampled.AudioInputStream;
 
@@ -42,7 +47,10 @@ import marytts.htsengine.HTSVocoder;
 import marytts.modules.synthesis.Voice;
 import marytts.server.MaryProperties;
 import marytts.util.MaryUtils;
+import marytts.util.dom.DomUtils;
 import marytts.util.dom.MaryDomUtils;
+import marytts.util.math.ArrayUtils;
+import marytts.util.math.Polynomial;
 import marytts.modules.synthesis.HMMSynthesizer;
 
 /***
@@ -154,12 +162,385 @@ public class HMMDurationF0Modeller extends InternalModule
     } else {      
       logger.debug("No HMM voice called " + hmmVoiceName);
     }
+    
     // the result is already in d
+    applyProsodySpecifications(doc);
+    
     return d; 
   }
- 
   
-  /** Parse Mary context features. 
+  /**
+   * A method to modify prosody modifications
+   * @param doc
+   */
+  private void applyProsodySpecifications(Document doc) {
+      
+      TreeWalker tw = MaryDomUtils.createTreeWalker(doc, doc, MaryXML.PHONE, MaryXML.BOUNDARY, MaryXML.PROSODY);
+      Element e = null;
+      
+      // TODO: read contours from back to start
+      while ((e = (Element) tw.nextNode()) != null) {
+          
+          if ( "prosody".equals(e.getNodeName() ) ) {
+              NodeList nl = e.getElementsByTagName("ph");
+              applyNewContourSpecifications(nl, e);
+          }
+      }      
+ }
+  
+  /**
+   * 
+   * @param nl
+   * @param prosodyElement
+   */
+  private void applyNewContourSpecifications(NodeList nl, Element prosodyElement) {
+      
+      double[] contour = getContiniousContour(nl);
+      contour = interpolateNonZeroValues(contour);
+      double[] coeffs     = Polynomial.fitPolynomial(contour, 1);
+      double[] polyValues = Polynomial.generatePolynomialValues(coeffs, 100, 0, 1);
+      double[] diffValues = new double[100];
+      
+      // Extract base contour from original contour
+      for ( int i=0; i < contour.length ; i++ ) {
+          diffValues[i] =  contour[i] - polyValues[i];
+      }
+      
+      String contourAttribute = null;
+      if ( prosodyElement.hasAttribute("contour") ) {
+          contourAttribute =  prosodyElement.getAttribute("contour");
+      }
+      
+      String pitchAttribute = null;
+      if ( prosodyElement.hasAttribute("pitch") ) {
+          pitchAttribute =  prosodyElement.getAttribute("pitch");
+      }
+      
+      polyValues = setBaseContourModifications(polyValues, contourAttribute, pitchAttribute);
+      
+      // Now, imposing back the diff. contour
+      for ( int i=0; i < contour.length ; i++ ) {
+          contour[i] =  diffValues[i] + polyValues[i];
+      }
+      
+      setModifiedContour(nl, contour);
+   
+      return;         
+  }
+  
+  
+  /**
+   * To set new modified contour into XML
+   * @param nl
+   * @param contour
+   */
+  private void setModifiedContour(NodeList nl, double[] contour) {
+      
+      Element firstElement =  (Element) nl.item(0);
+      Element lastElement =  (Element) nl.item(nl.getLength()-1);
+      
+      double fEnd = (new Double(firstElement.getAttribute("end"))).doubleValue();
+      double fDuration = 0.001 * (new Double(firstElement.getAttribute("d"))).doubleValue();
+      double lEnd = (new Double(lastElement.getAttribute("end"))).doubleValue();
+      double fStart = fEnd - fDuration; // 'prosody' tag starting point
+      double duration = lEnd - fStart;  // duaration of 'prosody' modification request
+      
+      Map<Integer, Integer> f0Map;
+      
+      for ( int i=0; i < nl.getLength(); i++ ) {
+          
+          Element e = (Element) nl.item(i);
+          String f0Attribute = e.getAttribute("f0");
+          
+          if( f0Attribute == null || "".equals(f0Attribute) ) {
+              continue;
+          }
+          
+          double phoneEndTime       = (new Double(e.getAttribute("end"))).doubleValue();
+          double phoneDuration = 0.001 * (new Double(e.getAttribute("d"))).doubleValue();
+          
+          Pattern p = Pattern.compile("(\\d+,\\d+)");
+          
+          // Split input with the pattern
+          Matcher m = p.matcher(e.getAttribute("f0"));
+          String setF0String = "";
+          while ( m.find() ) {
+              String[] f0Values = (m.group().trim()).split(",");
+              Integer percent = new Integer(f0Values[0]);
+              Integer f0Value = new Integer(f0Values[1]);
+              double partPhone = phoneDuration * (percent.doubleValue()/100.0);
+              
+              int placeIndex  = (int) Math.floor((( ((phoneEndTime - phoneDuration) - fStart ) +  partPhone ) * 100 ) / (double) duration );
+              setF0String = setF0String + "(" + percent + "," +(int) contour[placeIndex] + ")" ;          
+              
+          }
+        
+          e.setAttribute("f0", setF0String);
+      }    
+}
+
+  
+  
+  /**
+   * Set modifications to base contour (first order polynomial fit contour) 
+   * @param polyValues
+   * @param contourAttribute
+   * @param pitchAttribute
+   * @return
+   */
+  private double[] setBaseContourModifications(double[] polyValues, String contourAttribute, String pitchAttribute) {
+      
+      if(pitchAttribute != null && !"".equals(pitchAttribute) ) {
+          polyValues = setPitchSpecifications( polyValues, pitchAttribute );
+      }
+      
+      if(contourAttribute != null && !"".equals(contourAttribute) ) {
+          polyValues = setContourSpecifications( polyValues, contourAttribute );
+      }
+      
+      return polyValues;
+  }
+  
+  
+  /**
+   * Set all specifications to original contour
+   * @param polyValues
+   * @param contourAttribute
+   * @return
+   */
+  private double[] setContourSpecifications(double[] polyValues, String contourAttribute) {
+   
+      Map<String, String> f0Specifications = getContourSpecifications(contourAttribute);
+      Iterator<String> it =  f0Specifications.keySet().iterator();
+      double[] modifiedF0Values = new double[100];
+      Arrays.fill(modifiedF0Values, 0.0);
+      
+      if ( polyValues.length != modifiedF0Values.length ) {
+          throw new RuntimeException("The lengths of two arrays are not same!");
+      }
+      
+      modifiedF0Values[0] = polyValues[0];
+      modifiedF0Values[ modifiedF0Values.length - 1 ] = polyValues[ modifiedF0Values.length - 1 ];
+      
+      while ( it.hasNext() ) {
+      
+          String percent = it.next();
+          String f0Value = f0Specifications.get( percent );
+          
+          int percentDuration  =  (new Integer(percent.substring(0, percent.length()-1))).intValue();
+          
+          //System.out.println( percent  + " " + f0Value );
+          
+          if ( f0Value.startsWith("+") ) {
+              if ( f0Value.endsWith("%") ) {
+                  double f0Mod = (new Double(f0Value.substring( 1, f0Value.length()-1 ))).doubleValue();
+                  modifiedF0Values[percentDuration] = polyValues[percentDuration] + (polyValues[percentDuration] * (f0Mod / 100.0));
+              }
+              else if ( f0Value.endsWith("Hz") ) {
+                  int f0Mod = (new Integer(f0Value.substring( 1, f0Value.length()-2 ))).intValue();
+                  modifiedF0Values[percentDuration] = polyValues[percentDuration] + f0Mod ;
+              }
+          }
+          else if ( f0Value.startsWith("-") ) {
+              if ( f0Value.endsWith("%") ) {
+                  double f0Mod = (new Double(f0Value.substring( 1, f0Value.length()-1 ))).doubleValue();
+                  modifiedF0Values[percentDuration] = polyValues[percentDuration] - (polyValues[percentDuration] * (f0Mod / 100.0));
+              
+              }
+              else if ( f0Value.endsWith("Hz") ) {
+                  int f0Mod = (new Integer(f0Value.substring( 1, f0Value.length()-2 ))).intValue();
+                  modifiedF0Values[percentDuration] = polyValues[percentDuration] - f0Mod ;
+              }
+          }        
+      }
+      
+    modifiedF0Values = interpolateNonZeroValues(modifiedF0Values);
+      
+    return modifiedF0Values;
+  
+  }
+
+  
+  /**
+   * set pitch specifications: 
+   * Ex: pitch="+20%" or pitch="+50Hz"
+   * @param polyValues
+   * @param pitchAttribute
+   * @return
+   */
+  private double[] setPitchSpecifications(double[] polyValues, String pitchAttribute) {
+    
+      boolean positivePitch = pitchAttribute.startsWith("+");
+      double modificationPitch = (new Integer(pitchAttribute.substring(1, pitchAttribute.length()-1))).doubleValue();
+      
+      if ( pitchAttribute.startsWith("+") ) {
+          if ( pitchAttribute.endsWith("%") ) {
+              for ( int i=0; i < polyValues.length; i++ ) {
+                  polyValues[i] = polyValues[i] + (polyValues[i] * (modificationPitch / 100.0));
+              }
+          }
+          else if ( pitchAttribute.endsWith("Hz") ) {
+              for ( int i=0; i < polyValues.length; i++ ) {
+                  polyValues[i] = polyValues[i] + modificationPitch;
+              }
+          }
+      }
+      else if ( pitchAttribute.startsWith("-") ) {
+          if ( pitchAttribute.endsWith("%") ) {
+              for ( int i=0; i < polyValues.length; i++ ) {
+                  polyValues[i] = polyValues[i] - (polyValues[i] * (modificationPitch / 100.0));
+              }
+          }
+          else if ( pitchAttribute.endsWith("Hz") ) {
+              for ( int i=0; i < polyValues.length; i++ ) {
+                  polyValues[i] = polyValues[i] - modificationPitch;
+              }
+          }
+      }
+      
+    return polyValues;
+    
+  }
+
+  
+  /**
+   * to get contour specifications into MAP
+   * @param attribute
+   * @return
+   */
+  private Map<String, String> getContourSpecifications(String attribute) {
+      
+      Map<String, String> f0Map = new HashMap<String, String>();
+      Pattern p = Pattern.compile("(\\d+%,[+|-]\\d+[%|Hz])");
+      
+      // Split input with the pattern
+      Matcher m = p.matcher(attribute);
+      while ( m.find() ) {
+          //System.out.println(m.group());
+          String[] f0Values = (m.group().trim()).split(",");
+          f0Map.put(f0Values[0], f0Values[1]);
+      }
+      return f0Map;
+  }
+
+  /**
+   * To interpolate Zero values with respect to NonZero values
+   * @param contour
+   * @return
+   */
+  private double[] interpolateNonZeroValues( double[] contour ) {
+      
+      for ( int i=0; i < contour.length ; i++ ) {
+          if ( contour[i] == 0 ) {
+              int index = findNextIndexNonZero( contour, i );
+              //System.out.println("i: "+i+"index: "+index);
+              if( index == -1 ) {
+                  for ( int j=i; j < contour.length; j++ ) {
+                      contour[j] = contour[j-1];
+                  }
+                  break;
+              }
+              else {
+                  for ( int j=i; j < index; j++ ) {
+                      //contour[j] = contour[i-1] * (index - j) + contour[index] * (j - (i-1)) / ( index - i );
+                      contour[j] = contour[j-1] + ((contour[index] - contour[i-1]) / (index - i)) ;
+                  }
+                  i = index-1; 
+              }
+          }
+      }
+      
+      return contour;
+  }
+  
+  /**
+   * To find next NonZero index
+   * @param contour
+   * @param current
+   * @return
+   */
+  private int findNextIndexNonZero(double[] contour, int current) {
+      for ( int i=current+1; i < contour.length ; i++ ) {
+          if ( contour[i] != 0 ) {
+              return i;
+          }
+      }
+     return -1;
+  }
+
+  
+  /**
+   * get Continuous contour from "ph" nodelist
+   * @param nl
+   * @return
+   */
+  private double[] getContiniousContour(NodeList nl) {
+    
+
+    Element firstElement =  (Element) nl.item(0);
+    Element lastElement =  (Element) nl.item(nl.getLength()-1);
+    
+    double[] contour = new double[100]; // Assume contour has 100 frames
+    Arrays.fill(contour, 0.0);
+    
+    double fEnd = (new Double(firstElement.getAttribute("end"))).doubleValue();
+    double fDuration = 0.001 * (new Double(firstElement.getAttribute("d"))).doubleValue();
+    double lEnd = (new Double(lastElement.getAttribute("end"))).doubleValue();
+    double fStart = fEnd - fDuration; // 'prosody' tag starting point
+    double duration = lEnd - fStart;  // duaration of 'prosody' modification request
+    
+    Map<Integer, Integer> f0Map;
+    
+    for ( int i=0; i < nl.getLength(); i++ ) {
+        Element e = (Element) nl.item(i);
+        String f0Attribute = e.getAttribute("f0");
+        
+        if( f0Attribute == null || "".equals(f0Attribute) ) {
+            continue;
+        }
+        
+        double phoneEndTime       = (new Double(e.getAttribute("end"))).doubleValue();
+        double phoneDuration = 0.001 * (new Double(e.getAttribute("d"))).doubleValue();
+        //double localStartTime = endTime - phoneDuration;
+        
+        f0Map = getPhoneF0Data(e.getAttribute("f0"));
+        
+        Iterator<Integer> it =  f0Map.keySet().iterator();
+        while(it.hasNext()){
+            Integer percent = it.next();
+            Integer f0Value = f0Map.get(percent);
+            double partPhone = phoneDuration * (percent.doubleValue()/100.0);
+            int placeIndex  = (int) Math.floor((( ((phoneEndTime - phoneDuration) - fStart ) +  partPhone ) * 100 ) / (double) duration );
+            contour[placeIndex] = f0Value.doubleValue();
+        }
+    }
+    
+    return contour;
+  }
+
+  /**
+   * Get f0 specifications in HashMap
+   * @param attribute
+   * @return
+   */
+  private Map<Integer, Integer> getPhoneF0Data(String attribute) {
+    
+    Map<Integer, Integer> f0Map = new HashMap<Integer, Integer>();
+    Pattern p = Pattern.compile("(\\d+,\\d+)");
+    
+    // Split input with the pattern
+    Matcher m = p.matcher(attribute);
+    while ( m.find() ) {
+        String[] f0Values = (m.group().trim()).split(",");
+        f0Map.put(new Integer(f0Values[0]), new Integer(f0Values[1]));
+    }
+
+    //attribute.split(regex)
+    return f0Map;
+    
+  }
+
+/** Parse Mary context features. 
    * For each triphone model in the file, it creates a Model object in a linked list of 
    * Model objects -> UttModel um 
    * It also estimates state duration from state duration model (Gaussian).
