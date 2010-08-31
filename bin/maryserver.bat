7707dbddad8c7b23cd6c99320b5a9b7b7480b3c2
Marc Schroeder <marc.schroeder@dfki.de>
2010-08-31 19:22:26 +0000

    Fix for startup scripts in Windows.
    git-svn-id: https://mary.opendfki.de/repos/trunk@2635 953a6561-930b-0410-b2a6-db37d1b2ae63

--- bin/maryserver.bat
+++ bin/maryserver.bat
@@ -42,5 +42,5 @@ set MARY_BASE=.
 set PATH=%MARY_BASE%\lib\windows;%PATH%
 
 set CLASSPATH="%MARY_BASE%\java\mary-common.jar;%MARY_BASE%\java\log4j-1.2.15.jar"
-java -ea -Xms40m -Xmx1g -cp %CLASSPATH% "-Dmary.base=%MARY_BASE%" "%*" marytts.server.Mary
+java -ea -Xms40m -Xmx1g -cp %CLASSPATH% "-Dmary.base=%MARY_BASE%" marytts.server.Mary
 
