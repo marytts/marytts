ff087a307a1b6bb971d0b509518d12f6edaf9897
Marc Schroeder <marc.schroeder@dfki.de>
2010-08-31 11:55:39 +0000

    Moved signal processing unit tests to marytts.tests.junit4, and updated them to use JUnit 4 syntax.
    git-svn-id: https://mary.opendfki.de/repos/trunk@2628 953a6561-930b-0410-b2a6-db37d1b2ae63

--- java/marytts/tests/junit4/FrameOverlapAddTest.java
+++ java/marytts/tests/junit4/FrameOverlapAddTest.java
@@ -17,14 +17,14 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
-package marytts.signalproc.tests;
+package marytts.tests.junit4;
 
-import java.awt.Color;
 
-import junit.framework.TestCase;
-import marytts.signalproc.display.FunctionGraph;
-import marytts.signalproc.display.SignalGraph;
-import marytts.signalproc.process.PhaseVocoder;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import marytts.signalproc.process.FrameOverlapAddSource;
+import marytts.signalproc.process.NaiveVocoder;
 import marytts.util.data.BufferedDoubleDataSource;
 import marytts.util.math.MathUtils;
 
@@ -32,33 +32,27 @@ import marytts.util.math.MathUtils;
  * @author Marc Schr&ouml;der
  *
  */
-public class PhaseVocoderTest extends TestCase
+public class FrameOverlapAddTest
 {
-
+    @Test
     public void testIdentity()
     {
         double[] signal =  FFTTest.getSampleSignal(16000);
         int samplingRate = 8000;
-        PhaseVocoder pv = new PhaseVocoder(new BufferedDoubleDataSource(signal), samplingRate, 1);
-        double[] result = pv.getAllData();
+        FrameOverlapAddSource ola = new FrameOverlapAddSource(new BufferedDoubleDataSource(signal), 2048, samplingRate, null);
+        double[] result = ola.getAllData();
         double err = MathUtils.sumSquaredError(signal, result);
-        if (err > 1.E-20) {
-            SignalGraph graph = new SignalGraph(signal, 16000);
-            graph.addDataSeries(result, Color.RED, FunctionGraph.DRAW_LINE, -1);
-            graph.showInJFrame("Test signal", true, true);
-            try {Thread.sleep(100000);} catch(Exception e) {}
-        }
-        assertTrue("Error: "+err, err<1.E-15);
+        assertTrue("Error: "+err, err<1.E-19);
     }
 
     public void testStretch1()
     {
-        double[] signal =  FFTTest.getSampleSignal(16000);
+        double[] signal =  FFTTest.getSampleSignal(2048+128);
         int samplingRate = 8000;
         double rateFactor = 0.5;
-        PhaseVocoder pv = new PhaseVocoder(new BufferedDoubleDataSource(signal), samplingRate, rateFactor);
-        double[] result = pv.getAllData();
-        int expectedLength = pv.computeOutputLength(signal.length);
+        NaiveVocoder nv = new NaiveVocoder(new BufferedDoubleDataSource(signal), samplingRate, rateFactor);
+        double[] result = nv.getAllData();
+        int expectedLength = nv.computeOutputLength(signal.length);
         assertTrue("Expected result length: " + expectedLength +", found: "+result.length,
                 result.length == expectedLength);
     }
@@ -68,13 +62,13 @@ public class PhaseVocoderTest extends TestCase
         double[] signal =  FFTTest.getSampleSignal(16000);
         int samplingRate = 8000;
         double rateFactor = 0.5;
-        PhaseVocoder pv = new PhaseVocoder(new BufferedDoubleDataSource(signal), samplingRate, rateFactor);
-        double[] result = pv.getAllData();
+        NaiveVocoder nv = new NaiveVocoder(new BufferedDoubleDataSource(signal), samplingRate, rateFactor);
+        double[] result = nv.getAllData();
         double meanSignalEnergy = MathUtils.mean(MathUtils.multiply(signal, signal));
         double meanResultEnergy = MathUtils.mean(MathUtils.multiply(result, result));
         double percentDifference = Math.abs(meanSignalEnergy-meanResultEnergy)/meanSignalEnergy*100;
         assertTrue("Stretching changed signal energy by  "+percentDifference+"%",
-                percentDifference<2);
+                percentDifference<6);
     }
 
 }
