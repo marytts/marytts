20ddb428b4f7fa80636ccf40fde92836418bb24c
Sathish Chandra Pammi <sach01@dfki.de>
2010-12-07 12:48:49 +0000

    A new way of vocalization selection to impose intonation contours on unit candidates. Two extra steps implemented here:
    1. precondition:
    
    formula : for all CC(j) <= CCmax  [where j1, j2, j3 ... are contour candidates]    
    	 CCmax (threshold) = min (CC(i1), CC(i2), CC(i3)....) [where i1, i2, i3 .. are unit candidates]
    
    2. New cost mechanism for impose intonation contours
    
    NEWCOST = (contourCost * contourCostWeight) + ( mergeCost * (1 - contourCostWeight))
    
    Here, Contour Cost = polynomial contour distance between two units 
    
    MergeCost = segmentalformCost(u(i)) + intonationCost(c(i)) +  voiceQualityCost(u(i)) + 0.5 * (meaningCost(u(i)) + meaningCost(c(i)) )
    Here u(i)  -> unit candidates
         c(i)  -> contour candidates
    
    
    Two extra parameters added to enable above steps: 
    
    voice.<voice name>.vocalization.usePrecondition = true
    voice.<voice name>.vocalization.contourCostWeight = 0.02
    
    
    
    git-svn-id: https://mary.opendfki.de/repos/trunk@2781 953a6561-930b-0410-b2a6-db37d1b2ae63

--- java/marytts/vocalizations/VocalizationSelector.java
+++ java/marytts/vocalizations/VocalizationSelector.java
@@ -23,6 +23,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.Arrays;
 import java.util.List;
 
@@ -35,6 +36,7 @@ import marytts.unitselection.data.Unit;
 import marytts.unitselection.select.Target;
 import marytts.unitselection.select.VocalizationFFRTargetCostFunction;
 import marytts.util.MaryUtils;
+import marytts.util.math.MathUtils;
 import marytts.util.math.Polynomial;
 
 import org.apache.log4j.Level;
@@ -53,9 +55,13 @@ public class VocalizationSelector {
     protected FeatureDefinition f0FeatureDefinition;
     protected VocalizationIntonationReader vIntonationReader;
     protected VocalizationUnitFileReader unitFileReader;
-    protected VocalizationFFRTargetCostFunction vffrtCostFunction = null;
-    protected VocalizationFFRTargetCostFunction vffrtIntonationCostFunction = null;
+    protected VocalizationFFRTargetCostFunction vffrtUnitCostFunction = null;
+    protected VocalizationFFRTargetCostFunction vffrtContourCostFunction = null;
     protected boolean f0ContourImposeSupport;
+    protected boolean usePrecondition;
+    protected double contourCostWeight;
+    private DecimalFormat df; 
+
     protected int noOfSuitableUnits = 1;
     
     protected Logger logger = MaryUtils.getLogger("Vocalization Selector");
@@ -66,13 +72,12 @@ public class VocalizationSelector {
         String featureFile  = MaryProperties.getFilename("voice."+voice.getName()+".vocalization.featurefile");
         String featureDefinitionFile  = MaryProperties.getFilename("voice."+voice.getName()+".vocalization.featureDefinitionFile");
         f0ContourImposeSupport = MaryProperties.getBoolean("voice."+voice.getName()+".f0ContourImposeSupport", false);
-        
+        df = new DecimalFormat("##.##");
         try {
             BufferedReader fDBufferedReader = new BufferedReader( new FileReader( new File(featureDefinitionFile)));
             this.featureDefinition = new FeatureDefinition(fDBufferedReader, true);
             this.featureFileReader = new VocalizationFeatureFileReader(featureFile);
-            vffrtCostFunction = new VocalizationFFRTargetCostFunction();
-            vffrtCostFunction.load(this.featureFileReader, this.featureDefinition);
+            vffrtUnitCostFunction = new VocalizationFFRTargetCostFunction(this.featureFileReader, this.featureDefinition);
             unitFileReader = new VocalizationUnitFileReader(unitFileName);
 
             if(this.featureFileReader.getNumberOfUnits() != this.unitFileReader.getNumberOfUnits()) {
@@ -82,12 +87,16 @@ public class VocalizationSelector {
             if ( this.f0ContourImposeSupport ) {
                 String intonationFDFile = MaryProperties.getFilename("voice."+voice.getName()+".vocalization.intonation.featureDefinitionFile");
                 String intonationFile = MaryProperties.getFilename("voice."+voice.getName()+".vocalization.intonationfile");
+                usePrecondition = MaryProperties.getBoolean("voice."+voice.getName()+".vocalization.usePrecondition", false);
+                contourCostWeight = (new Double(MaryProperties.getProperty("voice."+voice.getName()+".vocalization.contourCostWeight", "0.5"))).doubleValue();
+                if ( contourCostWeight < 0 || contourCostWeight > 1.0) {
+                    throw new MaryConfigurationException("contourCostWeight should be between 0 and 1");
+                }
                 BufferedReader f0FDBufferedReader = new BufferedReader( new FileReader( new File(intonationFDFile)));
                 f0FeatureDefinition = new FeatureDefinition(f0FDBufferedReader, true);
                 vIntonationReader = new VocalizationIntonationReader(intonationFile);
                 noOfSuitableUnits = MaryProperties.getInteger("voice."+voice.getName()+".vocalization.intonation.numberOfSuitableUnits");
-                vffrtIntonationCostFunction = new VocalizationFFRTargetCostFunction();
-                vffrtIntonationCostFunction.load(this.featureFileReader, this.f0FeatureDefinition);
+                vffrtContourCostFunction = new VocalizationFFRTargetCostFunction(this.featureFileReader, this.f0FeatureDefinition);
             }
         }
         catch (IOException e) {
@@ -112,23 +121,91 @@ public class VocalizationSelector {
     public SourceTargetPair getBestCandidatePairtoImposeF0(Element domElement) {
         
         
-        //Target targetF0Unit = createIntonationTarget(domElement);
         VocalizationCandidate[] vCosts = getBestMatchingCandidates(domElement);
         VocalizationCandidate[] vIntonationCosts = getBestIntonationCandidates(domElement);
         
+        int noOfSuitableF0Units;
+        if (usePrecondition) {
+            noOfSuitableF0Units = getNumberContoursAboveThreshold(vCosts, vIntonationCosts);
+        } else {
+            noOfSuitableF0Units = noOfSuitableUnits;
+        }
+        
+        if ( noOfSuitableF0Units == 0 ) {
+            return new SourceTargetPair(vCosts[0].unitIndex, vCosts[0].unitIndex, 0);
+        }
+        
         VocalizationCandidate[] suitableCandidates = new VocalizationCandidate[noOfSuitableUnits];
         System.arraycopy(vCosts, 0, suitableCandidates, 0, noOfSuitableUnits);
-        VocalizationCandidate[] suitableF0Candidates = new VocalizationCandidate[noOfSuitableUnits];
-        System.arraycopy(vIntonationCosts, 0, suitableF0Candidates, 0, noOfSuitableUnits);
-        
+        VocalizationCandidate[] suitableF0Candidates = new VocalizationCandidate[noOfSuitableF0Units];
+        System.arraycopy(vIntonationCosts, 0, suitableF0Candidates, 0, noOfSuitableF0Units);
+         
         Target targetUnit =  createTarget(domElement);
         if (logger.getEffectiveLevel().equals(Level.DEBUG)) {
             debugLogCandidates(targetUnit, suitableCandidates, suitableF0Candidates);
         }
         
-        SourceTargetPair[] sortedImposeF0Data = vocalizationF0DistanceComputer(suitableCandidates, suitableF0Candidates);
+        SourceTargetPair[] sortedImposeF0Data = vocalizationF0DistanceComputer(suitableCandidates, suitableF0Candidates, domElement);
         
         return sortedImposeF0Data[0];
+    }
+    
+    /**
+     * compute a threshold according to precondition and 
+     * get number of contours above computed threshold
+     * 
+     * formula : for all CC(j) <= CCmax 
+     * where j1, j2, j3 ... are contour candidates    
+     * CCmax (threshold) = min (CC(i1), CC(i2), CC(i3)....)
+     * where i1, i2, i3 .. are unit candidates
+     * 
+     * @param vCosts VocalizationCandidates
+     * @param vIntonationCosts VocalizationF0Candidates
+     * @return int number of contours above computed threshold 
+     */
+    private int getNumberContoursAboveThreshold(VocalizationCandidate[] vCosts, VocalizationCandidate[] vIntonationCosts) {
+        
+        // get minimum cc cost for all units 
+        double[] costs = new double[noOfSuitableUnits];
+        for ( int i=0; i<costs.length; i++ ) {
+            int unitIndex = vCosts[i].unitIndex;
+            VocalizationCandidate contourCandidate =  getCandidateByIndex(vIntonationCosts, unitIndex);
+            if ( contourCandidate != null ) {
+                costs[i] = contourCandidate.cost;
+            }
+            else {
+                costs[i] = Double.MAX_VALUE;
+            }
+        }
+        
+        double threshold = MathUtils.min(costs);
+        int contourSetSize = 0;
+        for ( int i=0; i<vIntonationCosts.length; i++ ) {
+            if (vIntonationCosts[i].cost <= threshold) {
+                contourSetSize = i+1;
+            }
+            else {
+                break;
+            }
+        }
+        
+        return contourSetSize;
+    }
+    
+    /**
+     * search candidate by unitindex
+     * @param vIntonationCosts VocalizationF0Candidates
+     * @param unitIndex unitindex
+     * @return VocalizationCandidate of given index
+     */
+    private VocalizationCandidate getCandidateByIndex(VocalizationCandidate[] vIntonationCosts, int unitIndex) {
+        
+        for (int i=0; i < vIntonationCosts.length; i++ ) {
+            if ( vIntonationCosts[i].unitIndex == unitIndex ) {
+                return vIntonationCosts[i];
+            }
+        }
+        return null;
     }
     
     /**
@@ -138,7 +215,7 @@ public class VocalizationSelector {
      * @return an array of candidate pairs
      */
     private SourceTargetPair[] vocalizationF0DistanceComputer(VocalizationCandidate[] suitableCandidates,
-            VocalizationCandidate[] suitableF0Candidates) {
+            VocalizationCandidate[] suitableF0Candidates, Element domElement ) {
         
         int noPossibleImpositions = suitableCandidates.length * suitableF0Candidates.length;
         SourceTargetPair[] imposeF0Data = new SourceTargetPair[noPossibleImpositions];
@@ -148,26 +225,80 @@ public class VocalizationSelector {
             for ( int j=0; j < suitableF0Candidates.length; j++ ) {
                 int sourceIndex = suitableCandidates[i].unitIndex;
                 int targetIndex = suitableF0Candidates[j].unitIndex;
-                double distance;
-                if ( targetIndex == sourceIndex ) {
-                    distance = 0;
-                }
-                else {
-                    double[] targetCoeffs = vIntonationReader.getIntonationCoeffs(targetIndex);
-                    double[] sourceCoeffs = vIntonationReader.getIntonationCoeffs(sourceIndex);
-                    if (targetCoeffs != null && sourceCoeffs != null && targetCoeffs.length == sourceCoeffs.length) {
-                        distance = Polynomial.polynomialDistance(sourceCoeffs, targetCoeffs);
-                    } else {
-                        distance = Double.MAX_VALUE;
-                    }
-                }
-                imposeF0Data[count++] = new SourceTargetPair(sourceIndex, targetIndex, distance);
+                
+                double contourCost = getContourCostDistance(sourceIndex, targetIndex);
+                double mergeCost =  getMergeCost(sourceIndex, targetIndex, domElement );
+                double cost = (contourCost * contourCostWeight) + ( mergeCost * (1 - contourCostWeight)) ;
+                logger.debug("Unit Index "+sourceIndex+ " & Contour Index " +targetIndex+ " :: Countour cost: "+df.format(contourCost)
+                        +" + Merge Cost: "+df.format(mergeCost)+" --> TotalCost: "+df.format(cost));
+                imposeF0Data[count++] = new SourceTargetPair(sourceIndex, targetIndex, cost);
             }
         }
         
         Arrays.sort(imposeF0Data);
         
         return imposeF0Data;
+    }
+    
+    /**
+     * Compute mergeCost for unit and contour candidates
+     * Formula = segmentalformCost(u(i)) + intonationCost(c(i)) +  voiceQualityCost(u(i)) 
+     *              + 0.5 * (meaningCost(u(i)) + meaningCost(c(i)) )
+     * @param sourceIndex unit index
+     * @param targetIndex unit index
+     * @return double merge cost  
+     */
+    private double getMergeCost(int sourceIndex, int targetIndex, Element domElement ) {
+        
+        Target targetUnit = createTarget(domElement);
+        Target targetContour = createIntonationTarget(domElement);
+        
+        Unit unitCandidate = this.unitFileReader.getUnit(sourceIndex);
+        Unit contourCandidate = this.unitFileReader.getUnit(targetIndex);
+        
+        // unit features
+        double segmentalCost = vffrtUnitCostFunction.featureCost(targetUnit, unitCandidate, "name");
+        double voiceQualityCost = vffrtUnitCostFunction.featureCost(targetUnit, unitCandidate, "voicequality");
+        double meaningUnitCost = 0;
+        String[] meaningFeatureNames = vffrtUnitCostFunction.getFeatureDefinition().getContinuousFeatureNameArray();
+        for ( int i=0; i < meaningFeatureNames.length; i++ ) {
+            meaningUnitCost += vffrtUnitCostFunction.featureCost(targetUnit, unitCandidate, meaningFeatureNames[i]);
+        }
+        
+        // contour features
+        double intonationCost = this.vffrtContourCostFunction.featureCost(targetContour, contourCandidate, "intonation");
+        double meaningContourCost = 0;
+        String[] meaningContourFeatures = vffrtContourCostFunction.getFeatureDefinition().getContinuousFeatureNameArray();
+        for ( int i=0; i < meaningContourFeatures.length; i++ ) {
+            meaningContourCost += vffrtContourCostFunction.featureCost(targetContour, contourCandidate, meaningContourFeatures[i]);
+        }
+            
+        double mergeCost = segmentalCost +  voiceQualityCost + intonationCost + 0.5 * (meaningUnitCost + meaningContourCost);
+        
+        return mergeCost;
+    }
+
+    /**
+     * compute contour distance (polynomial distance)
+     * @param sourceIndex unit index
+     * @param targetIndex unit index
+     * @return polynomial distance measure
+     */
+    private double getContourCostDistance(int sourceIndex, int targetIndex) {
+        double distance;
+        if ( targetIndex == sourceIndex ) {
+            distance = 0;
+        }
+        else {
+            double[] targetCoeffs = vIntonationReader.getIntonationCoeffs(targetIndex);
+            double[] sourceCoeffs = vIntonationReader.getIntonationCoeffs(sourceIndex);
+            if (targetCoeffs != null && sourceCoeffs != null && targetCoeffs.length == sourceCoeffs.length) {
+                distance = Polynomial.polynomialDistance(sourceCoeffs, targetCoeffs);
+            } else {
+                distance = Double.MAX_VALUE;
+            }
+        }
+        return distance;
     }
 
     /**
@@ -183,7 +314,7 @@ public class VocalizationSelector {
         int index = 0;
         for( int i=0; i<numberUnits; i++ ) {
             Unit singleUnit = this.unitFileReader.getUnit(i);
-            double cost = vffrtCostFunction.cost(targetUnit, singleUnit);
+            double cost = vffrtUnitCostFunction.cost(targetUnit, singleUnit);
             if( cost < minCost ) {
                 minCost = cost;
                 index = i;
@@ -205,7 +336,7 @@ public class VocalizationSelector {
         VocalizationCandidate[] vocalizationCandidates = new VocalizationCandidate[numberUnits];
         for( int i=0; i<numberUnits; i++ ) {
             Unit singleUnit = this.unitFileReader.getUnit(i);
-            double cost = vffrtCostFunction.cost(targetUnit, singleUnit);
+            double cost = vffrtUnitCostFunction.cost(targetUnit, singleUnit);
             vocalizationCandidates[i] = new VocalizationCandidate(i,cost);
         }
         Arrays.sort(vocalizationCandidates);
@@ -226,7 +357,7 @@ public class VocalizationSelector {
         int fiName = fd.getFeatureIndex("name");
         int fiIntonation = fd.getFeatureIndex("intonation");
         int fiVQ = fd.getFeatureIndex("voicequality");
-        for (int i=0; i<noOfSuitableUnits; i++) {
+        for (int i=0; i<suitableCandidates.length; i++) {
             int unitIndex = suitableCandidates[i].unitIndex;
             double unitCost = suitableCandidates[i].cost; 
             FeatureVector fv = featureFileReader.getFeatureVector(unitIndex);
@@ -251,9 +382,9 @@ public class VocalizationSelector {
             }
             logger.debug(sb.toString());
         }
-        for (int i=0; i<noOfSuitableUnits; i++) {
+        for (int i=0; i<suitableF0Candidates.length; i++) {
             int unitIndex = suitableF0Candidates[i].unitIndex;
-            double unitCost = suitableCandidates[i].cost;
+            double unitCost = suitableF0Candidates[i].cost;
             FeatureVector fv = featureFileReader.getFeatureVector(unitIndex);
             StringBuilder sb = new StringBuilder();
             sb.append("F0 Candidate ").append(i).append(": ").append(unitIndex).append(" ( "+unitCost+" ) ").append(" -- ");
@@ -291,7 +422,7 @@ public class VocalizationSelector {
         VocalizationCandidate[] vocalizationCandidates = new VocalizationCandidate[numberUnits];
         for( int i=0; i<numberUnits; i++ ) {
             Unit singleUnit = this.unitFileReader.getUnit(i);
-            double cost = vffrtIntonationCostFunction.cost(targetUnit, singleUnit);
+            double cost = vffrtContourCostFunction.cost(targetUnit, singleUnit);
             vocalizationCandidates[i] = new VocalizationCandidate(i,cost);
         }
         Arrays.sort(vocalizationCandidates);
