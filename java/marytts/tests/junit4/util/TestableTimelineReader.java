25c05b2833e6b9a1e70c7c9aef7c13c1ad0d54c6
Marc Schroeder <marc.schroeder@dfki.de>
2010-12-20 10:30:31 +0000

    Keep test utility classes separate from the actual test classes, so as not to confuse the ant tests target.
    git-svn-id: https://mary.opendfki.de/repos/trunk@2837 953a6561-930b-0410-b2a6-db37d1b2ae63

--- java/marytts/tests/junit4/util/TestableTimelineReader.java
+++ java/marytts/tests/junit4/util/TestableTimelineReader.java
@@ -17,7 +17,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
-package marytts.tests.junit4;
+package marytts.tests.junit4.util;
 
 import java.io.File;
 import java.io.IOException;
