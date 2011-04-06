b00bd3e19388e76e8234d2a672d0dc34891b5de4
Marc Schroeder <marc.schroeder@dfki.de>
2011-04-06 18:46:34 +0000

    Do the right thing: redstart and transcription should actually be separate sub-projects.
    git-svn-id: https://mary.opendfki.de/repos/branches/maven-branch@2909 953a6561-930b-0410-b2a6-db37d1b2ae63

--- modular/marytts/marytts-builder/src/main/java/marytts/tools/dbselection/SynthesisScriptGUI.java
+++ modular/marytts/marytts-builder/src/main/java/marytts/tools/dbselection/SynthesisScriptGUI.java
@@ -20,25 +20,36 @@
 package marytts.tools.dbselection;
 
 
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.util.ArrayList;
-import javax.swing.*;
-import javax.swing.table.*;
-import javax.swing.event.TableModelEvent;
-import javax.swing.event.TableModelListener;
-
-import marytts.tools.transcription.TranscriptionGUI;
-
-import java.beans.PropertyChangeListener;
-import java.beans.PropertyChangeEvent;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
 
 
 /**
