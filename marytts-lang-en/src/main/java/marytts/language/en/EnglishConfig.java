c94e38d53f1fb0298202adb7bb5bdd6bf108ea17
Marc Schroeder <marc.schroeder@dfki.de>
2011-05-04 08:20:22 +0000

    Make config classes read property files from classpath
    git-svn-id: https://mary.opendfki.de/repos/branches/maven-branch@2984 953a6561-930b-0410-b2a6-db37d1b2ae63

--- marytts-lang-en/src/main/java/marytts/language/en/EnglishConfig.java
+++ marytts-lang-en/src/main/java/marytts/language/en/EnglishConfig.java
@@ -1,12 +1,14 @@
 package marytts.language.en;
 
+import java.io.IOException;
 import java.util.Locale;
 
 import marytts.config.LanguageConfig;
+import marytts.exceptions.MaryConfigurationException;
 
 
 public class EnglishConfig extends LanguageConfig {
-	public EnglishConfig() {
-		super(Locale.ENGLISH);
+	public EnglishConfig() throws MaryConfigurationException {
+		super(EnglishConfig.class.getResourceAsStream("en.config"));
 	}
 }
--- marytts-runtime/src/main/java/marytts/config/MainConfig.java
+++ marytts-runtime/src/main/java/marytts/config/MainConfig.java
@@ -3,12 +3,18 @@
  */
 package marytts.config;
 
+
+import marytts.exceptions.MaryConfigurationException;
+
 /**
  * @author marc
  *
  */
 public class MainConfig extends MaryConfig {
 
+	public MainConfig() throws MaryConfigurationException {
+		super(MainConfig.class.getResourceAsStream("marybase.config"));
+	}
 	@Override
 	public boolean isMainConfig() {
 		return true;
