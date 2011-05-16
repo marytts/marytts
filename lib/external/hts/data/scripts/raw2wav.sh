6fd610d027958fe0d9bd2290eb0d307870c3f338
Marcela Charfuelan <Marcela.Charfuelan@dfki.de>
2011-05-16 14:18:03 +0000

    fixing bugs on hmm voice building procedure
    git-svn-id: https://mary.opendfki.de/repos/trunk@3001 953a6561-930b-0410-b2a6-db37d1b2ae63

--- lib/external/hts/data/scripts/raw2wav.sh
+++ lib/external/hts/data/scripts/raw2wav.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-sox_cmd=$1/sox
 
-soxVer=`$sox_cmd --version | awk '{print substr($3,2,2)}'`
 
-options="-c 1 -s -w -r 16000 "  
-if [ $soxVer -ge 13 ]; then
-  options="-c 1 -s -2 -r 16000 "  
-fi
-
-if [ $# -ne 3 ] ; then
+if [ $# -ne 4 ] ; then
   echo ""
   echo "raw2wav.sh: convert .raw files into .wav files using sox."
-  echo "     Usage: $0 sox-path-cmd raw-directory wav-directory"
+  echo "     Usage: $0 sox-path-cmd raw-directory wav-directory sampling-freq"
   echo ""
 else
+  sox_cmd=$1/sox
+  Fs=$4
+  soxVer=`$sox_cmd --version | awk '{print substr($3,2,2)}'`
+
+  options="-c 1 -s -w -r $Fs"  
+  if [ $soxVer -ge 13 ]; then
+    options="-c 1 -s -2 -r $Fs"  
+  fi
+  
+
   for file in $2/*.raw; do
     file_name=`basename $file .raw`
     echo $sox_cmd  $options $2/$file_name.raw "-->"  $options $3/$file_name.wav
