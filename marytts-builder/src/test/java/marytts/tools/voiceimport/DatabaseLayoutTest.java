7e39ccdecf07a5ab268b625e8e11443d9c168820
Ingmar Steiner <ingmar.steiner@inria.fr>
2012-02-28 16:27:48 +0100

    Fixes #10
    took the opportunity to rewrite parts of DatabaseLayoutTest to use JUnit's temp folder/file rules

--- marytts-builder/src/test/java/marytts/tools/voiceimport/DatabaseLayoutTest.java
+++ marytts-builder/src/test/java/marytts/tools/voiceimport/DatabaseLayoutTest.java
@@ -1,6 +1,5 @@
 package marytts.tools.voiceimport;
 
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
@@ -11,24 +10,26 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.regex.Pattern;
 
-import org.apache.commons.io.FileUtils;
-import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
-
+import org.junit.rules.TemporaryFolder;
 
 public class DatabaseLayoutTest {
-	File dummyVoiceDir;
 	File dummyConfigFile;
 	DatabaseLayout db;
-	
-	private static File createDummyConfigFile(File parent) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(DatabaseLayoutTest.class.getResourceAsStream("database.config"), "UTF-8"));
-		File conf = new File(parent, "dummy.config");
+
+	private static File createDummyConfigFile(TemporaryFolder parent)
+			throws IOException {
+		BufferedReader br = new BufferedReader(
+				new InputStreamReader(DatabaseLayoutTest.class
+						.getResourceAsStream("database.config"), "UTF-8"));
+		File conf = parent.newFile("dummy.config");
 		PrintWriter out = new PrintWriter(conf, "UTF-8");
 		String line;
 		String defaultPath = Pattern.quote("/home/test/my_voice");
-		String currentPath = parent.getAbsolutePath();
+		File root = parent.getRoot();
+		String currentPath = root.getAbsolutePath().replace("\\", "\\\\");
 		while ((line = br.readLine()) != null) {
 			line = line.replaceAll(defaultPath, currentPath);
 			out.println(line);
@@ -37,38 +38,25 @@ public class DatabaseLayoutTest {
 		out.close();
 		return conf;
 	}
-	
-	
+
+	@Rule
+	public TemporaryFolder dummyVoiceDir = new TemporaryFolder();
+
 	@Before
 	public void setUp() throws Exception {
-		dummyVoiceDir = new File(FileUtils.getTempDirectory(), "dummy_voice");
-		if (dummyVoiceDir.exists()) {
-			FileUtils.deleteDirectory(dummyVoiceDir);
-		}
-		dummyVoiceDir.mkdir();
 		dummyConfigFile = createDummyConfigFile(dummyVoiceDir);
 		assertTrue(dummyConfigFile.length() > 0);
-		File wavDir = new File(dummyVoiceDir, "wav");
-		wavDir.mkdir();
+		dummyVoiceDir.newFolder("wav");
 		db = new DatabaseLayout(dummyConfigFile, new VoiceImportComponent[] {
-			new AllophonesExtractor(),
-			new BasenameTimelineMaker(),
-			new EHMMLabeler()
-		});
+				new AllophonesExtractor(), new BasenameTimelineMaker(),
+				new EHMMLabeler() });
 	}
-	
+
 	@Test
 	public void isInitialized() {
 		assertTrue(db.isInitialized());
 		assertNotNull(db.getAllophoneSet());
 		assertNotNull(db.getLocale());
-	}
-	
-	
-	@After
-	public void tearDown() throws Exception {
-		dummyConfigFile.delete();
-		FileUtils.deleteDirectory(dummyVoiceDir);
 	}
 
 }
