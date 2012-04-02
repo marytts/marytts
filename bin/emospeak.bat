c99f7ec5c90b79f70d5693042f68042b227bad99
Marc Schröder <marc.schroeder@dfki.de>
2012-04-02 17:07:14 +0200

    Added more shell scripts to release assembly. Windows files are untested for now.

--- bin/emospeak.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set BINDIR=%~dp0
-call :RESOLVE "%BINDIR%\.." MARY_BASE
-java -showversion -ea -Dserver.host=localhost -jar "%MARY_BASE%"\java\emospeak.jar
-goto :EOF
-
-:RESOLVE
-set %2=%~f1
-goto :EOF
