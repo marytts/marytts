70bcbe88fd215d467daad1f6c398e53274a25393
Harald Berthelsen <harald@stts.se>
2012-04-10 11:37:49 +0200

    Added marytts-lang-sv

---
+++ marytts-lang-sv/src/main/java/marytts/language/sv/svPhonemizer_engLexicon.java
@@ -0,0 +1,463 @@
+/**
+ * Copyright 2002-2008 DFKI GmbH.
+ * All Rights Reserved.  Use is subject to license terms.
+ *
+ * This file is part of MARY TTS.
+ *
+ * MARY TTS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package marytts.language.sv;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import marytts.datatypes.MaryData;
+import marytts.datatypes.MaryDataType;
+import marytts.datatypes.MaryXML;
+import marytts.exceptions.MaryConfigurationException;
+import marytts.fst.FSTLookup;
+import marytts.modules.InternalModule;
+import marytts.modules.phonemiser.AllophoneSet;
+import marytts.modules.phonemiser.TrainedLTS;
+import marytts.server.MaryProperties;
+import marytts.util.MaryUtils;
+import marytts.util.dom.MaryDomUtils;
+import marytts.util.dom.NameNodeFilter;
+
+import org.w3c.dom.DOMException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.traversal.DocumentTraversal;
+import org.w3c.dom.traversal.NodeFilter;
+import org.w3c.dom.traversal.NodeIterator;
+import org.xml.sax.SAXException;
+
+
+/**
+ * The phonemiser module -- java implementation.
+ * 
+ * This file has been tweaked slightly from the original file JPhonemiser.java.
+ * It is used in tandem with svPhonemizer.java to generate pronunciations for 
+ * Swedish text including loan words and English terminology.
+ *
+ * @author Marc Schr&ouml;der, Sathish, tweaked by Erik Sterneberg
+ */
+
+public class svPhonemizer_engLexicon extends InternalModule
+{
+    protected Map<String, List<String>> userdict;
+    protected FSTLookup lexicon;
+    protected TrainedLTS lts;
+
+    protected AllophoneSet allophoneSet;
+
+    /*
+     * 	Needs en_GB voices to be installed
+     */
+    public svPhonemizer_engLexicon() throws IOException,  MaryConfigurationException{
+        this("svPhonemizer_engLexicon", MaryDataType.PARTSOFSPEECH, MaryDataType.PHONEMES,
+                MaryProperties.needFilename("sv.engAllophoneset"),
+                MaryProperties.getFilename("sv.engUserdict"),
+                MaryProperties.needFilename("sv.engLexicon"),
+                MaryProperties.needFilename("sv.engLettertosound"));
+    }
+    
+    
+    /**
+     * Constructor providing the individual filenames of files that are required.
+     * @param allophonesFilename
+     * @param userdictFilename
+     * @param lexiconFilename
+     * @param ltsFilename
+     * @throws Exception
+     */
+    public svPhonemizer_engLexicon(String componentName, 
+            MaryDataType inputType, MaryDataType outputType,
+            String allophonesFilename, String userdictFilename, String lexiconFilename, String ltsFilename)
+    throws IOException, MaryConfigurationException
+    {
+        super(componentName, inputType, outputType,
+                AllophoneSet.getAllophoneSet(allophonesFilename).getLocale());
+        allophoneSet = AllophoneSet.getAllophoneSet(allophonesFilename);
+        // userdict is optional
+        if (userdictFilename != null)
+            userdict = readLexicon(userdictFilename);
+        lexicon = new FSTLookup(lexiconFilename);
+        lts = new TrainedLTS(allophoneSet, new FileInputStream(ltsFilename));
+    }
+    
+    // never used
+    public MaryData process(MaryData d)
+        throws Exception
+    {
+    	/*
+        Document doc = d.getDocument();
+        NodeIterator it = MaryDomUtils.createNodeIterator(doc, doc, MaryXML.TOKEN);
+        Element t = null;
+        while ((t = (Element) it.nextNode()) != null) {
+                String text;
+                
+                // Do not touch tokens for which a transcription is already
+                // given (exception: transcription contains a '*' character:
+                if (t.hasAttribute("ph") &&
+                    !t.getAttribute("ph").contains("*")) {
+                    continue;
+                }
+                if (t.hasAttribute("sounds_like"))
+                    text = t.getAttribute("sounds_like");
+                else
+                    text = MaryDomUtils.tokenText(t);
+                
+                String pos = null;
+                // use part-of-speech if available
+                if (t.hasAttribute("pos")){
+                    pos = t.getAttribute("pos");
+                }
+                
+                if (text != null && !text.equals("")) {
+                    // If text consists of several parts (e.g., because that was
+                    // inserted into the sounds_like attribute), each part
+                    // is transcribed separately.
+                    StringBuilder ph = new StringBuilder();
+                    String g2pMethod = null;
+                    StringTokenizer st = new StringTokenizer(text, " -");
+                    while (st.hasMoreTokens()) {
+                        String graph = st.nextToken();
+                        StringBuilder helper = new StringBuilder();
+                        String phon = phonemise(graph, pos, helper);
+                        if (ph.length() == 0) { // first part
+                            // The g2pMethod of the combined beast is
+                            // the g2pMethod of the first constituant.
+                            g2pMethod = helper.toString();
+                            ph.append(phon);
+                        } else { // following parts
+                            ph.append(" - ");
+                            // Reduce primary to secondary stress:
+                            ph.append(phon.replace('\'', ','));
+                       }
+                    }
+                    
+                    if (ph != null && ph.length() > 0) {
+                        setPh(t, ph.toString());
+                        t.setAttribute("g2p_method", g2pMethod);
+                    }
+            }
+        }
+        MaryData result = new MaryData(outputType(), d.getLocale());
+        result.setDocument(doc);
+        return result;
+        */
+    	return d;
+    }
+
+    /**
+     * Phonemise the word text. This starts with a simple lexicon lookup,
+     * followed by some heuristics, and finally applies letter-to-sound rules
+     * if nothing else was successful. 
+     * 
+     * @param text the textual (graphemic) form of a word.
+     * @param pos the part-of-speech of the word
+     * @param g2pMethod This is an awkward way to return a second
+     * String parameter via a StringBuilder. If a phonemisation of the text is
+     * found, this parameter will be filled with the method of phonemisation
+     * ("lexicon", ... "rules"). 
+     * @return a phonemisation of the text if one can be generated, or
+     * null if no phonemisation method was successful.
+     */
+    public String phonemise(String text, String pos, StringBuilder g2pMethod)
+    {
+
+        // First, try a simple userdict and lexicon lookup:
+
+        String result = userdictLookup(text, pos);
+        if (result != null) {
+            g2pMethod.append("userdict");
+            return result;
+        }
+        
+        result = lexiconLookup(text, pos);
+        if (result != null) {
+            g2pMethod.append("lexicon");
+            return result;
+        }
+
+        // Lookup attempts failed. Try normalising exotic letters
+        // (diacritics on vowels, etc.), look up again:
+        String normalised = MaryUtils.normaliseUnicodeLetters(text, getLocale());
+        if (!normalised.equals(text)) {
+            result = userdictLookup(normalised, pos);
+            if (result != null) {
+                g2pMethod.append("userdict");
+                return result;
+            }
+            result = lexiconLookup(normalised, pos);
+            if (result != null) {
+                g2pMethod.append("lexicon");
+                return result;
+            }
+        }
+
+        //If not in English lexicon, don't use LTS rules. That method will be called later if necessary.
+        
+    	/*
+        // Cannot find it in the lexicon -- apply letter-to-sound rules
+        // to the normalised form
+
+        String phones = lts.predictPronunciation(text);
+        result = lts.syllabify(phones);
+        if (result != null) {
+            g2pMethod.append("rules");
+            return result;
+        }
+    	 */
+        return null;
+    }
+
+    /*
+    public String phonemise_UseDicts(String text, String pos, StringBuilder g2pMethod){
+        // First, try a simple userdict and lexicon lookup:
+
+        String result = userdictLookup(text, pos);
+        if (result != null) {
+            g2pMethod.append("english userdict");
+            return result;
+        }
+                
+        result = lexiconLookup(text, pos);
+        if (result != null) {
+            g2pMethod.append("english lexicon");
+            System.out.println("Found word in English lexicon. Pronunciation: " + result);
+            return result;
+        }
+
+        // Lookup attempts failed. Try normalising exotic letters
+        // (diacritics on vowels, etc.), look up again:
+        String normalised = MaryUtils.normaliseUnicodeLetters(text, getLocale());
+        if (!normalised.equals(text)) {
+            result = userdictLookup(normalised, pos);
+            if (result != null) {
+                g2pMethod.append("english userdict");
+                System.out.println("Found word in English user dictionary after normalizing exotic letters. Pronunciation: " + result);
+                return result;
+            }
+            result = lexiconLookup(normalised, pos);
+            if (result != null) {
+                g2pMethod.append("english lexicon");
+                System.out.println("Found word in English lexicon after normalizing exotic letters. Pronunciation: " + result);
+                return result;
+            }
+        }
+           
+        // If the word was not in either the user dictionary or the lexicon, return null
+        return null;
+    }
+      */  
+    
+    public String ltsLookup(String text){
+        // Cannot find it in the lexicon -- apply letter-to-sound rules
+        // to the normalised form
+
+        String phones = lts.predictPronunciation(text);
+        String result = lts.syllabify(phones);
+        if (result != null) {            
+            return result;
+        }
+        
+        return null;
+    }
+    
+    
+    /**
+     * Look a given text up in the (standard) lexicon. part-of-speech is used 
+     * in case of ambiguity.
+     * 
+     * @param text
+     * @param pos
+     * @return
+     */
+    public String lexiconLookup(String text, String pos)
+    {
+        if (text == null || text.length() == 0) return null;
+        String[] entries;
+        entries = lexiconLookupPrimitive(text, pos);
+        // If entry is not found directly, try the following changes:
+        // - lowercase the word
+        // - all lowercase but first uppercase
+        if (entries.length  == 0) {
+            text = text.toLowerCase(getLocale());
+            entries = lexiconLookupPrimitive(text, pos);
+        }
+        if (entries.length  == 0) {
+            text = text.substring(0,1).toUpperCase(getLocale()) + text.substring(1);
+            entries = lexiconLookupPrimitive(text, pos);
+         }
+         
+         if (entries.length  == 0) return null;
+         return entries[0];
+    }
+
+    private String[] lexiconLookupPrimitive(String text, String pos) {
+        String[] entries;
+        if (pos != null) { // look for pos-specific version first
+            entries = lexicon.lookup(text+pos);
+            if (entries.length == 0) {  // not found -- lookup without pos
+                entries = lexicon.lookup(text);
+            }
+        } else {
+            entries = lexicon.lookup(text);
+        }
+        return entries;
+    }
+    
+    /**
+     * look a given text up in the userdict. part-of-speech is used 
+     * in case of ambiguity.
+     * 
+     * @param text
+     * @param pos
+     * @return
+     */
+    public String userdictLookup(String text, String pos)
+    {
+        if (userdict == null || text == null || text.length() == 0) return null;
+        List<String> entries = userdict.get(text);
+        // If entry is not found directly, try the following changes:
+        // - lowercase the word
+        // - all lowercase but first uppercase
+        if (entries  == null) {
+            text = text.toLowerCase(getLocale());
+            entries = userdict.get(text);
+         }
+         if (entries == null) {
+             text = text.substring(0,1).toUpperCase(getLocale()) + text.substring(1);
+             entries = userdict.get(text);
+         }
+         
+         if (entries == null) return null;
+
+         String transcr = null;
+         for (String entry : entries) {
+             String[] parts = entry.split("\\|");
+             transcr = parts[0];
+             if (parts.length > 1 && pos != null) {
+                 StringTokenizer tokenizer = new StringTokenizer(entry);
+                 while (tokenizer.hasMoreTokens()) {
+                     String onePos = tokenizer.nextToken();
+                     if (pos.equals(onePos)) return transcr; // found
+                 }
+             }
+         }
+         // no match of POS: return last entry
+         return transcr;
+    }
+    
+    /**
+     * Access the allophone set underlying this phonemiser.
+     * @return
+     */
+    public AllophoneSet getAllophoneSet()
+    {
+        return allophoneSet;
+    }
+    
+
+    
+    /**
+     * Read a lexicon. Lines must have the format
+     * 
+     * graphemestring | phonestring | optional-parts-of-speech
+     * 
+     * The pos-item is optional. Different pos's belonging to one grapheme
+     * chain may be separated by whitespace
+     * 
+     * 
+     * @param lexiconFilename
+     * @return
+     */
+    protected Map<String, List<String>> readLexicon(String lexiconFilename)
+    throws IOException
+    {
+        String line;
+        Map<String,List<String>> fLexicon = new HashMap<String,List<String>>();
+
+        BufferedReader lexiconFile = new BufferedReader(new InputStreamReader(new FileInputStream(lexiconFilename), "UTF-8"));
+        while ((line = lexiconFile.readLine()) != null) {
+            // Ignore empty lines and comments:
+            if (line.trim().equals("") || line.startsWith("#"))
+                continue;
+
+            String[] lineParts = line.split("\\s*\\|\\s*");
+            String graphStr = lineParts[0];
+            String phonStr = lineParts[1];
+            try {
+                allophoneSet.splitIntoAllophones(phonStr);
+            } catch (RuntimeException re) {
+                logger.warn("Lexicon '"+lexiconFilename+"': invalid entry for '"+graphStr+"'", re);
+            }
+            String phonPosStr = phonStr;
+            if (lineParts.length > 2){
+                String pos = lineParts[2];
+                if (!pos.trim().equals(""))
+                    phonPosStr += "|" + pos;
+            }
+
+            List<String> transcriptions = fLexicon.get(graphStr);
+            if  (null == transcriptions) {
+                transcriptions = new ArrayList<String>();
+                fLexicon.put(graphStr, transcriptions);
+            }
+            transcriptions.add(phonPosStr);
+        }
+        lexiconFile.close();
+        return fLexicon; 
+    }
+
+    
+    protected void setPh(Element t, String ph)
+    {
+        if (!t.getTagName().equals(MaryXML.TOKEN))
+            throw new DOMException(DOMException.INVALID_ACCESS_ERR,
+                                   "Only t elements allowed, received " +
+                                   t.getTagName() + ".");
+        if (t.hasAttribute("ph")) {
+            String prevPh = t.getAttribute("ph");
+            // In previous sampa, replace star with sampa:
+            String newPh = prevPh.replaceFirst("\\*", ph);
+            t.setAttribute("ph", newPh);
+        } else {
+            t.setAttribute("ph", ph);
+        }
+    }
+
+}
+
