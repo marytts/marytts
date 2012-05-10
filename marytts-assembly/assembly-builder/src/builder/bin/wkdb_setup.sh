faf09fc5d946a1b5d8386624bfd81912009de6d4
Fabio Tesser <fabio.tesser@gmail.com>
2012-05-10 14:59:52 +0200

    Issue #22: setup script for wiki

---
+++ marytts-assembly/assembly-builder/src/builder/bin/wkdb_setup.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+##################################
+# wkdb_setup.sh
+# Created on: May 9, 2012
+# Author: Fabio Tesser
+# Institution: CNR-ISTC, Padova - Italy
+# Email: fabio.tesser@gmail.com
+##################################
+
+DESCRIPTION="Create wkdb config file in wkdb data path"
+
+MINARG=2
+if [ $# -lt $MINARG ]
+then
+  echo "NAME:
+  	`basename $0`
+
+DESCRIPTION:
+    $DESCRIPTION
+
+USAGE: 
+	`basename $0` [wkdb data path] [wkdb locale]
+wkdb data path: full path for the wkdb data path a configuration file will be create
+wkdb locale: the two letter locale for your language (e.g.: en, de, te, it, ...)
+EXAMPLE:
+	`basename $0` /home/mary/wikidb_data/ en"
+  exit 1
+fi  
+
+
+WIKIDATAPATH=$1
+
+mkdir -p $WIKIDATAPATH
+
+if [ -f $WIKIDATAPATH/wkdb.conf ]
+then
+    echo "Warining file $WIKIDATAPATH/wkdb.conf already exists"
+    echo "A backup file $WIKIDATAPATH/wkdb.conf.bak will be created"
+    cp $WIKIDATAPATH/wkdb.conf $WIKIDATAPATH/wkdb.conf.bak
+fi
+
+
+FULLPATHWIKIDATAPATH="`(cd $WIKIDATAPATH; pwd)`"
+
+
+echo "WIKIDATAPATH=$FULLPATHWIKIDATAPATH" > $WIKIDATAPATH/wkdb.conf
+echo "WIKILOCALE=$2" >> $WIKIDATAPATH/wkdb.conf
+
+
+
+
+
