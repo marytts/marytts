28f16b0082817c12727281074a6bb798ea9f8b1f
Fabio Tesser <fabio.tesser@gmail.com>
2012-10-01 15:51:39 +0200

    JTok for Italian added. Official version of Italian Jtok used, for now. Merged from 4.x branch

---
+++ marytts-lang-it/src/main/java/marytts/language/it/JTokeniser.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright 2003 DFKI GmbH.
+ * All Rights Reserved.  Use is subject to license terms.
+ *
+ * This file is part of MARY TTS.
+ *
+ * MARY TTS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package marytts.language.it;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Set;
+
+import marytts.datatypes.MaryData;
+import marytts.datatypes.MaryDataType;
+import marytts.datatypes.MaryXML;
+import marytts.util.dom.MaryDomUtils;
+import marytts.util.dom.NameNodeFilter;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.traversal.DocumentTraversal;
+import org.w3c.dom.traversal.NodeFilter;
+import org.w3c.dom.traversal.NodeIterator;
+
+
+/**
+ * Italian Tokenizer using JTok for Italian 
+ * @author Fabio Tesser
+ */
+public class JTokeniser extends marytts.modules.JTokeniser {
+
+    public JTokeniser() {
+        super(MaryDataType.RAWMARYXML, 
+                MaryDataType.TOKENS,
+                Locale.ITALIAN);
+    }
+
+	public MaryData process(MaryData d) throws Exception
+	{
+		MaryData result = super.process(d);
+		return result;
+	}
+}
