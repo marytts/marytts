e18deb486f6f0585f11757edcfec805372a2612f
Fabio Tesser <fabio.tesser@gmail.com>
2012-10-08 15:41:28 +0200

    Aestetic fixing for  issue marytts-it/marytts#6

--- marytts-assembly/assembly-builder/src/builder/bin/lexicon_lts_pos_builder.sh
+++ marytts-assembly/assembly-builder/src/builder/bin/lexicon_lts_pos_builder.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 BINDIR="`dirname "$0"`"
 export MARY_BASE="`(cd "$BINDIR"/.. ; pwd)`"
-#java -showversion -ea $* -cp "$MARY_BASE/lib/*" marytts.tools.transcription.LTSLexiconPOSBuilder 
 java -showversion -ea -Xmx4096m  -cp "$MARY_BASE/lib/*" marytts.tools.transcription.LTSLexiconPOSBuilder $*
-
-#lexicon_lts_pos_builder.sh ./marytts-lang-it/src/main/resources/marytts/language/it/lexicon/allophones.it.xml ~/tests/marytts5.0-new-lexicon/standard.lexicon
