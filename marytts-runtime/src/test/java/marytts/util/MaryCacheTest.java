0afafba1644fc36dba6f14ae24996f5544123863
Ingmar Steiner <steiner@coli.uni-saarland.de>
2014-02-19 01:47:25 +0100

    autoformat in Eclipse (Kepler)

--- marytts-runtime/src/test/java/marytts/util/MaryCacheTest.java
+++ marytts-runtime/src/test/java/marytts/util/MaryCacheTest.java
@@ -32,102 +32,93 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-
 /**
  * @author marc
- *
+ * 
  */
-public class MaryCacheTest
-{
-    private static MaryCache c;
-    private static File maryCacheFile = new File("tmp/testfiles-deleteme");
-    private static String inputtype = "TEXT";
-    private static String outputtype = "RAWMARYXML";
-    private static String locale = "de";
-    private static String voice = "de1";
-    private static String inputtext = "Welcome to the world of speech synthesis";
-    private static String targetValue = "<rawmaryxml/>";
-    private static byte[] targetAudio = new byte[12345];
-    private static String inputtext2 = "Some other input text";
-    private static String targetValue2 = "Two\nlines";
-    
-    /**
-     * @throws java.lang.Exception
-     */
-    @BeforeClass
-    public static void setUp() throws Exception {
-        c = new MaryCache(maryCacheFile, true);
-        c.insertText(inputtype, outputtype, locale, voice, inputtext, targetValue);
-        c.insertAudio(inputtype, locale, voice, inputtext, targetAudio);
-        c.insertText(inputtype, outputtype, locale, voice, inputtext2, targetValue2);
-    }
-    
+public class MaryCacheTest {
+	private static MaryCache c;
+	private static File maryCacheFile = new File("tmp/testfiles-deleteme");
+	private static String inputtype = "TEXT";
+	private static String outputtype = "RAWMARYXML";
+	private static String locale = "de";
+	private static String voice = "de1";
+	private static String inputtext = "Welcome to the world of speech synthesis";
+	private static String targetValue = "<rawmaryxml/>";
+	private static byte[] targetAudio = new byte[12345];
+	private static String inputtext2 = "Some other input text";
+	private static String targetValue2 = "Two\nlines";
 
-    /**
-     * @throws java.lang.Exception
-     */
-    @AfterClass
-    public static void tearDown() throws Exception {
-        c.shutdown();
-    }
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUp() throws Exception {
+		c = new MaryCache(maryCacheFile, true);
+		c.insertText(inputtype, outputtype, locale, voice, inputtext, targetValue);
+		c.insertAudio(inputtype, locale, voice, inputtext, targetAudio);
+		c.insertText(inputtype, outputtype, locale, voice, inputtext2, targetValue2);
+	}
 
-    @Test
-    public void lookupText() throws Exception
-    {
-        String lookupValue = c.lookupText(inputtype, outputtype, locale, voice, inputtext);
-        assertEquals(targetValue, lookupValue);
-    }
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDown() throws Exception {
+		c.shutdown();
+	}
 
-    @Test
-    public void lookupText2() throws Exception
-    {
-        String lookupValue = c.lookupText(inputtype, outputtype, locale, voice, inputtext2);
-        assertEquals(targetValue2, lookupValue);
-    }
+	@Test
+	public void lookupText() throws Exception {
+		String lookupValue = c.lookupText(inputtype, outputtype, locale, voice, inputtext);
+		assertEquals(targetValue, lookupValue);
+	}
 
-    @Test
-    public void lookupAudio() throws Exception
-    {
-        byte[] lookupAudio = c.lookupAudio(inputtype, locale, voice, inputtext);
-        assertNotNull(lookupAudio);
-        assertArrayEquals(targetAudio, lookupAudio);
-    }
-    
-    @Test
-    public void canInsertAgain() throws Exception
-    {
-        int numExceptions = 0;
-        try {
-            c.insertText(inputtype, outputtype, locale, voice, inputtext, targetValue);
-        } catch (SQLException e) {
-            numExceptions++;
-        }
-        try {
-            c.insertAudio(inputtype, locale, voice, inputtext, targetAudio);
-        } catch (SQLException e) {
-            numExceptions++;
-        }
-        assertEquals(0, numExceptions);
-    }
-    
-    @Test
-    public void isPersistent() throws Exception
-    {
-        c.shutdown();
-        c = new MaryCache(maryCacheFile, false);
-        lookupText();
-        lookupAudio();
-    }
-    
-    @Test
-    public void isClearable() throws Exception
-    {
-        c.shutdown();
-        c = new MaryCache(maryCacheFile, true);
-        String lookupValue = c.lookupText(inputtype, outputtype, locale, voice, inputtext);
-        assertNull(lookupValue);
-        byte[] lookupAudio = c.lookupAudio(inputtype, locale, voice, inputtext);
-        assertNull(lookupAudio);
-    }
-    
+	@Test
+	public void lookupText2() throws Exception {
+		String lookupValue = c.lookupText(inputtype, outputtype, locale, voice, inputtext2);
+		assertEquals(targetValue2, lookupValue);
+	}
+
+	@Test
+	public void lookupAudio() throws Exception {
+		byte[] lookupAudio = c.lookupAudio(inputtype, locale, voice, inputtext);
+		assertNotNull(lookupAudio);
+		assertArrayEquals(targetAudio, lookupAudio);
+	}
+
+	@Test
+	public void canInsertAgain() throws Exception {
+		int numExceptions = 0;
+		try {
+			c.insertText(inputtype, outputtype, locale, voice, inputtext, targetValue);
+		} catch (SQLException e) {
+			numExceptions++;
+		}
+		try {
+			c.insertAudio(inputtype, locale, voice, inputtext, targetAudio);
+		} catch (SQLException e) {
+			numExceptions++;
+		}
+		assertEquals(0, numExceptions);
+	}
+
+	@Test
+	public void isPersistent() throws Exception {
+		c.shutdown();
+		c = new MaryCache(maryCacheFile, false);
+		lookupText();
+		lookupAudio();
+	}
+
+	@Test
+	public void isClearable() throws Exception {
+		c.shutdown();
+		c = new MaryCache(maryCacheFile, true);
+		String lookupValue = c.lookupText(inputtype, outputtype, locale, voice, inputtext);
+		assertNull(lookupValue);
+		byte[] lookupAudio = c.lookupAudio(inputtype, locale, voice, inputtext);
+		assertNull(lookupAudio);
+	}
+
 }
