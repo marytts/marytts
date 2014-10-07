0ecaacdc34c26469299e6d171fefc7aa44cd7d5a
Tristan <hamiltt@tcd.ie>
2014-10-07 14:17:32 +0200

    issue #154 trailing whitespace solved
    solving issue #154 - test to check for trailing whitespace in config file using test file
    
    fix head
    
    fix issue#154
    
    issue#154 trailing whitespace solved

--- marytts-runtime/src/test/java/marytts/config/MaryConfigTest.java
+++ marytts-runtime/src/test/java/marytts/config/MaryConfigTest.java
@@ -24,9 +24,12 @@ import static org.junit.Assert.*;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import marytts.exceptions.MaryConfigurationException;
 
@@ -51,6 +54,61 @@ public class MaryConfigTest {
 	public void haveMainConfig() {
 		MaryConfig m = MaryConfig.getMainConfig();
 		assertNotNull(m);
+	}
+	
+	/**
+	 * @author Tristan
+	 * Test to check properties file for trailing whitespace [SOLVED]
+	 * -> created PropertiesTrimTrailingWhitespace class to trim trailing whitespace 
+	 * @throws MaryConfigurationException 
+	 * 
+	 */
+	@Test
+	public void checkTrailingWhitespace() throws InstantiationException, IllegalAccessException, MaryConfigurationException {
+		//should fail when Properties class is used in MaryConfig
+		Pattern p = Pattern.compile("\\s+$");
+		Matcher mat;
+		InputStream is = this.getClass().getResourceAsStream("/marytts/tests/junit4/testConfig.config");
+		Properties props = new Properties();
+		MaryConfig m = new testMainConfig(is);
+		
+		props = m.getProperties();
+		for(String key : props.stringPropertyNames()){
+			
+			if(props.getProperty(key).isEmpty()){
+				//System.out.printf("'%s' --> '%s'%n", key, props.getProperty(key));
+				continue;
+			}
+			if(props.getProperty(key).trim().isEmpty()){
+				//System.out.println(key);
+				//assertFalse(props.getProperty(key).trim().equals(""));
+				fail(key + " has no value...check for trailing whitespace in config file");
+			}
+			mat = p.matcher(props.getProperty(key));
+			if(mat.find()){
+				fail(key + "'s value has trailing whitespace, check config file");
+			}
+			//System.out.printf("'%s' --> '%s'%n", key, props.getProperty(key));	
+		}	
+	}
+	
+	@Test
+	public void testConfigExists() {
+	   assertNotNull("Test file missing", this.getClass().getResource("/marytts/tests/junit4/testConfig.config"));
+	   }
+	
+	/**
+	 * @author Tristan
+	 * Nested class to allow for instance of MaryConfig
+	 * 
+	 */
+	public static class testMainConfig extends MaryConfig{
+
+		protected testMainConfig(InputStream propertyStream)
+				throws MaryConfigurationException {
+			super(propertyStream);
+		
+		}
 	}
 	
 	@Test(expected=MaryConfigurationException.class)
