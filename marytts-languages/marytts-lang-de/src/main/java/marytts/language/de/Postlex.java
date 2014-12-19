6ff4a8d0e388f86787c45932cd2dac879e1709cf
Ingmar Steiner <steiner@coli.uni-saarland.de>
2014-12-19 13:10:25 +0100

    format java code in marytts-lang-de
    incantation:
    $ mvn com.googlecode.maven-java-formatter-plugin:maven-java-formatter-plugin:format -pl marytts-languages/marytts-lang-de

--- marytts-languages/marytts-lang-de/src/main/java/marytts/language/de/Postlex.java
+++ marytts-languages/marytts-lang-de/src/main/java/marytts/language/de/Postlex.java
@@ -39,116 +39,103 @@ import org.w3c.dom.traversal.DocumentTraversal;
 import org.w3c.dom.traversal.NodeFilter;
 import org.w3c.dom.traversal.TreeWalker;
 
-
 /**
  * The postlexical phonological processes module.
- *
+ * 
  * @author Marc Schr&ouml;der
  */
 
-public class Postlex extends PronunciationModel
-{
+public class Postlex extends PronunciationModel {
 
-    public Postlex()
-    {
-        super(Locale.GERMAN);
-    }
+	public Postlex() {
+		super(Locale.GERMAN);
+	}
 
-    public MaryData process(MaryData d)
-    throws Exception
-    {
-        Document doc = d.getDocument();
-        mtuPostlex(doc);
-        phonologicalRules(doc);
-        return super.process(d);
-    }
+	public MaryData process(MaryData d) throws Exception {
+		Document doc = d.getDocument();
+		mtuPostlex(doc);
+		phonologicalRules(doc);
+		return super.process(d);
+	}
 
-    private void mtuPostlex(Document doc)
-        throws DOMException
-    {
-        TreeWalker tw = ((DocumentTraversal)doc).
-            createTreeWalker(doc, NodeFilter.SHOW_ELEMENT,
-                             new NameNodeFilter(MaryXML.MTU), false);
-        Element m = null;
-        while ((m = (Element)tw.nextNode()) != null) {
-            if (MaryDomUtils.hasAncestor(m, MaryXML.MTU)) // not highest-level
-                continue;
-            // Now m is a highest-level mtu element
-            // Search for the token whose accent is retained;
-            // all other accents will be deleted.
-            Element c = m;
-            while (c != null &&
-                   !c.getTagName().equals(MaryXML.TOKEN)) {
-                String whatToAccent = c.getAttribute("accent");
-                if (whatToAccent != null && whatToAccent.equals("first"))
-                    c = MaryDomUtils.getFirstChildElement(c);
-                else
-                    c = MaryDomUtils.getLastChildElement(c);
-            }
-            Element retainAccentToken = c;
+	private void mtuPostlex(Document doc) throws DOMException {
+		TreeWalker tw = ((DocumentTraversal) doc).createTreeWalker(doc, NodeFilter.SHOW_ELEMENT, new NameNodeFilter(MaryXML.MTU),
+				false);
+		Element m = null;
+		while ((m = (Element) tw.nextNode()) != null) {
+			if (MaryDomUtils.hasAncestor(m, MaryXML.MTU)) // not highest-level
+				continue;
+			// Now m is a highest-level mtu element
+			// Search for the token whose accent is retained;
+			// all other accents will be deleted.
+			Element c = m;
+			while (c != null && !c.getTagName().equals(MaryXML.TOKEN)) {
+				String whatToAccent = c.getAttribute("accent");
+				if (whatToAccent != null && whatToAccent.equals("first"))
+					c = MaryDomUtils.getFirstChildElement(c);
+				else
+					c = MaryDomUtils.getLastChildElement(c);
+			}
+			Element retainAccentToken = c;
 
-            // Now all token below m except retainAccentToken get
-            // their accent deleted.
-            NodeList tokens = m.getElementsByTagName(MaryXML.TOKEN);
-            for (int i=0; i<tokens.getLength(); i++) {
-                Element t = (Element) tokens.item(i);
-                if (t != retainAccentToken) // not the same *Object*!
-                    t.removeAttribute("accent");
-            }
-        } // for all highest-level mtu elements
-    }
+			// Now all token below m except retainAccentToken get
+			// their accent deleted.
+			NodeList tokens = m.getElementsByTagName(MaryXML.TOKEN);
+			for (int i = 0; i < tokens.getLength(); i++) {
+				Element t = (Element) tokens.item(i);
+				if (t != retainAccentToken) // not the same *Object*!
+					t.removeAttribute("accent");
+			}
+		} // for all highest-level mtu elements
+	}
 
-
-    private void phonologicalRules(Document doc)
-    {
-        NodeList allTokens = doc.getElementsByTagName(MaryXML.TOKEN);
-        for (int i=0; i<allTokens.getLength(); i++) {
-            Element t = (Element)allTokens.item(i);
-            if (t.hasAttribute("ph")) { // otherwise there's no point
-                String transcript = t.getAttribute("ph");
-                // determine the pronunciation precision required:
-                // The precision depends on two things:
-                // a) the precision requested in the closest enclosing
-                //    <phonology> element
-                int phonolPrecision = PhonologicalRules.NORMAL;
-                Element phonolAncestor =
-                    (Element) MaryDomUtils.getAncestor(t, MaryXML.PHONOLOGY);
-                if (phonolAncestor != null) {
-                    String phonolPrecisionString = phonolAncestor.getAttribute("precision");
-                    if (phonolPrecisionString != null) {
-                        if (phonolPrecisionString.equals("precise")) {
-                            phonolPrecision = PhonologicalRules.PRECISE;
-                        } else if (phonolPrecisionString.equals("sloppy")) {
-                            phonolPrecision = PhonologicalRules.SLOPPY;
-                        }
-                    }
-                }
-                // b) whether this token has an accent or not.
-                boolean hasAccent = t.hasAttribute("accent") &&
-                    !t.getAttribute("accent").equals("none");
-                // Roughly, tokens that carry an accent are stepped
-                // one step up in precision.
-                int precision = phonolPrecision;
-                if (hasAccent) {
-                    if (precision == PhonologicalRules.NORMAL)
-                        precision = PhonologicalRules.PRECISE;
-                    else if (precision == PhonologicalRules.SLOPPY)
-                        precision = PhonologicalRules.NORMAL;
-                }
-                List rules = PhonologicalRules.getRules();
-                // for all rules
-                for (Iterator it = rules.iterator(); it.hasNext(); ) {
-                    PhonologicalRules pr = (PhonologicalRules) it.next();
-                    // if a key matches
-                    if (pr.matches(transcript)) {
-                        // apply the rule and remember the result
-                        transcript = pr.apply(transcript, precision);
-                    }
-                    // apply more rules if more rules match
-                }
-                t.setAttribute("ph", transcript);
-            } // if token has transcript
-        } // for all tokens
-    }
+	private void phonologicalRules(Document doc) {
+		NodeList allTokens = doc.getElementsByTagName(MaryXML.TOKEN);
+		for (int i = 0; i < allTokens.getLength(); i++) {
+			Element t = (Element) allTokens.item(i);
+			if (t.hasAttribute("ph")) { // otherwise there's no point
+				String transcript = t.getAttribute("ph");
+				// determine the pronunciation precision required:
+				// The precision depends on two things:
+				// a) the precision requested in the closest enclosing
+				// <phonology> element
+				int phonolPrecision = PhonologicalRules.NORMAL;
+				Element phonolAncestor = (Element) MaryDomUtils.getAncestor(t, MaryXML.PHONOLOGY);
+				if (phonolAncestor != null) {
+					String phonolPrecisionString = phonolAncestor.getAttribute("precision");
+					if (phonolPrecisionString != null) {
+						if (phonolPrecisionString.equals("precise")) {
+							phonolPrecision = PhonologicalRules.PRECISE;
+						} else if (phonolPrecisionString.equals("sloppy")) {
+							phonolPrecision = PhonologicalRules.SLOPPY;
+						}
+					}
+				}
+				// b) whether this token has an accent or not.
+				boolean hasAccent = t.hasAttribute("accent") && !t.getAttribute("accent").equals("none");
+				// Roughly, tokens that carry an accent are stepped
+				// one step up in precision.
+				int precision = phonolPrecision;
+				if (hasAccent) {
+					if (precision == PhonologicalRules.NORMAL)
+						precision = PhonologicalRules.PRECISE;
+					else if (precision == PhonologicalRules.SLOPPY)
+						precision = PhonologicalRules.NORMAL;
+				}
+				List rules = PhonologicalRules.getRules();
+				// for all rules
+				for (Iterator it = rules.iterator(); it.hasNext();) {
+					PhonologicalRules pr = (PhonologicalRules) it.next();
+					// if a key matches
+					if (pr.matches(transcript)) {
+						// apply the rule and remember the result
+						transcript = pr.apply(transcript, precision);
+					}
+					// apply more rules if more rules match
+				}
+				t.setAttribute("ph", transcript);
+			} // if token has transcript
+		} // for all tokens
+	}
 
 }
--- marytts-languages/marytts-lang-de/src/main/java/marytts/language/de/phonemiser/DeLex.java
+++ marytts-languages/marytts-lang-de/src/main/java/marytts/language/de/phonemiser/DeLex.java
@@ -37,134 +37,116 @@ import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
+public class DeLex {
 
+	private String modelPath = "DE-LEX.xml";
 
-public class DeLex
-{
+	protected Hashtable modelTable;
 
-   	private String modelPath = "DE-LEX.xml";
+	public DeLex(String knowledgeBasePath) {
 
-    protected Hashtable modelTable;
-    
-    public DeLex(String knowledgeBasePath){
+		this.modelTable = new Hashtable(40);
+		this.modelPath = knowledgeBasePath + modelPath;
+		loadInputModel();
 
-	this.modelTable = new Hashtable(40);
-	this.modelPath = knowledgeBasePath +modelPath;
-	loadInputModel();
+	}
 
-    }
+	public void loadInputModel() {
 
+		try {
 
+			// System.out.println("############################################################");
+			// System.out.println("Processing File : '"+modelPath+"'");
 
-    public void loadInputModel(){
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder parser = factory.newDocumentBuilder();
+			Document doc = parser.parse(modelPath);
+			Element rootNode = doc.getDocumentElement();
 
-	try{
+			if (rootNode.getNodeName().equals("entries")) {
+				NodeList entriesList = rootNode.getChildNodes();
 
-	    //System.out.println("############################################################");
-        //    System.out.println("Processing File : '"+modelPath+"'");
+				for (int i = 0; i < entriesList.getLength(); i++) {
+					if (entriesList.item(i).getNodeType() == org.w3c.dom.Node.ELEMENT_NODE) {
+						Element entry = (Element) entriesList.item(i);
 
-	    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-	    DocumentBuilder parser = factory.newDocumentBuilder();
-	    Document doc  = parser.parse(modelPath);
-	    Element rootNode = doc.getDocumentElement();
+						NodeList keyList = entry.getElementsByTagName("key");
 
-	    if(rootNode.getNodeName().equals("entries")){
-		NodeList entriesList = rootNode.getChildNodes();
+						NodeList valueList = entry.getElementsByTagName("value");
 
-		for (int i=0;i<entriesList.getLength();i++)
-		{
-		    if (entriesList.item(i).getNodeType()==org.w3c.dom.Node.ELEMENT_NODE){
-			Element entry=(Element)entriesList.item(i);
+						if (keyList.getLength() == 1 && valueList.getLength() == 1) {
+							NodeList keyTextNodeList = ((Element) keyList.item(0)).getChildNodes();
+							NodeList valueTextNodeList = ((Element) valueList.item(0)).getChildNodes();
 
-			NodeList keyList =entry.getElementsByTagName("key");
-			
-			NodeList valueList=entry.getElementsByTagName("value");
+							String keyString = "";
+							String valueString = "";
 
-			if (keyList.getLength() == 1 && valueList.getLength() == 1 )
-			{
-			    NodeList keyTextNodeList = ((Element)keyList.item(0)).getChildNodes();
-			    NodeList valueTextNodeList  = ((Element)valueList.item(0)).getChildNodes();
-			    
-			    String keyString = "";
-			    String valueString  = "";
+							for (int a = 0; a < keyTextNodeList.getLength(); a++) {
+								if (keyTextNodeList.item(a).getNodeType() == org.w3c.dom.Node.TEXT_NODE) {
+									org.w3c.dom.Text textNode = (org.w3c.dom.Text) keyTextNodeList.item(a);
+									keyString += textNode.getData().trim();
+								}
+							}
 
-			    
-			    for (int a=0;a<keyTextNodeList.getLength();a++)
-			    {
-				if (keyTextNodeList.item(a).getNodeType() == org.w3c.dom.Node.TEXT_NODE)
-				{
-				    org.w3c.dom.Text textNode=(org.w3c.dom.Text)keyTextNodeList.item(a);
-				    keyString+=textNode.getData().trim();
+							for (int a = 0; a < valueTextNodeList.getLength(); a++) {
+								if (valueTextNodeList.item(a).getNodeType() == org.w3c.dom.Node.TEXT_NODE) {
+									org.w3c.dom.Text textNode = (org.w3c.dom.Text) valueTextNodeList.item(a);
+									valueString += textNode.getData().trim();
+
+									// Element valueDOM = (Element) valueElementNodeList.item(a);
+									// valueString = document2String(valueDOM);
+								}
+							}
+							// System.out.println("<key> : "+keyString);
+							// System.out.println("<value> : "+valueString);
+							this.modelTable.put(keyString, valueString);
+
+						} else {
+							System.out.println("ERROR: The " + i + "th entry contains wrong number of subElements !!");
+						}
+					}
 				}
-			    }
 
-			    for (int a=0;a<valueTextNodeList.getLength();a++)
-			    {
-				if (valueTextNodeList.item(a).getNodeType() == org.w3c.dom.Node.TEXT_NODE)
-				{
-				    org.w3c.dom.Text textNode=(org.w3c.dom.Text)valueTextNodeList.item(a);
-				    valueString+=textNode.getData().trim();
-				    
-				    //Element valueDOM = (Element) valueElementNodeList.item(a);
-				    //valueString    = document2String(valueDOM);
-				}
-			    }
-			    //System.out.println("<key> : "+keyString);
-			    //System.out.println("<value> : "+valueString);
-			    this.modelTable.put(keyString,valueString);
-			    
-			    
-			}
-			else
-			{
-			    System.out.println("ERROR: The "+i+"th entry contains wrong number of subElements !!");
-			}
-		    }
+			} else
+				System.out.println("Document is not wellformed. Top-level element should be <entries>.");
+
+		} catch (IOException e) {
+			System.out.println("Can't find file '" + modelPath + "'.");
+		} catch (ParserConfigurationException e) {
+			System.out.println("Parser Configuration exception (Use trace mode for details)");
+		} catch (SAXException e) {
+			System.out.println("Error in XML file:\n" + e.getMessage());
+		} catch (Exception e) {
+			System.out.println("Unknown Error\n" + e.getMessage());
+			e.printStackTrace();
 		}
-		
-	    }
-	    else
-		System.out.println("Document is not wellformed. Top-level element should be <entries>.");
-	    
-	}catch (IOException e){
-            System.out.println("Can't find file '"+modelPath+"'.");
-        }catch (ParserConfigurationException e){
-            System.out.println("Parser Configuration exception (Use trace mode for details)");
-        }catch (SAXException e){
-            System.out.println("Error in XML file:\n"+e.getMessage());
-        }catch (Exception e){
-            System.out.println("Unknown Error\n"+e.getMessage());
-	    e.printStackTrace();
-        }
 
-    }
+	}
 
-     /**
-      * Prints a textual representation of a DOM object into a text string..
-      *
-      * @param element DOM object to parse.
-      * @return String representation of <i>document</i>.
-      */
-      public static String document2String(Element element) {
-	  String result = null;
+	/**
+	 * Prints a textual representation of a DOM object into a text string..
+	 * 
+	 * @param element
+	 *            DOM object to parse.
+	 * @return String representation of <i>document</i>.
+	 */
+	public static String document2String(Element element) {
+		String result = null;
 
-	  StringWriter strWtr = new StringWriter();
-          StreamResult strResult = new StreamResult(strWtr);
-          TransformerFactory tfac = TransformerFactory.newInstance();
-          try {
-	      Transformer trans = tfac.newTransformer();
-	      trans.setOutputProperty("omit-xml-declaration","yes");
-	      
-	      trans.transform(new DOMSource(element), strResult);
-	  } catch (Exception e) {
-	      System.err.println("XMLUtils.document2String(): " + e);
-	  }
-	  result = strResult.getWriter().toString();
+		StringWriter strWtr = new StringWriter();
+		StreamResult strResult = new StreamResult(strWtr);
+		TransformerFactory tfac = TransformerFactory.newInstance();
+		try {
+			Transformer trans = tfac.newTransformer();
+			trans.setOutputProperty("omit-xml-declaration", "yes");
 
-	  return result;
-      }//document2String()
+			trans.transform(new DOMSource(element), strResult);
+		} catch (Exception e) {
+			System.err.println("XMLUtils.document2String(): " + e);
+		}
+		result = strResult.getWriter().toString();
 
-    
+		return result;
+	}// document2String()
 
 }
-
