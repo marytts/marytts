4c205eca67bbdd31e525a6f687bf5b0b3663c089
Ingmar Steiner <steiner@coli.uni-saarland.de>
2015-01-06 16:29:33 +0100

    switch to testng for parameterized tests with proper array unwrapping

--- marytts-common/src/test/java/marytts/modules/phonemiser/AllophoneSetTest.java
+++ marytts-common/src/test/java/marytts/modules/phonemiser/AllophoneSetTest.java
@@ -1,21 +1,17 @@
 package marytts.modules.phonemiser;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
 import java.io.InputStream;
 
 import marytts.exceptions.MaryConfigurationException;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.testng.Assert;
+import org.testng.annotations.*;
 
 public class AllophoneSetTest {
 
 	private AllophoneSet allophoneSet;
 
-	@Before
+	@BeforeTest
 	public void setUp() throws MaryConfigurationException {
 		InputStream alloStream = AllophoneSetTest.class.getResourceAsStream("allophones.de.xml");
 		allophoneSet = AllophoneSet.getAllophoneSet(alloStream, "test");
@@ -25,22 +21,22 @@ public class AllophoneSetTest {
 	public void testLoadOnceReuse() throws MaryConfigurationException {
 		InputStream alloStream1b = AllophoneSetTest.class.getResourceAsStream("allophones.de.xml");
 		AllophoneSet allo1b = AllophoneSet.getAllophoneSet(alloStream1b, "test");
-		assertEquals(allophoneSet, allo1b);
+		Assert.assertEquals(allophoneSet, allo1b);
 	}
 
 	@Test
 	public void testUnloadedIsUnavailable() {
-		assertFalse(AllophoneSet.hasAllophoneSet("laaleeloo"));
+		Assert.assertFalse(AllophoneSet.hasAllophoneSet("laaleeloo"));
 	}
 
 	@Test
 	public void testLoadedIsAvailable() throws MaryConfigurationException {
-		assertTrue(AllophoneSet.hasAllophoneSet("test"));
+		Assert.assertTrue(AllophoneSet.hasAllophoneSet("test"));
 		AllophoneSet copy = AllophoneSet.getAllophoneSetById("test");
-		assertEquals(allophoneSet, copy);
+		Assert.assertEquals(allophoneSet, copy);
 	}
 
-	@Test(expected = IllegalArgumentException.class)
+	@Test(expectedExceptions = IllegalArgumentException.class)
 	public void testGetInvalidAllophone() {
 		allophoneSet.getAllophone("fnord");
 	}
