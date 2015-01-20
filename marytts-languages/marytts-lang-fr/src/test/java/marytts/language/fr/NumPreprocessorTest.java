f227e4210a72f5c441527a52c93ddd925e8e1ed5
Tristan Hamilton <hamilton@coli831.coli.uni-saarland.de>
2015-01-20 15:05:07 +0100

    issue #269 fix - initial preprocessing, tests working and formatted correctly

--- marytts-languages/marytts-lang-fr/src/test/java/marytts/language/fr/NumPreprocessorTest.java
+++ marytts-languages/marytts-lang-fr/src/test/java/marytts/language/fr/NumPreprocessorTest.java
@@ -10,7 +10,6 @@ import marytts.language.fr.NumPreprocess;
 import marytts.util.MaryUtils;
 import marytts.util.dom.DomUtils;
 
-import org.junit.BeforeClass;
 import org.custommonkey.xmlunit.*;
 import org.testng.Assert;
 import org.testng.annotations.*;
@@ -30,61 +29,39 @@ import javax.xml.parsers.ParserConfigurationException;
 public class NumPreprocessorTest {
 
 	private static NumPreprocess module;
-	
-	@BeforeClass
+
+	@BeforeSuite
 	public static void setUpBeforeClass() {
 		module = new NumPreprocess();
 	}
 
+	@DataProvider(name = "DocData")
+	private Object[][] numberExpansionDocData() {
+		return new Object[][] { { "1", "un" }, 
+								{ "2", "deux" }, 
+								{ "3", "trois" }, 
+								{ "4", "quatre" },
+								{ "42", "quarante-deux"} };
+	}
 
-	@DataProvider(name="DocData")
-	private Object[][] numberExpansionDocData(){
-		return new Object[][]{
-				{"1","un"},
-				{"2","deux"},
-				{"3","trois"},
-				{"4","quatre"}
-		};
-	}
-	
-	public static MaryData createMaryDataFromText(String text, Locale locale) {
-		Document doc = MaryXML.newDocument();
-		doc.getDocumentElement().setAttribute("xml:lang", MaryUtils.locale2xmllang(locale));
-		doc.getDocumentElement().appendChild(doc.createTextNode(text));
-		MaryData md = new MaryData(MaryDataType.RAWMARYXML, locale);
-		md.setDocument(doc);
-		return md;
-	}
-	
-	
 	@Test(dataProvider = "DocData")
-	public void testProcess(String tokenised, String expected) throws Exception{
-		String tokens = "<maryxml xmlns=\"http://mary.dfki.de/2002/MaryXML\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" version=\"0.5\" xml:lang=\"fr\"><p><s><t>" + tokenised + "</t></s></p></maryxml>";
-		MaryData mdActual = createMaryDataFromText(tokens, Locale.FRENCH);
-		String words =  "<maryxml xmlns=\"http://mary.dfki.de/2002/MaryXML\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" version=\"0.5\" xml:lang=\"fr\"><p><s><t>" + expected + "</t></s></p></maryxml>";
-		MaryData mdExpected = createMaryDataFromText(words, Locale.FRENCH);
-		mdActual = module.process(mdActual);
-		Diff diff = XMLUnit.compareXML(mdActual.getDocument(), mdExpected.getDocument());
-		Assert.assertTrue(diff.identical());
-	}
-		
-	
-	
-	@Test(dataProvider = "DocData")
-	public void testSpellout(String tokenised, String expected) throws Exception,ParserConfigurationException, SAXException, IOException {
+	public void testSpellout(String tokenised, String expected) throws Exception, ParserConfigurationException, SAXException,
+			IOException {
 		Document tokenisedDoc;
 		Document expectedDoc;
-		String tokens = "<maryxml xmlns=\"http://mary.dfki.de/2002/MaryXML\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" version=\"0.5\" xml:lang=\"fr\"><p><s><t>" + tokenised + "</t></s></p></maryxml>";
+		String tokens = "<maryxml xmlns=\"http://mary.dfki.de/2002/MaryXML\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" version=\"0.5\" xml:lang=\"fr\"><p><s><t>"
+				+ tokenised + "</t></s></p></maryxml>";
 		tokenisedDoc = DomUtils.parseDocument(tokens);
-		String words =  "<maryxml xmlns=\"http://mary.dfki.de/2002/MaryXML\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" version=\"0.5\" xml:lang=\"fr\"><p><s><t>" + expected + "</t></s></p></maryxml>";
+		String words = "<maryxml xmlns=\"http://mary.dfki.de/2002/MaryXML\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" version=\"0.5\" xml:lang=\"fr\"><p><s><t>"
+				+ expected + "</t></s></p></maryxml>";
 		expectedDoc = DomUtils.parseDocument(words);
 		module.checkForNumbers(tokenisedDoc);
 		Diff diff = XMLUnit.compareXML(expectedDoc, tokenisedDoc);
 		Assert.assertTrue(diff.identical());
 	}
-	
+
 	@Test(dataProvider = "DocData")
-	public void testExpandNum(String token, String word){
+	public void testExpandNum(String token, String word) {
 		double x = Double.parseDouble(token);
 		String actual = module.expandNumber(x);
 		Assert.assertEquals(actual, word);
