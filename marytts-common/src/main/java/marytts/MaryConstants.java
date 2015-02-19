1f986ad0f329d532da2d5817318a9adfe6ac94b6
Sébastien Le Maguer <slemaguer@coli.uni-saarland.de>
2015-02-19 15:39:47 +0100

    Add a global access mary constant class

---
+++ marytts-common/src/main/java/marytts/MaryConstants.java
@@ -0,0 +1,15 @@
+package marytts;
+
+
+/**
+ * All arbitrary constants used inside MaryTTS are declared here. 
+ *
+ * (WORK IN PROGRESS)
+ *
+ * @author <a href="mailto:slemaguer@coli.uni-saarland.de">Sébastien Le Maguer</a>
+ */
+public class MaryConstants
+{
+    /** Punction regular expression used to exclude punctuation of the phonetization stage */
+    public static final String PUNCT_POS_REGEXP = "^[^a-zA-Z]";
+}
