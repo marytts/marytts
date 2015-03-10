3b6c91073084e47ac80f17909c0bd96b76b74f6b
Tristan Hamilton <hamilton@coli831.coli.uni-saarland.de>
2015-03-10 14:42:03 +0100

    created UnitSelectionIT class, required Hamcrest for testing -> updated Hamcrest dependencies to Hamcrest-integration. Hamcrest developers advise against using Hamcrest-all. The maven dependency order for using hamcrest and junit also seem to rely on hamcrest coming BEFORE junit. Also looked at implementing a mocked voice for the integration test using mockito, however it seems a real voice is needed to test.

---
+++ marytts-unitselection/src/test/java/marytts/unitselection/UnitSelectionIT.java
@@ -0,0 +1,69 @@
+package marytts.unitselection;
+
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+import javax.sound.sampled.AudioInputStream;
+
+import marytts.LocalMaryInterface;
+import marytts.MaryInterface;
+import marytts.modules.synthesis.Voice;
+import marytts.config.VoiceConfig;
+import marytts.util.MaryRuntimeUtils;
+import marytts.util.dom.DomUtils;
+import marytts.config.MaryConfig;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.mockito.Mockito;
+
+public class UnitSelectionIT {
+
+	MaryInterface mary;
+
+	@Before
+	public void setUp() throws Exception {
+		mary = new LocalMaryInterface();
+	}
+
+	@Test
+	public void canReadUnitSelectionConfig() throws Exception {
+		assertThat(marytts.config.MaryConfig.getConfigs(), hasItem(isA(UnitSelectionConfig.class)));
+	}
+	
+	/**
+	@Test
+	public void canLoadVoice() throws Exception {
+		Voice voice = new UnitSelectionVoice(mockedVoiceConfig.getName(), null);
+		assertNotNull(voice);
+	}
+	**/
+	
+	/**
+	@Test
+	public void canSetVoice() throws Exception {
+		UnitSelectionVoice mockedVoice = Mockito.mock(UnitSelectionVoice.class);
+		when(mockedVoice.getName()).thenReturn("fnord");
+		
+		mary.setVoice(mockedVoice.getName());
+		assertEquals("fnord", mary.getVoice());
+		
+	}
+	**/
+	
+	/**
+	@Test
+	public void canProcessTextToSpeech() throws Exception {
+		mary.setVoice(mockedVoiceConfig.getName());
+		AudioInputStream audio = mary.generateAudio("Hello world");
+		assertNotNull(audio);
+	}
+	**/
+}
