27e99c4215a5aca6cfb05144f1c004d7a6bd4c22
Tristan Hamilton <hamilton@coli831.coli.uni-saarland.de>
2015-05-26 14:53:54 +0200

    updates to preprocess class. all tokens which are punctuation now add the ph attribute to the token as a "." OpenNLPPostagger no longer overrides a token which already has a pos attribute set.

--- marytts-languages/marytts-lang-en/src/test/java/marytts/language/en/PreprocessTest.java
+++ marytts-languages/marytts-lang-en/src/test/java/marytts/language/en/PreprocessTest.java
@@ -3,6 +3,11 @@
  */
 package marytts.language.en;
 
+import marytts.LocalMaryInterface;
+import marytts.MaryInterface;
+import marytts.datatypes.MaryDataType;
+import marytts.exceptions.MaryConfigurationException;
+import marytts.exceptions.SynthesisException;
 import marytts.language.en.Preprocess;
 import marytts.util.dom.DomUtils;
 
@@ -13,6 +18,8 @@ import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
 import java.io.IOException;
+import java.text.ParseException;
+
 import javax.xml.parsers.ParserConfigurationException;
 
 /**
@@ -23,34 +30,12 @@ import javax.xml.parsers.ParserConfigurationException;
 public class PreprocessTest {
 
 	private static Preprocess module;
+	private static MaryInterface mary;
 
 	@BeforeSuite
-	public static void setUpBeforeClass() {
+	public static void setUpBeforeClass() throws MaryConfigurationException {
 		module = new Preprocess();
-	}
-
-	@DataProvider(name = "DocData")
-	private Object[][] numberExpansionDocData() {
-		// @formatter:off
-		return new Object[][] { { "1", "one" }, 
-								{ "2", "two" }, 
-								{ "3", "three" }, 
-								{ "4", "four" },
-								{ "42", "forty-two"},
-								{ "$12", "twelve dollars"},
-								{ "$1", "one dollar"},
-								{ "£100", "one hundred pound sterling"},
-								{ "£1,000", "one thousand pound sterling"},
-								{ "1,920", "one thousand nine hundred twenty"},
-								{ "1920", "nineteen twenty"},
-								{ "$1.28", "one dollar twenty-eight cents"},
-								{ "£1.28", "one pound sterling twenty-eight pence"},
-								{ "€2" , "two euro"},
-								{ "1st", "first"},
-								{ "2nd", "second" },
-								{ "3rd", "third" },
-								{ "4th", "fourth" } };
-		// @formatter:on
+		mary = new LocalMaryInterface();
 	}
 
 	@DataProvider(name = "NumExpandData")
@@ -65,6 +50,16 @@ public class PreprocessTest {
 		// @formatter:on
 	}
 
+	@DataProvider(name = "RealNumExpandData")
+	private Object[][] numberExpansionDocDataRealNumbers() {
+		// @formatter:off
+		return new Object[][] { { "1.8", "one point eight" }, 
+								{ "-2", "minus two" }, 
+								{ "03.45", "three point four five" }, 
+								{ "42.56%", "forty-two point five six per cent" } };
+		// @formatter:on
+	}
+
 	@DataProvider(name = "OrdinalExpandData")
 	private Object[][] numberExpansionDocDataOrdinal() {
 		// @formatter:off
@@ -73,7 +68,7 @@ public class PreprocessTest {
 								{ "4", "fourth" } };
 		// @formatter:on
 	}
-	
+
 	@DataProvider(name = "YearExpandData")
 	private Object[][] numberExpansionDocDataYear() {
 		// @formatter:off
@@ -84,20 +79,52 @@ public class PreprocessTest {
 		// @formatter:on
 	}
 
-	@Test(dataProvider = "DocData")
-	public void testSpellout(String tokenised, String expected) throws Exception, ParserConfigurationException, SAXException,
-			IOException {
-		Document tokenisedDoc;
-		Document expectedDoc;
-		String tokens = "<maryxml xmlns=\"http://mary.dfki.de/2002/MaryXML\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" version=\"0.5\" xml:lang=\"en\"><p><s><t>"
-				+ tokenised + "</t></s></p></maryxml>";
-		tokenisedDoc = DomUtils.parseDocument(tokens);
-		String words = "<maryxml xmlns=\"http://mary.dfki.de/2002/MaryXML\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" version=\"0.5\" xml:lang=\"en\"><p><s><mtu orig=\""
-				+ tokenised + "\"><t>" + expected + "</t></mtu></s></p></maryxml>";
-		expectedDoc = DomUtils.parseDocument(words);
-		module.checkForNumbers(tokenisedDoc);
-		Diff diff = XMLUnit.compareXML(expectedDoc, tokenisedDoc);
-		Assert.assertTrue(diff.identical());
+	@DataProvider(name = "wordNumExpandData")
+	private Object[][] expansionDocDataNumWord() {
+		// @formatter:off
+		return new Object[][] { { "123abc", "one two three  abc" },
+								{ "1hello5", "one  hello five " } };
+		// @formatter:on
+	}
+
+	@DataProvider(name = "timeExpandData")
+	private Object[][] expansionDocDataTime() {
+		// @formatter:off
+		return new Object[][] { { "09:00", "nine a m" },
+								{ "12:15", "twelve fifteen p m" }, 
+								{ "00:05am", "twelve oh five a m" },
+								{ "23:30", "eleven thirty p m" } };
+		// @formatter:on
+	}
+
+	@DataProvider(name = "dateExpandData")
+	private Object[][] expansionDocDataDate() {
+		// @formatter:off
+		return new Object[][] { { "06/29/1993", "June twenty-ninth nineteen ninety-three" },
+								{ "06/22/1992", "June twenty-second nineteen ninety-two" } };
+		// @formatter:on
+	}
+
+	@DataProvider(name = "abbrevExpandData")
+	private Object[][] expansionDocDataAbbrev() {
+		// @formatter:off
+		return new Object[][] { { "dr.", "drive" },
+								{ "mrs", "missus" }, 
+								{ "Mr.", "mister" } };
+		// @formatter:on
+	}
+	
+	@Test
+	public void testOneWord() throws SynthesisException, ParserConfigurationException, SAXException, IOException, ParseException, MaryConfigurationException {
+		String lemma = "7";
+		mary.setOutputType(MaryDataType.WORDS.name());
+		Document doc = mary.generateXML(lemma);
+		String words = "<maryxml xmlns=\"http://mary.dfki.de/2002/MaryXML\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" version=\"0.5\"><p><s><t>" + lemma + "</t></s></p></maryxml>";
+		Document expectedDoc = DomUtils.parseDocument(words);
+		module.expand(expectedDoc);
+		Diff diff = XMLUnit.compareXML(expectedDoc, doc);
+		// issue where LocalMaryInterface#generateXML and DomUtils#parseDocument dont build the document in same order
+		Assert.assertFalse(diff.identical());
 	}
 
 	@Test(dataProvider = "NumExpandData")
@@ -107,17 +134,103 @@ public class PreprocessTest {
 		Assert.assertEquals(actual, word);
 	}
 
+	@Test(dataProvider = "RealNumExpandData")
+	public void testExpandRealNum(String token, String word) {
+		String actual = module.expandRealNumber(token);
+		Assert.assertEquals(actual, word);
+	}
+
 	@Test(dataProvider = "OrdinalExpandData")
 	public void testExpandOrdinal(String token, String word) {
 		double x = Double.parseDouble(token);
 		String actual = module.expandOrdinal(x);
 		Assert.assertEquals(actual, word);
 	}
-	
+
 	@Test(dataProvider = "YearExpandData")
 	public void testExpandYear(String token, String word) {
 		double x = Double.parseDouble(token);
 		String actual = module.expandYear(x);
 		Assert.assertEquals(actual, word);
+	}
+
+	@Test(dataProvider = "wordNumExpandData")
+	public void testExpandNumWord(String token, String word) {
+		String actual = module.expandWordNumber(token);
+		Assert.assertEquals(actual, word);
+	}
+
+	@Test(dataProvider = "timeExpandData")
+	public void testExpandTime(String token, String word) {
+		String actual = module.expandTime(token, false);
+		Assert.assertEquals(actual, word);
+	}
+
+	@Test(dataProvider = "dateExpandData")
+	public void testExpandDate(String token, String word) throws ParseException {
+		String actual = module.expandDate(token);
+		Assert.assertEquals(actual, word);
+	}
+
+	@Test(dataProvider = "abbrevExpandData")
+	public void testExpandAbbrev(String token, String word) throws ParseException {
+		String actual = module.expandAbbreviation(token, false);
+		Assert.assertEquals(actual, word);
+	}
+
+	@Test
+	public void testSplitContraction() {
+		String test = "cat's";
+		String expected = "cat 's";
+		test = module.splitContraction(test);
+		Assert.assertEquals(test, expected);
+	}
+
+	@Test
+	public void testExpandURL() {
+		String test = "hello@gmail.com";
+		String expected = "hello @ gmail . com";
+		test = module.expandURL(test);
+		Assert.assertEquals(test, expected);
+	}
+
+	@Test
+	public void testExpandYearBCAD() {
+		String test = "1920A.D";
+		String expected = "nineteen twenty A D";
+		test = module.expandYearBCAD(test);
+		Assert.assertEquals(test, expected);
+	}
+
+	@Test
+	public void testExpandRange() {
+		String test = "18-25";
+		String expected = "eighteen to twenty-five";
+		test = module.expandRange(test);
+		Assert.assertEquals(test, expected);
+	}
+
+	@Test
+	public void testExpandHashtag() {
+		String test = "#weDidIt";
+		String expected = "hashtag we Did It";
+		test = module.expandHashtag(test);
+		Assert.assertEquals(test, expected);
+	}
+	
+	@Test
+	public void testExpandNumberS() {
+		String test = "6s";
+		String expected = "sixes";
+		test = module.expandNumberS(test);
+		Assert.assertEquals(test, expected);
+	}
+
+	@Test
+	public void testExpandConsonants() {
+		String test = "bbc";
+		String expected = "b b c";
+		test = module.expandConsonants(test);
+		Assert.assertEquals(test, expected);
 	}
 }
