787299eb09b158fa6663add95991e41ec5382a33
Aitor Martinez Egurcegui <aitorme@coli.uni-saarland.de>
2015-09-21 15:28:46 +0200

    fixed broken itemize lists in some package-info.java files

--- marytts-signalproc/src/main/java/marytts/signalproc/adaptation/prosody/package-info.java
+++ marytts-signalproc/src/main/java/marytts/signalproc/adaptation/prosody/package-info.java
@@ -1,10 +1,11 @@
 /**
- * Prosody transformation algorithms for voice conversion.
- * A prosody modification framework has been implemented which supports:
- *  -Mean and standard deviation transformation of f0
- *  -Sentence slope transformation
- * <p>
- * Mean and standard deviation transformation is the best method so far.
- * Duration and energy transformation have not yet been implemented.
+ * <p>Prosody transformation algorithms for voice conversion.<br>
+ * A prosody modification framework has been implemented which supports:<br>
+ * - Mean and standard deviation transformation of f0<br>
+ * - Sentence slope transformation<br>
+ * </p>
+ * <p>Mean and standard deviation transformation is the best method so far.<br>
+ * Duration and energy transformation have not yet been implemented.<br>
+ * </p>
  */
 package marytts.signalproc.adaptation.prosody;
