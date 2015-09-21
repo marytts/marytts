83f8489c21dcf0d28cbbdcc189b264e41740f274
Ingmar Steiner <steiner@coli.uni-saarland.de>
2015-09-21 16:02:24 +0200

    tweak lists in package-info javadoc

--- marytts-signalproc/src/main/java/marytts/signalproc/analysis/package-info.java
+++ marytts-signalproc/src/main/java/marytts/signalproc/analysis/package-info.java
@@ -1,7 +1,6 @@
 /**
- * <p>A collection of analysis algorithms for signal processing.<br>
- * Important classes are as follows:<br>
- * </p>
+ * A collection of analysis algorithms for signal processing.<br>
+ * Important classes are as follows:
  * <ul>
  *   <li>LpcAnalyser: Linear prediction analysis using autocorrelation
  * appraoch and Durbin recursion</li>
@@ -9,13 +8,12 @@
  * line spectral pairs - LSPs) based on LpcAnalyser</li>
  *   <li>EnergyAnalyser: Energy contour estimation with voice activity
  * detection support</li>
- *   <li>F0TrackerAutocorrelationHeuristic: An autocorrelation&nbsp; based
+ *   <li>F0TrackerAutocorrelationHeuristic: An autocorrelation based
  * f0 analysis algorithm extended with heuristic post-processing to reduce
  * voiced/unvoiced errors and f0 doubling/halving problems. This tracker
  * works better as compared to Praat but worse as compared to
  * Snack/Wavesurfer. Snack employs RAPT (Robust Algorithm for Pitch
- * Tracking [Talkin, 1995])<br>
- *   </li>
+ * Tracking [Talkin, 1995])</li>
  *   <li>SeevocAnalyser: A basic implementation of the Spectral Envelope
  * Estimation Vocoder which fits a spectral envelope to spectral peaks
  * using peak detection and linear interpolation. This is used by
@@ -23,8 +21,7 @@
  *   <li>RegularizedCepstrumEstimator (and classes derived from it):
  * Estimation of a spectral envelope by cepstrum method using spectral
  * amplitudes measured in frequency domain. These methods are used in
- * sinusoidal and harmonics plus noise models-<br>
- *   </li>
+ * sinusoidal and harmonics plus noise models</li>
  * </ul>
  * <p>[Talkin, 1995] D. Talkin, "A robust algorithm for pitch tracking
  * (RAPT)," in <i>Speech Coding and Synthesis</i> (W. B. Kleijn and K. K.
