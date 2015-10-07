marytts/marytts

commit dc434951b77ad0b2dbc040f9444210525d819403

commit fixing various errors for the javadoc file under marytts-transcription

marytts-client/src/main/java/marytts/client/MaryClient.java
@@ -42,7 +42,7 @@ import marytts.util.http.Address;
  * 
  * @author Marc Schr&ouml;der, oytun.turk
  * @see MaryGUIClient A GUI interface to this client
- * @link {marytts.server.MaryServer} Description of the MARY protocol
+ * @link {marytts.server#MaryServer} Description of the MARY protocol
  */
 
 public abstract class MaryClient {

marytts-client/src/main/java/marytts/client/MarySocketClient.java
@@ -51,7 +51,7 @@ import marytts.util.string.StringUtils;
  * 
  * @author Marc Schr&ouml;der
  * @see MaryGUIClient A GUI interface to this client
- * @link {marytts.server.MaryServer} Description of the MARY protocol
+ * @link {marytts.server#MaryServer} Description of the MARY protocol
  */
 
 public class MarySocketClient extends MaryClient {

marytts-client/src/main/java/marytts/client/http/MaryHttpClient.java
@@ -54,7 +54,7 @@ import marytts.util.string.StringUtils;
  * 
  * @author Marc Schr&ouml;der, oytun.turk
  * @see MaryGUIClient A GUI interface to this client
- * @link {marytts.server.MaryServer} Description of the MARY protocol.
+ * @link {marytts.server#MaryServer} Description of the MARY protocol.
  */
 
 public class MaryHttpClient extends MaryClient {

marytts-client/src/main/java/marytts/tools/emospeak/RectangularTwoDimensionalModel.java
@@ -180,7 +180,7 @@ public class RectangularTwoDimensionalModel implements TwoDimensionalModel {
 	/**
 	 * Get the X value.
 	 * 
-	 * @param return x
+	 * @return x
 	 */
 	public int getX() {
 		return x;
@@ -189,7 +189,7 @@ public class RectangularTwoDimensionalModel implements TwoDimensionalModel {
 	/**
 	 * Get the Y value.
 	 * 
-	 * @param return y
+	 * @return y
 	 */
 	public int getY() {
 		return y;

marytts-transcription/src/main/java/marytts/tools/transcription/LTSLexiconPOSBuilder.java
@@ -45,7 +45,7 @@ import marytts.util.io.FileUtils;
  * 
  * Train Letter-to-sound(LTS) rules, create FST dictionary and POS tagger
  * 
- * @author Fabio Tesser <fabio.tesser@gmail.com>
+ * @author Fabio Tesser &lt;fabio.tesser@gmail.com&gt;
  * 
  *         Example use:
  *         <code>java -showversion -ea -Xmx4096m  -cp "$MARY_BASE/lib/*" marytts.tools.transcription.LTSLexiconPOSBuilder ./marytts-lang-it/src/main/resources/marytts/language/it/lexicon/allophones.it.xml ./marytts-lang-it/lib/modules/it/lexicon/it.txt </code>
@@ -77,7 +77,9 @@ public class LTSLexiconPOSBuilder {
 	 * load phoneset
 	 * 
 	 * @param filePath
+	 *            filePath
 	 * @throws MaryConfigurationException
+	 *             MaryConfigurationException
 	 */
 	private void loadPhoneSet(String filePath) throws MaryConfigurationException {
 		phoneSet = AllophoneSet.getAllophoneSet(filePath);
@@ -88,7 +90,9 @@ public class LTSLexiconPOSBuilder {
 	 * Load transcription from file
 	 * 
 	 * @param fileName
+	 *            fileName
 	 * @throws Exception
+	 *             Exception
 	 */
 	private void loadTranscription(String fileName) throws Exception {
 		transcriptionModel.loadTranscription(fileName, false);
@@ -118,8 +122,11 @@ public class LTSLexiconPOSBuilder {
 	 * train and predict module
 	 * 
 	 * @param treeAbsolutePath
+	 *            treeAbsolutePath
 	 * @throws IOException
+	 *             IOException
 	 * @throws MaryConfigurationException
+	 *             MaryConfigurationException
 	 */
 	public void trainPredict(String treeAbsolutePath) throws IOException, MaryConfigurationException {
 		Object[][] tableData = transcriptionModel.getData();
@@ -194,7 +201,9 @@ public class LTSLexiconPOSBuilder {
 	 * save transcription into file
 	 * 
 	 * @param fileName
+	 *            fileName
 	 * @throws Exception
+	 *             Exception
 	 */
 	public void saveTranscription(String fileName) throws Exception {
 
@@ -226,6 +235,8 @@ public class LTSLexiconPOSBuilder {
 	/**
 	 * @param args
 	 *            first argument, PhoneSet file name; second argument, Transcriptions file name
+	 * @throws Exception
+	 *             Exception
 	 */
 	public static void main(String[] args) throws Exception {
 		try {

marytts-transcription/src/main/java/marytts/tools/transcription/TranscriptionTable.java
@@ -126,6 +126,7 @@ public class TranscriptionTable extends JPanel implements ActionListener {
 	 * verify transcription syntax
 	 * 
 	 * @param row
+	 *            row
 	 */
 	private void checkTranscriptionSyntax(int row) {
 
@@ -184,6 +185,11 @@ public class TranscriptionTable extends JPanel implements ActionListener {
 	 * train and predict module
 	 * 
 	 * @param treeAbsolutePath
+	 *            treeAbsolutePath
+	 * @param myRemoveTrailingOneFromPhones
+	 *            myRemoveTrailingOneFromPhones
+	 * @throws MaryConfigurationException
+	 *             MaryConfigurationException
 	 */
 	public void trainPredict(String treeAbsolutePath, boolean myRemoveTrailingOneFromPhones) throws MaryConfigurationException {
 		Object[][] tableData = transcriptionModel.getData();
@@ -232,9 +238,10 @@ public class TranscriptionTable extends JPanel implements ActionListener {
 	}
 
 	/**
-	 * save transcrption into file
+	 * save transcription into file
 	 * 
 	 * @param fileName
+	 *            fileName
 	 */
 	public void saveTranscription(String fileName) {
 		try {
@@ -272,6 +279,7 @@ public class TranscriptionTable extends JPanel implements ActionListener {
 	 * Load transcription from file
 	 * 
 	 * @param fileName
+	 *            fileName
 	 */
 	public void loadTranscription(String fileName) {
 		try {
@@ -291,6 +299,7 @@ public class TranscriptionTable extends JPanel implements ActionListener {
 	 * Add words from file
 	 * 
 	 * @param fileName
+	 *            fileName
 	 */
 	public void addWordsToTranscription(String fileName) {
 		try {
@@ -310,7 +319,9 @@ public class TranscriptionTable extends JPanel implements ActionListener {
 	 * Load transcription from a hashmap
 	 * 
 	 * @param map
+	 *            map
 	 * @throws Exception
+	 *             Exception
 	 */
 	@Deprecated
 	// doesn't seem to get used -- remove?
@@ -327,7 +338,9 @@ public class TranscriptionTable extends JPanel implements ActionListener {
 	 * Load transcription from a arrayList
 	 * 
 	 * @param arrList
+	 *            arrList
 	 * @throws Exception
+	 *             Exception
 	 */
 	public void loadTranscription(ArrayList<String> arrList) throws Exception {
 		this.transcriptionModel.loadTranscription(arrList);
@@ -342,6 +355,7 @@ public class TranscriptionTable extends JPanel implements ActionListener {
 	 * load phoneset
 	 * 
 	 * @param filePath
+	 *            filePath
 	 */
 	public void loadPhoneSet(String filePath) {
 		try {

marytts-transcription/src/main/java/marytts/tools/transcription/TranscriptionTableModel.java
@@ -123,7 +123,9 @@ public class TranscriptionTableModel extends AbstractTableModel {
 	 * Save transcription to a file
 	 * 
 	 * @param fileName
+	 *            fileName
 	 * @throws Exception
+	 *             Exception
 	 */
 	public void saveTranscription(String fileName) throws Exception {
 		PrintWriter out = new PrintWriter(new OutputStreamWriter(new FileOutputStream(fileName), "UTF-8"));
@@ -167,9 +169,11 @@ public class TranscriptionTableModel extends AbstractTableModel {
 	 * existing data will be added.
 	 * 
 	 * @param fileName
+	 *            fileName
 	 * @param keepCurrentData
 	 *            whether to keep any current data and add, or use only the loaded data.
 	 * @throws Exception
+	 *             Exception
 	 */
 	public void loadTranscription(String fileName, boolean keepCurrentData) throws Exception {
 		List<String> lines = new ArrayList<String>();
@@ -255,7 +259,9 @@ public class TranscriptionTableModel extends AbstractTableModel {
 	 * Load transcription from HashMap
 	 * 
 	 * @param wordList
+	 *            wordList
 	 * @throws Exception
+	 *             Exception
 	 */
 	@Deprecated
 	// doesn't seem to get used -- remove?
@@ -300,7 +306,9 @@ public class TranscriptionTableModel extends AbstractTableModel {
 	 * Save user entered and verified transcription in to lexicon format
 	 * 
 	 * @param fileName
+	 *            fileName
 	 * @throws IOException
+	 *             IOException
 	 */
 	public void saveSampaLexiconFormat(String fileName) throws IOException {
 		if (!hasLexiconData())
@@ -328,7 +336,11 @@ public class TranscriptionTableModel extends AbstractTableModel {
 	 * Save user entered and verified transcription in to lexicon format
 	 * 
 	 * @param fileName
+	 *            fileName
+	 * @param phoneSet
+	 *            phoneSet
 	 * @throws IOException
+	 *             IOException
 	 */
 	public void saveSampaLexiconFormat(String fileName, AllophoneSet phoneSet) throws IOException {
 		if (!hasLexiconData())
@@ -355,7 +367,9 @@ public class TranscriptionTableModel extends AbstractTableModel {
 	 * Save all functional words into text file
 	 * 
 	 * @param fileName
+	 *            fileName
 	 * @throws IOException
+	 *             IOException
 	 */
 	public void saveFunctionalWords(String fileName) throws IOException {
 		if (!hasFunctionalData())
@@ -398,8 +412,11 @@ public class TranscriptionTableModel extends AbstractTableModel {
 	 * For all words in lexicon, verify if they can be looked up in fst file.
 	 * 
 	 * @param lexiconFilename
+	 *            lexiconFilename
 	 * @param fstFilename
+	 *            fstFilename
 	 * @throws IOException
+	 *             IOException
 	 */
 	public void testFST(String lexiconFilename, String fstFilename) throws IOException {
 		System.err.println("Testing FST...");
@@ -469,8 +486,11 @@ public class TranscriptionTableModel extends AbstractTableModel {
 	 * Creates lexicon in FST format and letter-to-sound models
 	 * 
 	 * @param lexiconFilename
+	 *            lexiconFilename
 	 * @param fstFilename
+	 *            fstFilename
 	 * @throws Exception
+	 *             Exception
 	 */
 	public void createLexicon(String lexiconFilename, String fstFilename) throws Exception {
 
@@ -502,6 +522,8 @@ public class TranscriptionTableModel extends AbstractTableModel {
 
 	/**
 	 * get column count
+	 * 
+	 * @return columnNames.length
 	 */
 	public int getColumnCount() {
 		return columnNames.length;
@@ -509,6 +531,8 @@ public class TranscriptionTableModel extends AbstractTableModel {
 
 	/**
 	 * get row count
+	 * 
+	 * @return data.length
 	 */
 	public int getRowCount() {
 		return data.length;
@@ -516,6 +540,10 @@ public class TranscriptionTableModel extends AbstractTableModel {
 
 	/**
 	 * get column name
+	 * 
+	 * @param col
+	 *            col
+	 * @return columnNames[col]
 	 */
 	public String getColumnName(int col) {
 		return columnNames[col];
@@ -523,6 +551,12 @@ public class TranscriptionTableModel extends AbstractTableModel {
 
 	/**
 	 * get value at given location
+	 * 
+	 * @param row
+	 *            row
+	 * @param col
+	 *            col
+	 * @return data[row][col]
 	 */
 	public Object getValueAt(int row, int col) {
 		return data[row][col];

commit 450721d2f8c58a1cfa67e6647078920db96fd01c

add marytts-runtime as javadoc dependency of marytts-client
and fix issues introduced in dc434951b77ad0b2dbc040f9444210525d819403 and 92a67836f583470f8a2ef8fce73b580fb2a8eafe

marytts-client/pom.xml
@@ -66,4 +66,21 @@
 			</plugin>
 		</plugins>
 	</build>
+
+	<reporting>
+		<plugins>
+			<plugin>
+				<artifactId>maven-javadoc-plugin</artifactId>
+				<configuration>
+					<additionalDependencies>
+						<additionalDependency>
+							<groupId>${project.groupId}</groupId>
+							<artifactId>marytts-runtime</artifactId>
+							<version>${project.version}</version>
+						</additionalDependency>
+					</additionalDependencies>
+				</configuration>
+			</plugin>
+		</plugins>
+	</reporting>
 </project>

marytts-client/src/main/java/marytts/client/MaryClient.java
@@ -42,7 +42,7 @@ import marytts.util.http.Address;
  * 
  * @author Marc Schr&ouml;der, oytun.turk
  * @see MaryGUIClient A GUI interface to this client
- * @link {marytts.server#MaryServer} Description of the MARY protocol
+ * @see marytts.server.MaryServer Description of the MARY protocol
  */
 
 public abstract class MaryClient {

marytts-client/src/main/java/marytts/client/MarySocketClient.java
@@ -51,7 +51,7 @@ import marytts.util.string.StringUtils;
  * 
  * @author Marc Schr&ouml;der
  * @see MaryGUIClient A GUI interface to this client
- * @link {marytts.server#MaryServer} Description of the MARY protocol
+ * @see marytts.server.MaryServer Description of the MARY protocol
  */
 
 public class MarySocketClient extends MaryClient {

marytts-client/src/main/java/marytts/client/http/MaryHttpClient.java
@@ -28,7 +28,6 @@ import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
-import java.net.UnknownHostException;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -54,7 +53,7 @@ import marytts.util.string.StringUtils;
  * 
  * @author Marc Schr&ouml;der, oytun.turk
  * @see MaryGUIClient A GUI interface to this client
- * @link {marytts.server#MaryServer} Description of the MARY protocol.
+ * @see marytts.server.MaryServer Description of the MARY protocol.
  */
 
 public class MaryHttpClient extends MaryClient {