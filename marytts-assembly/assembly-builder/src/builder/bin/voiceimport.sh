260ca64d0e94f50081ac34780946f225119ef5b3
Sebastien Le Maguer <slemaguer@coli.uni-saarland.de>
2016-02-15 23:06:30 +0100

    remove maven

--- marytts-assembly/assembly-builder/src/builder/bin/voiceimport.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-BINDIR="`dirname "$0"`"
-export MARY_BASE="`(cd "$BINDIR"/.. ; pwd)`"
-java -showversion -Xmx1024m -Dmary.base="$MARY_BASE" -cp "$MARY_BASE/lib/*" marytts.tools.voiceimport.DatabaseImportMain $*
