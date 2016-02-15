2e227f32f086381d9cc1a493e198f8c23469129a
Sebastien Le Maguer <slemaguer@coli.uni-saarland.de>
2016-02-15 23:06:30 +0100

    remove maven

--- marytts-assembly/assembly-builder/src/builder/bin/lexicon_lts_pos_builder.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-BINDIR="`dirname "$0"`"
-export MARY_BASE="`(cd "$BINDIR"/.. ; pwd)`"
-java -showversion -Xmx4096m  -cp "$MARY_BASE/lib/*" marytts.tools.transcription.LTSLexiconPOSBuilder $*
--- marytts-assembly/assembly-runtime/src/runtime/bin/marytts-component-installer.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set BINDIR=%~dp0
-call :RESOLVE "%BINDIR%\.." MARY_BASE
-java -showversion -Dmary.base="%MARY_BASE%" -cp ".;%MARY_BASE%\lib\*" marytts.tools.install.InstallerGUI
-goto :EOF
-
-:RESOLVE
-set %2=%~f1
-goto :EOF
