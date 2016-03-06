d21d18f9d156b8d7d1ca14b7ff665d043ce4fbbd
Sebastien Le Maguer <slemaguer@coli.uni-saarland.de>
2016-03-06 19:35:54 +0100

    skeleton for XMLSerializer

---
+++ marytts-common/src/main/java/marytts/io/XMLSerializer.java
@@ -0,0 +1,75 @@
+package marytts.io;
+
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
+x
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+
+import marytts.data.Utterance;
+
+
+/**
+ *
+ *
+ * @author <a href="mailto:slemaguer@coli.uni-saarland.de">Sébastien Le Maguer</a>
+ */
+public class XMLSerializer
+{
+
+    public Utterance load(File file)
+        throws MaryIOException
+    {
+        return new Utterance("");
+    }
+
+
+    public void save(File file, Utterance utt)
+        throws MaryIOException
+    {
+    }
+
+    public String toString(Utterance utt)
+        throws MaryIOException
+    {
+        try
+        {
+            Document doc = generateDocument(utt);
+            DOMSource domSource = new DOMSource(doc);
+            StringWriter writer = new StringWriter();
+            StreamResult result = new StreamResult(writer);
+            TransformerFactory tf = TransformerFactory.newInstance();
+            Transformer transformer = tf.newTransformer();
+            transformer.transform(domSource, result);
+
+            return writer.toString();
+        }
+        catch (TransformerConfigurationException ex)
+        {
+            throw new MaryIOException("Transformer configuration exception", ex);
+        }
+        catch (TransformerException ex)
+        {
+            throw new MaryIOException("Transformer exception", ex);
+        }
+    }
+
+    private Document generateDocument(Utterance utt)
+        throws MaryIOException
+    {
+        Document doc = null;
+
+        return doc;
+    }
+}
