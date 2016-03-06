ba852a250ae7cfaf47d81c4bbd03ca3db8dec0db
Sebastien Le Maguer <slemaguer@coli.uni-saarland.de>
2016-03-06 19:34:47 +0100

    prepare data baseline

--- marytts-common/src/main/java/marytts/data/item/Phrase.java
+++ marytts-common/src/main/java/marytts/data/item/Phrase.java
@@ -10,4 +10,31 @@ import java.util.ArrayList;
 public class Phrase extends Item
 {
 
+	private ArrayList<Word> m_words;
+	private String m_text;
+	
+	public Phrase(String text)
+	{
+		setText(text);
+	}
+	
+	public ArrayList<Word> getWords() 
+	{
+		return m_words;
+	}
+
+	public void setWords(ArrayList<Word> words) 
+	{
+		m_words = words;
+	}
+
+	public String getText() 
+	{
+		return m_text;
+	}
+
+	protected void setText(String text) 
+	{
+		m_text = text;
+	}
 }
--- marytts-common/src/main/java/marytts/data/item/Word.java
+++ marytts-common/src/main/java/marytts/data/item/Word.java
@@ -9,5 +9,57 @@ import java.util.ArrayList;
  */
 public class Word extends Item
 {
-
+	private String m_POS;
+	private String m_text;
+	private ArrayList<Syllable> m_syllables;
+	private String m_g2p_method;
+	
+	public Word(String text)
+	{
+		setText(text);
+	}
+	
+	
+	public String getPOS() 
+	{
+		return m_POS;
+	}
+	
+	public void setPOS(String POS) 
+	{
+		m_POS = POS;
+	}
+	
+	
+	public String getText() 
+	{
+		return m_text;
+	}
+	
+	protected void setText(String text) 
+	{
+		m_text = text;
+	}
+	
+	
+	public ArrayList<Syllable> getSyllables() 
+	{
+		return m_syllables;
+	}
+	
+	public void setSyllables(ArrayList<Syllable> syllables) 
+	{
+		m_syllables = syllables;
+	}
+	
+	
+	public String getG2PMethod() 
+	{
+		return m_g2p_method;
+	}
+	
+	public void setG2PMethod(String g2p_method) 
+	{
+		m_g2p_method = g2p_method;
+	}
 }
