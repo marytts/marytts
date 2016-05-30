53a9f0de9de3c00f82e5a94e3529b179e5377c61
Sebastien Le Maguer <slemaguer@coli.uni-saarland.de>
2016-05-30 06:28:08 +0200

    XML Stripped from the RU phonemiser

--- marytts-languages/marytts-lang-ru/src/main/java/marytts/language/ru/Phonemiser.java
+++ marytts-languages/marytts-lang-ru/src/main/java/marytts/language/ru/Phonemiser.java
@@ -43,11 +43,14 @@ import marytts.server.MaryProperties;
 import marytts.util.MaryRuntimeUtils;
 import marytts.util.dom.MaryDomUtils;
 
-import org.w3c.dom.DOMException;
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.traversal.NodeIterator;
-import org.xml.sax.SAXException;
+
+
+import marytts.io.XMLSerializer;
+import marytts.data.Utterance;
+import marytts.data.item.linguistic.Word;
+import marytts.data.item.phonology.Phoneme;
+
 
 /**
  * Russian phonemiser module
@@ -63,15 +66,15 @@ public class Phonemiser extends InternalModule {
 
 	protected AllophoneSet allophoneSet;
 
-	public Phonemiser(String propertyPrefix) throws IOException, SAXException, ParserConfigurationException,
-			MaryConfigurationException {
+	public Phonemiser(String propertyPrefix) throws IOException, ParserConfigurationException,
+        MaryConfigurationException {
 		this("Phonemiser", MaryDataType.PARTSOFSPEECH, MaryDataType.PHONEMES, propertyPrefix + "allophoneset", propertyPrefix
-				+ "userdict");
+             + "userdict");
 	}
 
 	/**
 	 * Constructor providing the individual filenames of files that are required.
-	 * 
+	 *
 	 * @param componentName
 	 *            componentName
 	 * @param inputType
@@ -84,15 +87,15 @@ public class Phonemiser extends InternalModule {
 	 *            userdictProperty
 	 * @throws IOException
 	 *             IOException
-	 * @throws SAXException
-	 *             SAXException
 	 * @throws ParserConfigurationException
 	 *             ParserConfigurationException
 	 * @throws MaryConfigurationException
 	 *             MaryConfigurationException
 	 */
 	public Phonemiser(String componentName, MaryDataType inputType, MaryDataType outputType, String allophonesProperty,
-			String userdictProperty) throws IOException, SAXException, ParserConfigurationException, MaryConfigurationException {
+                      String userdictProperty)
+        throws IOException, ParserConfigurationException, MaryConfigurationException
+    {
 		super(componentName, inputType, outputType, MaryRuntimeUtils.needAllophoneSet(allophonesProperty).getLocale());
 		allophoneSet = MaryRuntimeUtils.needAllophoneSet(allophonesProperty);
 		// userdict is optional
@@ -102,198 +105,206 @@ public class Phonemiser extends InternalModule {
 			userdict = readLexicon(userdictFilename);
 	}
 
-	public MaryData process(MaryData d) throws Exception {
-		Document doc = d.getDocument();
-		NodeIterator it = MaryDomUtils.createNodeIterator(doc, doc, MaryXML.TOKEN);
-		Element t = null;
-		while ((t = (Element) it.nextNode()) != null) {
-			String text;
+	public MaryData process(MaryData d)
+        throws Exception
+    {
 
-			// Do not touch tokens for which a transcription is already
-			// given (exception: transcription contains a '*' character:
-			if (t.hasAttribute("ph") && !t.getAttribute("ph").contains("*")) {
-				continue;
-			}
-			if (t.hasAttribute("sounds_like"))
-				text = t.getAttribute("sounds_like");
-			else
-				text = MaryDomUtils.tokenText(t);
+        XMLSerializer xml_ser = new XMLSerializer();
+        Utterance utt = xml_ser.unpackDocument(d.getDocument());
 
-			String pos = null;
-			// use part-of-speech if available
-			if (t.hasAttribute("pos")) {
-				pos = t.getAttribute("pos");
-			}
+        for (Word w: utt.getAllWords())
+        {
+            String text;
 
-			if (text != null && !text.equals("")) {
-				// If text consists of several parts (e.g., because that was
-				// inserted into the sounds_like attribute), each part
-				// is transcribed separately.
-				StringBuilder ph = new StringBuilder();
-				String g2pMethod = null;
-				StringTokenizer st = new StringTokenizer(text, " -");
-				while (st.hasMoreTokens()) {
-					String graph = st.nextToken();
-					StringBuilder helper = new StringBuilder();
-					if (pos.equals("$PUNCT")) {
-						continue;
-					}
-					String phon = phonemise(graph, pos, helper);
-					if (ph.length() == 0) { // first part
-						// The g2pMethod of the combined beast is
-						// the g2pMethod of the first constituant.
-						g2pMethod = helper.toString();
-						ph.append(phon);
-					} else { // following parts
-						ph.append(" - ");
-						// Reduce primary to secondary stress:
-						ph.append(phon.replace('\'', ','));
-					}
-				}
 
-				if (ph != null && ph.length() > 0) {
-					setPh(t, ph.toString());
-					t.setAttribute("g2p_method", g2pMethod);
-				}
-			}
-		}
-		MaryData result = new MaryData(outputType(), d.getLocale());
-		result.setDocument(doc);
-		return result;
-	}
+            // Do not touch tokens for which a transcription is already
+            // given (exception: transcription contains a '*' character:
+            if (w.getPhonemes().size() > 0)
+                continue;
 
-	/**
-	 * Phonemise the word text. This starts with a simple lexicon lookup, followed by some heuristics, and finally applies
-	 * letter-to-sound rules if nothing else was successful.
-	 * 
-	 * @param text
-	 *            the textual (graphemic) form of a word.
-	 * @param pos
-	 *            the part-of-speech of the word
-	 * @param g2pMethod
-	 *            This is an awkward way to return a second String parameter via a StringBuilder. If a phonemisation of the text
-	 *            is found, this parameter will be filled with the method of phonemisation ("lexicon", ... "rules").
-	 * @return a phonemisation of the text if one can be generated, or null if no phonemisation method was successful.
-	 * @throws IOException
-	 *             IOException
-	 */
-	public String phonemise(String text, String pos, StringBuilder g2pMethod) throws IOException {
-		// First, try a simple userdict lookup:
+            if (w.soundsLike() != null)
+                text = w.soundsLike();
+            else
+                text = w.getText();
 
-		String result = userdictLookup(text, pos);
-		if (result != null) {
-			g2pMethod.append("userdict");
-			return result;
-		}
-		return null;
-	}
+            // use part-of-speech if available
+            String pos = w.getPOS();
 
-	/**
-	 * look a given text up in the userdict. part-of-speech is used in case of ambiguity.
-	 * 
-	 * @param text
-	 *            text
-	 * @param pos
-	 *            pos
-	 * @return null if userdict == null || text == null || text.length() == 0, null if entries == null, transcr otherwise
-	 */
-	public String userdictLookup(String text, String pos) {
-		if (userdict == null || text == null || text.length() == 0)
-			return null;
-		List<String> entries = userdict.get(text);
-		// If entry is not found directly, try the following changes:
-		// - lowercase the word
-		// - all lowercase but first uppercase
-		if (entries == null) {
-			text = text.toLowerCase(getLocale());
-			entries = userdict.get(text);
-		}
-		if (entries == null) {
-			text = text.substring(0, 1).toUpperCase(getLocale()) + text.substring(1);
-			entries = userdict.get(text);
-		}
 
-		if (entries == null)
-			return null;
+            ArrayList<Phoneme> new_phonemes = new ArrayList<Phoneme>();
 
-		String transcr = null;
-		for (String entry : entries) {
-			String[] parts = entry.split("\\|");
-			transcr = parts[0];
-			if (parts.length > 1 && pos != null) {
-				StringTokenizer tokenizer = new StringTokenizer(entry);
-				while (tokenizer.hasMoreTokens()) {
-					String onePos = tokenizer.nextToken();
-					if (pos.equals(onePos))
-						return transcr; // found
-				}
-			}
-		}
-		// no match of POS: return last entry
-		return transcr;
-	}
+            // If text consists of several parts (e.g., because that was
+            // inserted into the sounds_like attribute), each part
+            // is transcribed separately.
+            StringBuilder ph = new StringBuilder();
+            String g2p_method = null;
+            StringTokenizer st = new StringTokenizer(text, " -");
+            while (st.hasMoreTokens()) {
+                String graph = st.nextToken();
+                StringBuilder helper = new StringBuilder();
+                if (pos.equals("$PUNCT")) {
+                    continue;
+                }
 
-	/**
-	 * Read a lexicon. Lines must have the format
-	 * 
-	 * graphemestring | phonestring | optional-parts-of-speech
-	 * 
-	 * The pos-item is optional. Different pos's belonging to one grapheme chain may be separated by whitespace
-	 * 
-	 * 
-	 * @param lexiconFilename
-	 *            lexiconFilename
-	 * @throws IOException
-	 *             IOException
-	 * @return fLexicon
-	 */
-	protected Map<String, List<String>> readLexicon(String lexiconFilename) throws IOException {
-		String line;
-		Map<String, List<String>> fLexicon = new HashMap<String, List<String>>();
+                String phon = phonemise(graph, pos, helper);
 
-		BufferedReader lexiconFile = new BufferedReader(new InputStreamReader(new FileInputStream(lexiconFilename), "UTF-8"));
-		while ((line = lexiconFile.readLine()) != null) {
-			// Ignore empty lines and comments:
-			if (line.trim().equals("") || line.startsWith("#"))
-				continue;
 
-			String[] lineParts = line.split("\\s*\\|\\s*");
-			String graphStr = lineParts[0];
-			String phonStr = lineParts[1];
-			try {
-				allophoneSet.splitIntoAllophones(phonStr);
-			} catch (RuntimeException re) {
-				logger.warn("Lexicon '" + lexiconFilename + "': invalid entry for '" + graphStr + "'", re);
-			}
-			String phonPosStr = phonStr;
-			if (lineParts.length > 2) {
-				String pos = lineParts[2];
-				if (!pos.trim().equals(""))
-					phonPosStr += "|" + pos;
-			}
+                // FIXME what does it mean : null result should not be processed
+                if (phon == null)
+                    continue;
 
-			List<String> transcriptions = fLexicon.get(graphStr);
-			if (null == transcriptions) {
-				transcriptions = new ArrayList<String>();
-				fLexicon.put(graphStr, transcriptions);
-			}
-			transcriptions.add(phonPosStr);
-		}
-		lexiconFile.close();
-		return fLexicon;
-	}
+                if (ph.length() == 0)
+                    g2p_method = helper.toString();
 
-	protected void setPh(Element t, String ph) {
-		if (!t.getTagName().equals(MaryXML.TOKEN))
-			throw new DOMException(DOMException.INVALID_ACCESS_ERR, "Only t elements allowed, received " + t.getTagName() + ".");
-		if (t.hasAttribute("ph")) {
-			String prevPh = t.getAttribute("ph");
-			// In previous sampa, replace star with sampa:
-			String newPh = prevPh.replaceFirst("\\*", ph);
-			t.setAttribute("ph", newPh);
-		} else {
-			t.setAttribute("ph", ph);
-		}
-	}
+                // FIXME: hardcoded here, not really good
+                String stress = null;
+                if (phon.contains("'"))
+                {
+                    stress = ",";
+                    phon.replaceAll("'", "");
+                }
 
+                new_phonemes.add(new Phoneme(phon, stress));
+            }
+
+
+            if (new_phonemes.size() > 0)
+            {
+                // Adapt phoneme
+                w.setPhonemes(new_phonemes);
+
+                // Adapt G2P method
+                w.setG2PMethod(g2p_method);
+            }
+        }
+
+
+        MaryData result = new MaryData(outputType(), d.getLocale());
+        result.setDocument(xml_ser.generateDocument(utt));
+        return result;
+    }
+
+    /**
+     * Phonemise the word text. This starts with a simple lexicon lookup, followed by some heuristics, and finally applies
+     * letter-to-sound rules if nothing else was successful.
+     *
+     * @param text
+     *            the textual (graphemic) form of a word.
+     * @param pos
+     *            the part-of-speech of the word
+     * @param g2pMethod
+     *            This is an awkward way to return a second String parameter via a StringBuilder. If a phonemisation of the text
+     *            is found, this parameter will be filled with the method of phonemisation ("lexicon", ... "rules").
+     * @return a phonemisation of the text if one can be generated, or null if no phonemisation method was successful.
+     * @throws IOException
+     *             IOException
+     */
+    public String phonemise(String text, String pos, StringBuilder g2pMethod)
+        throws IOException
+    {
+        // First, try a simple userdict lookup:
+
+        String result = userdictLookup(text, pos);
+        if (result != null) {
+            g2pMethod.append("userdict");
+            return result;
+        }
+        return null;
+    }
+
+    /**
+     * look a given text up in the userdict. part-of-speech is used in case of ambiguity.
+     *
+     * @param text
+     *            text
+     * @param pos
+     *            pos
+     * @return null if userdict == null || text == null || text.length() == 0, null if entries == null, transcr otherwise
+     */
+    public String userdictLookup(String text, String pos)
+    {
+        if (userdict == null || text == null || text.length() == 0)
+            return null;
+        List<String> entries = userdict.get(text);
+        // If entry is not found directly, try the following changes:
+        // - lowercase the word
+        // - all lowercase but first uppercase
+        if (entries == null) {
+            text = text.toLowerCase(getLocale());
+            entries = userdict.get(text);
+        }
+        if (entries == null) {
+            text = text.substring(0, 1).toUpperCase(getLocale()) + text.substring(1);
+            entries = userdict.get(text);
+        }
+
+        if (entries == null)
+            return null;
+
+        String transcr = null;
+        for (String entry : entries) {
+            String[] parts = entry.split("\\|");
+            transcr = parts[0];
+            if (parts.length > 1 && pos != null) {
+                StringTokenizer tokenizer = new StringTokenizer(entry);
+                while (tokenizer.hasMoreTokens()) {
+                    String onePos = tokenizer.nextToken();
+                    if (pos.equals(onePos))
+                        return transcr; // found
+                }
+            }
+        }
+        // no match of POS: return last entry
+        return transcr;
+    }
+
+    /**
+     * Read a lexicon. Lines must have the format
+     *
+     * graphemestring | phonestring | optional-parts-of-speech
+     *
+     * The pos-item is optional. Different pos's belonging to one grapheme chain may be separated by whitespace
+     *
+     *
+     * @param lexiconFilename
+     *            lexiconFilename
+     * @throws IOException
+     *             IOException
+     * @return fLexicon
+     */
+    protected Map<String, List<String>> readLexicon(String lexiconFilename) throws IOException {
+        String line;
+        Map<String, List<String>> fLexicon = new HashMap<String, List<String>>();
+
+        BufferedReader lexiconFile = new BufferedReader(new InputStreamReader(new FileInputStream(lexiconFilename), "UTF-8"));
+        while ((line = lexiconFile.readLine()) != null) {
+            // Ignore empty lines and comments:
+            if (line.trim().equals("") || line.startsWith("#"))
+                continue;
+
+            String[] lineParts = line.split("\\s*\\|\\s*");
+            String graphStr = lineParts[0];
+            String phonStr = lineParts[1];
+            try {
+                allophoneSet.splitIntoAllophones(phonStr);
+            } catch (RuntimeException re) {
+                logger.warn("Lexicon '" + lexiconFilename + "': invalid entry for '" + graphStr + "'", re);
+            }
+            String phonPosStr = phonStr;
+            if (lineParts.length > 2) {
+                String pos = lineParts[2];
+                if (!pos.trim().equals(""))
+                    phonPosStr += "|" + pos;
+            }
+
+            List<String> transcriptions = fLexicon.get(graphStr);
+            if (null == transcriptions) {
+                transcriptions = new ArrayList<String>();
+                fLexicon.put(graphStr, transcriptions);
+            }
+            transcriptions.add(phonPosStr);
+        }
+        lexiconFile.close();
+        return fLexicon;
+    }
 }
