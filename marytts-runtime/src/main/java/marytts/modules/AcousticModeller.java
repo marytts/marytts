2e361e6d30b9c0b254a356cacda65aa5a474071e
Ingmar Steiner <steiner@coli.uni-saarland.de>
2016-06-29 06:47:08 +0200

    auto-format in Eclipse

--- marytts-runtime/src/main/java/marytts/modules/AcousticModeller.java
+++ marytts-runtime/src/main/java/marytts/modules/AcousticModeller.java
@@ -97,8 +97,8 @@ public class AcousticModeller extends InternalModule {
 	 *             Exception
 	 */
 	public AcousticModeller(String locale, String propertyPrefix) throws Exception {
-		this(MaryUtils.string2locale(locale), propertyPrefix, FeatureRegistry.getFeatureProcessorManager(MaryUtils
-				.string2locale(locale)));
+		this(MaryUtils.string2locale(locale), propertyPrefix,
+				FeatureRegistry.getFeatureProcessorManager(MaryUtils.string2locale(locale)));
 	}
 
 	/**
@@ -115,8 +115,8 @@ public class AcousticModeller extends InternalModule {
 	 *             Exception
 	 */
 	public AcousticModeller(String locale, String propertyPrefix, String featprocClassInfo) throws Exception {
-		this(MaryUtils.string2locale(locale), propertyPrefix, (FeatureProcessorManager) MaryRuntimeUtils
-				.instantiateObject(featprocClassInfo));
+		this(MaryUtils.string2locale(locale), propertyPrefix,
+				(FeatureProcessorManager) MaryRuntimeUtils.instantiateObject(featprocClassInfo));
 	}
 
 	/**
@@ -379,7 +379,8 @@ public class AcousticModeller extends InternalModule {
 
 			// at this point, no TBU should be null:
 			if (firstVoicedSegment == null || firstVowel == null || lastVoicedSegment == null) {
-				logger.debug("WARNING: could not identify F0 anchors in malformed syllable: '" + element.getAttribute("ph") + "'");
+				logger.debug(
+						"WARNING: could not identify F0 anchors in malformed syllable: '" + element.getAttribute("ph") + "'");
 			} else {
 				// we have what we need, append to Lists:
 				firstVoicedSegments.add(firstVoicedSegment);
--- marytts-runtime/src/main/java/marytts/modules/RealisedDurationsExtractor.java
+++ marytts-runtime/src/main/java/marytts/modules/RealisedDurationsExtractor.java
@@ -47,8 +47,8 @@ public class RealisedDurationsExtractor extends InternalModule {
 		MaryData result = new MaryData(outputType(), d.getLocale());
 		StringBuilder buf = new StringBuilder();
 		buf.append("#\n");
-		NodeIterator ni = ((DocumentTraversal) doc).createNodeIterator(doc, NodeFilter.SHOW_ELEMENT, new NameNodeFilter(
-				new String[] { MaryXML.SENTENCE, MaryXML.PHONE, MaryXML.BOUNDARY }), false);
+		NodeIterator ni = ((DocumentTraversal) doc).createNodeIterator(doc, NodeFilter.SHOW_ELEMENT,
+				new NameNodeFilter(new String[] { MaryXML.SENTENCE, MaryXML.PHONE, MaryXML.BOUNDARY }), false);
 		Element element = null;
 		float end = 0.f;
 		float sentenceEnd = 0;
