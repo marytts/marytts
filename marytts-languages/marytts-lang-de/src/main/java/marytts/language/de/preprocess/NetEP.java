98ec8aecdfd18f042481ed0bfbdc380f1ae81b9d
Sebastien Le Maguer <slemaguer@coli.uni-saarland.de>
2016-09-23 04:24:35 +0200

    Solving #606

--- marytts-languages/marytts-lang-de/src/main/java/marytts/language/de/preprocess/NetEP.java
+++ marytts-languages/marytts-lang-de/src/main/java/marytts/language/de/preprocess/NetEP.java
@@ -104,7 +104,7 @@ public class NetEP extends ExpansionPattern {
 	/**
 	 * Inform whether this module performs a full expansion of the input, or whether other patterns should be applied after this
 	 * one.
-	 * 
+	 *
 	 * @return false
 	 */
 	protected boolean doesFullExpansion() {
@@ -210,8 +210,8 @@ public class NetEP extends ExpansionPattern {
 		if (domain.endsWith(".com")) {
 			toExpand = domain.substring(0, domain.length() - 4);
 			domainSuffix = " dot['dOt] com['kOm]";
-		} else if (domain.lastIndexOf(".") == domain.length() - 3) {
-			// Domains consisting of two characters (de, ch, uk, ...)
+        } else if (domain.lastIndexOf(".") != -1 && domain.lastIndexOf(".") == domain.length() - 3) {
+            // Domains consisting of two characters (de, ch, uk, ...)
 			// are to be spelled out
 			toExpand = domain.substring(0, domain.length() - 3);
 			logger.debug("toExpand = `" + toExpand + "'");
@@ -237,140 +237,140 @@ public class NetEP extends ExpansionPattern {
 
 /*
  * Request for Comments: 2822 3. Syntax
- * 
+ *
  * 3.2.4. Atom
- * 
+ *
  * Several productions in structured header field bodies are simply strings of certain basic characters. Such productions are
  * called atoms.
- * 
+ *
  * Some of the structured header field bodies also allow the period character (".", ASCII value 46) within runs of atext. An
  * additional "dot-atom" token is defined for those purposes.
- * 
+ *
  * atext = ALPHA / DIGIT / ; Any character except controls, "!" / "#" / ; SP, and specials. "$" / "%" / ; Used for atoms "&" / "'"
  * / "*" / "+" / "-" / "/" / "=" / "?" / "^" / "_" / "`" / "{" / "|" / "}" / "~"
- * 
+ *
  * atom = [CFWS] 1*atext [CFWS]
- * 
+ *
  * dot-atom = [CFWS] dot-atom-text [CFWS]
- * 
+ *
  * dot-atom-text = 1*atext *("." 1*atext)
- * 
+ *
  * Both atom and dot-atom are interpreted as a single unit, comprised of the string of characters that make it up. Semantically,
  * the optional comments and FWS surrounding the rest of the characters are not part of the atom; the atom is only the run of
  * atext characters in an atom, or the atext and "." characters in a dot-atom.
- * 
+ *
  * 3.4. Address Specification
- * 
+ *
  * Addresses occur in several message header fields to indicate senders and recipients of messages. An address may either be an
  * individual mailbox, or a group of mailboxes.
- * 
+ *
  * address = mailbox / group
- * 
+ *
  * mailbox = name-addr / addr-spec
- * 
+ *
  * name-addr = [display-name] angle-addr
- * 
+ *
  * angle-addr = [CFWS] "<" addr-spec ">" [CFWS] / obs-angle-addr
- * 
+ *
  * group = display-name ":" [mailbox-list / CFWS] ";" [CFWS]
- * 
+ *
  * display-name = phrase
- * 
+ *
  * mailbox-list = (mailbox *("," mailbox)) / obs-mbox-list
- * 
+ *
  * address-list = (address *("," address)) / obs-addr-list
- * 
+ *
  * A mailbox receives mail. It is a conceptual entity which does not necessarily pertain to file storage. For example, some sites
  * may choose to print mail on a printer and deliver the output to the addressee's desk. Normally, a mailbox is comprised of two
  * parts: (1) an optional display name that indicates the name of the recipient (which could be a person or a system) that could
  * be displayed to the user of a mail application, and (2) an addr-spec address enclosed in angle brackets ("<" and ">"). There is
  * also an alternate simple form of a mailbox where the addr-spec address appears alone, without the recipient's name or the angle
  * brackets. The Internet addr-spec address is described in section 3.4.1.
- * 
+ *
  * Note: Some legacy implementations used the simple form where the addr-spec appears without the angle brackets, but included the
  * name of the recipient in parentheses as a comment following the addr-spec. Since the meaning of the information in a comment is
  * unspecified, implementations SHOULD use the full name-addr form of the mailbox, instead of the legacy form, to specify the
  * display name associated with a mailbox. Also, because some legacy implementations interpret the comment, comments generally
  * SHOULD NOT be used in address fields to avoid confusing such implementations.
- * 
+ *
  * When it is desirable to treat several mailboxes as a single unit (i.e., in a distribution list), the group construct can be
  * used. The group construct allows the sender to indicate a named group of recipients. This is done by giving a display name for
  * the group, followed by a colon, followed by a comma separated list of any number of mailboxes (including zero and one), and
  * ending with a semicolon. Because the list of mailboxes can be empty, using the group construct is also a simple way to
  * communicate to recipients that the message was sent to one or more named sets of recipients, without actually providing the
  * individual mailbox address for each of those recipients.
- * 
+ *
  * 3.4.1. Addr-spec specification
- * 
+ *
  * An addr-spec is a specific Internet identifier that contains a locally interpreted string followed by the at-sign character
  * ("@", ASCII value 64) followed by an Internet domain. The locally interpreted string is either a quoted-string or a dot-atom.
  * If the string can be represented as a dot-atom (that is, it contains no characters other than atext characters or "."
  * surrounded by atext
- * 
+ *
  * characters), then the dot-atom form SHOULD be used and the quoted-string form SHOULD NOT be used. Comments and folding white
  * space SHOULD NOT be used around the "@" in the addr-spec.
- * 
+ *
  * addr-spec = local-part "@" domain
- * 
+ *
  * local-part = dot-atom / quoted-string / obs-local-part
- * 
+ *
  * domain = dot-atom / domain-literal / obs-domain
- * 
+ *
  * domain-literal = [CFWS] "[" *([FWS] dcontent) [FWS] "]" [CFWS]
- * 
+ *
  * dcontent = dtext / quoted-pair
- * 
+ *
  * dtext = NO-WS-CTL / ; Non white space controls
- * 
+ *
  * %d33-90 / ; The rest of the US-ASCII %d94-126 ; characters not including "[", ; "]", or "\"
- * 
+ *
  * The domain portion identifies the point to which the mail is delivered. In the dot-atom form, this is interpreted as an
  * Internet domain name (either a host name or a mail exchanger name) as described in [STD3, STD13, STD14]. In the domain-literal
  * form, the domain is interpreted as the literal Internet address of the particular host. In both cases, how addressing is used
  * and how messages are transported to a particular host is covered in the mail transport document [RFC2821]. These mechanisms are
  * outside of the scope of this document.
- * 
+ *
  * The local-part portion is a domain dependent string. In addresses, it is simply interpreted on the particular host as a name of
  * a particular mailbox.
  */
 
 /*
  * RFC 2396 URI Generic Syntax August 1998
- * 
- * 
+ *
+ *
  * B. Parsing a URI Reference with a Regular Expression
- * 
+ *
  * As described in Section 4.3, the generic URI syntax is not sufficient to disambiguate the components of some forms of URI.
  * Since the "greedy algorithm" described in that section is identical to the disambiguation method used by POSIX regular
  * expressions, it is natural and commonplace to use a regular expression for parsing the potential four components and fragment
  * identifier of a URI reference.
- * 
+ *
  * The following line is the regular expression for breaking-down a URI reference into its components.
- * 
+ *
  * ^(([^:/?#]+):)?(//([^/?#]*))?([^?#]*)(\?([^#]*))?(#(.*))? 12 3 4 5 6 7 8 9
- * 
+ *
  * The numbers in the second line above are only to assist readability; they indicate the reference points for each subexpression
  * (i.e., each paired parenthesis). We refer to the value matched for subexpression <n> as $<n>. For example, matching the above
  * expression to
- * 
+ *
  * http://www.ics.uci.edu/pub/ietf/uri/#Related
- * 
+ *
  * results in the following subexpression matches:
- * 
+ *
  * $1 = http: $2 = http $3 = //www.ics.uci.edu $4 = www.ics.uci.edu $5 = /pub/ietf/uri/ $6 = <undefined> $7 = <undefined> $8 =
  * #Related $9 = Related
- * 
+ *
  * where <undefined> indicates that the component is not present, as is the case for the query component in the above example.
  * Therefore, we can determine the value of the four components and fragment as
- * 
+ *
  * scheme = $2 authority = $4 path = $5 query = $7 fragment = $9
- * 
+ *
  * and, going in the opposite direction, we can recreate a URI reference from its components using the algorithm in step 7 of
  * Section 5.2.
- * 
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
+ *
  * Berners-Lee, et. al. Standards Track [Page 29]
  */
