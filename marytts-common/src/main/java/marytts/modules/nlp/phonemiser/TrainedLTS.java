4f7c34a8d7e02c0b8b37a3d5143a412721d6177f
Sebastien Le Maguer <slemaguer@coli.uni-saarland.de>
2017-01-25 15:47:14 +0100

    Get rid of tree in trained lts

--- marytts-common/src/main/java/marytts/modules/nlp/phonemiser/TrainedLTS.java
+++ marytts-common/src/main/java/marytts/modules/nlp/phonemiser/TrainedLTS.java
@@ -25,12 +25,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Properties;
+import marytts.cart.DecisionTree;
 
-import org.hsqldb.error.Error;
-
-import marytts.modeling.cart.CART;
-import marytts.modeling.cart.LeafNode.StringAndFloatLeafNode;
-import marytts.modeling.cart.io.MaryCARTReader;
 import marytts.exceptions.MaryConfigurationException;
 import marytts.modeling.features.FeatureDefinition;
 import marytts.modeling.features.FeatureVector;
@@ -45,7 +41,7 @@ import marytts.modeling.features.FeatureVector;
 public class TrainedLTS {
 	public static final String PREDICTED_STRING_FEATURENAME = "predicted-string";
 
-	private CART tree;
+	private DecisionTree tree;
 	private FeatureDefinition featureDefinition;
 	private int indexPredictedFeature;
 	private int context;
@@ -92,9 +88,10 @@ public class TrainedLTS {
 		this(aPhonSet, treeStream, true);
 	}
 
-	public TrainedLTS(AllophoneSet aPhonSet, CART predictionTree) {
+	public TrainedLTS(AllophoneSet aPhonSet, DecisionTree predictionTree) {
 		this.allophoneSet = aPhonSet;
 		this.tree = predictionTree;
+        /*
 		this.featureDefinition = tree.getFeatureDefinition();
 		this.indexPredictedFeature = featureDefinition.getFeatureIndex(PREDICTED_STRING_FEATURENAME);
 		Properties props = tree.getProperties();
@@ -102,6 +99,7 @@ public class TrainedLTS {
 			throw new IllegalArgumentException("Prediction tree does not contain properties");
 		convertToLowercase = Boolean.parseBoolean(props.getProperty("lowercase"));
 		context = Integer.parseInt(props.getProperty("context"));
+        */
 	}
 
 	/**
@@ -116,16 +114,16 @@ public class TrainedLTS {
 	 *             MaryConfigurationException
 	 */
 	public void loadTree(InputStream treeStream) throws IOException, MaryConfigurationException {
-		MaryCARTReader cartReader = new MaryCARTReader();
-		this.tree = cartReader.loadFromStream(treeStream);
-		this.featureDefinition = tree.getFeatureDefinition();
-		this.indexPredictedFeature = featureDefinition.getFeatureIndex(PREDICTED_STRING_FEATURENAME);
-		this.convertToLowercase = false;
-		Properties props = tree.getProperties();
-		if (props == null)
-			throw new IllegalArgumentException("Prediction tree does not contain properties");
-		convertToLowercase = Boolean.parseBoolean(props.getProperty("lowercase"));
-		context = Integer.parseInt(props.getProperty("context"));
+		// MaryCARTReader cartReader = new MaryCARTReader();
+		// this.tree = cartReader.loadFromStream(treeStream);
+		// this.featureDefinition = tree.getFeatureDefinition();
+		// this.indexPredictedFeature = featureDefinition.getFeatureIndex(PREDICTED_STRING_FEATURENAME);
+		// this.convertToLowercase = false;
+		// Properties props = tree.getProperties();
+		// if (props == null)
+		// 	throw new IllegalArgumentException("Prediction tree does not contain properties");
+		// convertToLowercase = Boolean.parseBoolean(props.getProperty("lowercase"));
+		// context = Integer.parseInt(props.getProperty("context"));
 	}
 
 	public String predictPronunciation(String graphemes) {
@@ -143,21 +141,21 @@ public class TrainedLTS {
 
 				String grAtPos = (pos < 0 || pos >= graphemes.length()) ? "null" : graphemes.substring(pos, pos + 1);
 
-				try {
-					byteFeatures[fnr] = this.tree.getFeatureDefinition().getFeatureValueAsByte(fnr, grAtPos);
-					// ... can also try to call explicit:
-					// features[fnr] = this.fd.getFeatureValueAsByte("att"+fnr, cg.substr(pos)
-				} catch (IllegalArgumentException iae) {
-					// Silently ignore unknown characters
-					byteFeatures[fnr] = this.tree.getFeatureDefinition().getFeatureValueAsByte(fnr, "null");
-				}
+				// try {
+				// 	byteFeatures[fnr] = this.tree.getFeatureDefinition().getFeatureValueAsByte(fnr, grAtPos);
+				// 	// ... can also try to call explicit:
+				// 	// features[fnr] = this.fd.getFeatureValueAsByte("att"+fnr, cg.substr(pos)
+				// } catch (IllegalArgumentException iae) {
+				// 	// Silently ignore unknown characters
+				// 	byteFeatures[fnr] = this.tree.getFeatureDefinition().getFeatureValueAsByte(fnr, "null");
+				// }
 			}
 
 			FeatureVector fv = new FeatureVector(byteFeatures, new short[] {}, new float[] {}, 0);
 
-			StringAndFloatLeafNode leaf = (StringAndFloatLeafNode) tree.interpretToNode(fv, 0);
-			String prediction = leaf.mostProbableString(featureDefinition, indexPredictedFeature);
-			returnStr += prediction.substring(1, prediction.length() - 1);
+			// StringAndFloatLeafNode leaf = (StringAndFloatLeafNode) tree.interpretToNode(fv, 0);
+			// String prediction = leaf.mostProbableString(featureDefinition, indexPredictedFeature);
+			// returnStr += prediction.substring(1, prediction.length() - 1);
 		}
 
 		return returnStr;
@@ -177,34 +175,4 @@ public class TrainedLTS {
 	public String syllabify(String phones) throws IllegalArgumentException {
 		return allophoneSet.syllabify(phones);
 	}
-
-	public static void main(String[] args) throws IOException, MaryConfigurationException {
-
-		if (args.length < 2) {
-			System.out.println("Usage:");
-			System.out
-                .println("java marytts.modules.nlp.phonemiser.TrainedLTS allophones.xml lts-model.lts [removeTrailingOneFromPhones]");
-			System.exit(0);
-		}
-		String allophoneFile = args[0];
-		String ltsFile = args[1];
-		boolean myRemoveTrailingOneFromPhones = true;
-		if (args.length > 2) {
-			myRemoveTrailingOneFromPhones = Boolean.getBoolean(args[2]);
-		}
-
-		TrainedLTS lts = new TrainedLTS(AllophoneSet.getAllophoneSet(allophoneFile), new FileInputStream(ltsFile),
-                                        myRemoveTrailingOneFromPhones);
-
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String line;
-		while ((line = br.readLine()) != null) {
-			line = line.trim();
-			String pron = lts.predictPronunciation(line);
-			String syl = lts.syllabify(pron);
-			String sylStripped = syl.replaceAll("[-' ]+", "");
-			System.out.println(sylStripped);
-		}
-	}
-
 }
