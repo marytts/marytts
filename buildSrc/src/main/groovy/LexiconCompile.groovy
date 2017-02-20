7d516c5d0438610b3193de9a980f8e793cc11040
Ingmar Steiner <steiner@coli.uni-saarland.de>
2017-02-20 16:24:31 +0100

    ad-hoc lexicon loader
    no training yet!

--- buildSrc/src/main/groovy/LexiconCompile.groovy
+++ buildSrc/src/main/groovy/LexiconCompile.groovy
@@ -1,6 +1,8 @@
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.*
 
+import marytts.modules.phonemiser.AllophoneSet
+
 class LexiconCompile extends DefaultTask {
     @InputFile
     File allophonesFile = project.file("modules/$project.locale/lexicon/allophones.${project.locale}.xml")
@@ -20,14 +22,31 @@ class LexiconCompile extends DefaultTask {
 
     @TaskAction
     void compile() {
-        project.copy {
-            from allophonesFile, lexiconFile
-            into temporaryDir
+        // load allophoneset
+        def allophoneSet = AllophoneSet.getAllophoneSet(allophonesFile.newInputStream(), project.locale)
+
+        // read transcriptions
+        def lexicon = [:]
+        lexiconFile.eachLine('UTF-8') { line ->
+            def fields = line.split('\\s')
+            if (fields.first().startsWith('#')) {
+                // a comment
+            } else if (fields.size() == 1) {
+                project.logger.info "No transcription found in line: $line"
+            } else {
+                def (lemma, transcription) = fields.take(2)
+                // remap phones
+                phoneMapping.each { before, after ->
+                    transcription = transcription.replaceAll ~/$before/, after
+                }
+                if (allophoneSet.checkAllophoneSyntax(transcription)) {
+                    // store valid transcription
+                    lexicon[lemma] = transcription
+                } else {
+                    project.logger.warn "Invalid transcription for '$lemma': [$transcription]"
+                }
+            }
         }
-        project.javaexec {
-            classpath project.configurations.lexiconCompile
-            main 'marytts.tools.transcription.LTSLexiconPOSBuilder'
-            args "$temporaryDir/$allophonesFile.name", "$temporaryDir/$lexiconFile.name"
-        }
+        assert lexicon
     }
 }
--- buildSrc/src/main/groovy/LexiconPlugin.groovy
+++ buildSrc/src/main/groovy/LexiconPlugin.groovy
@@ -5,15 +5,6 @@ class LexiconPlugin implements Plugin<Project> {
     void apply(Project project) {
         project.pluginManager.apply(JavaPlugin)
 
-        project.configurations.create 'lexiconCompile'
-
-        project.dependencies {
-            lexiconCompile(group: 'de.dfki.mary', name: 'marytts-transcription', version: '5.2') {
-                exclude module: 'sgt'
-                exclude module: 'mwdumper'
-            }
-        }
-
         project.task('compileLexicon', type: LexiconCompile)
 
         project.processResources {
