4e40fff2e08bee4ecf3f3eb424a3de4124460653
Sebastien Le Maguer <slemaguer@coli.uni-saarland.de>
2017-02-21 12:36:02 +0100

    Adding maybe future useful classes

---
+++ marytts-common/src/main/java/marytts/cart/IndexedNode.java
@@ -0,0 +1,14 @@
+package marytts.cart;
+
+/**
+ *
+ *
+ * @author <a href="mailto:slemaguer@coli.uni-saarland.de">Sébastien Le Maguer</a>
+ */
+public interface IndexedNode
+{
+    public int getIndex(Object value);
+}
+
+
+/* IndexedNode.java ends here */
