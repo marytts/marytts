2bcd1dd13a4c1f531791f303a21307009dbf0c40
Sebastien Le Maguer <slemaguer@coli.uni-saarland.de>
2017-02-21 12:36:02 +0100

    Adding maybe future useful classes

---
+++ marytts-common/src/main/java/marytts/cart/MatchingNode.java
@@ -0,0 +1,33 @@
+package marytts.cart;
+
+import java.util.Set;
+
+/**
+ *
+ *
+ * @author <a href="mailto:slemaguer@coli.uni-saarland.de">Sébastien Le Maguer</a>
+ */
+public class MatchingNode implements IndexedNode
+{
+    private Set<String> m_matching_values;
+
+    public MatchingNode(Set<String> matching_values)
+    {
+        m_matching_values = matching_values;
+    }
+
+    public int getIndex(Object value)
+    {
+        if (value == null)
+            return 1;
+        String the_real_value = value.toString();
+
+        if (m_matching_values.contains(the_real_value))
+            return 0;
+
+        return 1;
+    }
+}
+
+
+/* MatchingNode.java ends here */
