f209d27a07492a9c2f4a4b68b4166fca24dc696c
Sebastien Le Maguer <slemaguer@coli.uni-saarland.de>
2017-05-13 23:19:03 +0200

    Sentence & word javadoc

--- marytts-common/src/main/java/marytts/data/item/linguistic/Sentence.java
+++ marytts-common/src/main/java/marytts/data/item/linguistic/Sentence.java
@@ -3,36 +3,58 @@ package marytts.data.item.linguistic;
 import java.util.ArrayList;
 
 import marytts.data.item.Item;
-import marytts.data.item.prosody.Phrase;
 
 /**
- *
+ * A class representing a pararaph. A sentence is just composed by its text
  *
  * @author <a href="mailto:slemaguer@coli.uni-saarland.de">Sébastien Le
  *         Maguer</a>
  */
 public class Sentence extends Item {
+    /** the text of the sentence */
 	private String m_text;
 
+    /**
+     * Constructor of a sentence
+     *
+     * @param text the text of the sentence
+     */
 	public Sentence(String text) {
 		super();
 		setText(text);
 	}
 
+    /**
+     * Accessor to get the text of the sentence
+     *
+     * @return the text of the sentence
+     */
 	public String getText() {
 		return m_text;
 	}
 
+    /**
+     * Accessor to set the text of the sentence
+     *
+     * @param text the text of the sentence
+     */
 	protected void setText(String text) {
 		m_text = text;
 	}
 
+	/**
+	 * Method to compare an object to the current sentence
+	 *
+	 * @param obj
+	 *            the object to compare
+	 * @return true if obj is a sentence and the sentence are equals, false else
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (!(obj instanceof Sentence))
 			return false;
 
-		Sentence sent = (Sentence) obj;
-		return sent.getText().equals(getText());
+		Sentence par = (Sentence) obj;
+		return par.getText().equals(getText());
 	}
 }
