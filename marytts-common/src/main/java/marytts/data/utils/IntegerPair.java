5ac315aa2fa127a5d8acfcb08e5472a76c508a39
Sebastien Le Maguer <slemaguer@coli.uni-saarland.de>
2017-05-13 22:57:27 +0200

    Utils javadoc done

--- marytts-common/src/main/java/marytts/data/utils/IntegerPair.java
+++ marytts-common/src/main/java/marytts/data/utils/IntegerPair.java
@@ -1,28 +1,53 @@
 package marytts.data.utils;
 
 /**
- *
+ * Encapsulation of a pair of integer for hashing purpose
  *
  * @author <a href="mailto:slemaguer@coli.uni-saarland.de">Sébastien Le
  *         Maguer</a>
  */
 public class IntegerPair {
-	private Integer left; // left member of pair
+    /** the left member of the pair */
+	private Integer left;
+
+    /** the right member of the pair */
 	private Integer right; // right member of pair
 
+    /**
+     * Constructor
+     *
+     * @param left the left member to set
+     * @param right the right member to set
+     */
 	public IntegerPair(Integer left, Integer right) {
 		this.left = left;
 		this.right = right;
 	}
 
+    /**
+     * Get the left member of the pair
+     *
+     * @return the left member of the pair
+     */
 	public Integer getLeft() {
 		return left;
 	}
 
+    /**
+     * Get the right member of the pair
+     *
+     * @return the right member of the pair
+     */
 	public Integer getRight() {
 		return right;
 	}
 
+    /**
+     * Method to compare the current pair of integer to a given one
+     *
+     * @param obj the other pair of integer
+     * @return true if obj is a pair of integer equals to the current one
+     */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -39,10 +64,8 @@ public class IntegerPair {
 	}
 
 	/**
-	 * <p>
 	 * Returns a suitable hash code. The hash code follows the definition in
 	 * {@code Map.Entry}.
-	 * </p>
 	 *
 	 * @return the hash code
 	 */
@@ -52,6 +75,11 @@ public class IntegerPair {
 		return (getLeft() == null ? 0 : getLeft().hashCode()) ^ (getRight() == null ? 0 : getRight().hashCode());
 	}
 
+	/**
+	 * Method to generate a string representation of the current pair
+	 *
+	 * @return the string representation of the current pair
+	 */
 	@Override
 	public String toString() {
 		return "(" + getLeft().toString() + ", " + getRight().toString() + ")";
--- marytts-common/src/main/java/marytts/data/utils/SequenceTypePair.java
+++ marytts-common/src/main/java/marytts/data/utils/SequenceTypePair.java
@@ -4,24 +4,44 @@ import marytts.data.Utterance;
 import marytts.data.SupportedSequenceType;
 
 /**
- *
+ * Encapsulation of a pair of sequence type for hashing purpose
  *
  * @author <a href="mailto:slemaguer@coli.uni-saarland.de">Sébastien Le
  *         Maguer</a>
  */
 public class SequenceTypePair {
-	private SupportedSequenceType left; // left member of pair
-	private SupportedSequenceType right; // right member of pair
+    /** the left member of the pair */
+	private SupportedSequenceType left;
 
+    /** the right member of the pair */
+	private SupportedSequenceType right;
+
+    /**
+     * Constructor
+     *
+     * @param left the left member to set
+     * @param right the right member to set
+     */
 	public SequenceTypePair(SupportedSequenceType left, SupportedSequenceType right) {
 		this.left = left;
 		this.right = right;
 	}
 
+    /**
+     * Get the left member of the pair
+     *
+     * @return the left member of the pair
+     */
+
 	public SupportedSequenceType getLeft() {
 		return left;
 	}
 
+    /**
+     * Get the right member of the pair
+     *
+     * @return the right member of the pair
+     */
 	public SupportedSequenceType getRight() {
 		return right;
 	}
@@ -29,6 +49,12 @@ public class SequenceTypePair {
 	/**************************************************************************************
 	 ** Object overriding part
 	 **************************************************************************************/
+    /**
+     * Method to compare the current pair of sequence type to a given one
+     *
+     * @param obj the other pair of sequence type
+     * @return true if obj is a pair of sequence type equals to the current one
+     */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -45,10 +71,8 @@ public class SequenceTypePair {
 	}
 
 	/**
-	 * <p>
 	 * Returns a suitable hash code. The hash code follows the definition in
 	 * {@code Map.Entry}.
-	 * </p>
 	 *
 	 * @return the hash code
 	 */
@@ -58,6 +82,11 @@ public class SequenceTypePair {
 		return (getLeft() == null ? 0 : getLeft().hashCode()) ^ (getRight() == null ? 0 : getRight().hashCode());
 	}
 
+	/**
+	 * Method to generate a string representation of the current pair
+	 *
+	 * @return the string representation of the current pair
+	 */
 	@Override
 	public String toString() {
 		return "(" + getLeft().toString() + ", " + getRight().toString() + ")";
