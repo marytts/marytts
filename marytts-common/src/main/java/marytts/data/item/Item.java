b4aab34c7b64fbea92316762901da35e8989fcdd
Sebastien Le Maguer <slemaguer@coli.uni-saarland.de>
2017-05-13 22:58:10 +0200

    Item & Paragraph javadoc

--- marytts-common/src/main/java/marytts/data/item/Item.java
+++ marytts-common/src/main/java/marytts/data/item/Item.java
@@ -10,12 +10,24 @@ import marytts.data.Sequence;
  *         Maguer</a>
  */
 public abstract class Item {
+    /** The sequence which is containing the item */
 	protected Sequence<? extends Item> m_sequence_reference;
 
+    /**
+     * Default constructor to set to null the sequence reference. Protected as it is an abstract
+     * class
+     *
+     */
 	protected Item() {
 		m_sequence_reference = null;
 	}
 
+    /**
+     * Method to set the sequence which contains the item
+     *
+     * @param seq the sequence which contains the item
+     * @return true if the reference was null before
+     */
 	public boolean setSequenceReference(Sequence<? extends Item> seq) {
 		if (m_sequence_reference == null) {
 			m_sequence_reference = seq;
@@ -26,14 +38,29 @@ public abstract class Item {
 		}
 	}
 
+    /**
+     * Unset the sequence. The item is therefore assumed to be orphan.
+     *
+     */
 	public void unsetSequenceReference() {
 		m_sequence_reference = null;
 	}
 
+    /**
+     * Check if the current item is in the given sequence
+     *
+     * @param seq the given sequence
+     * @return true if the current item is in the given sequence
+     */
 	public boolean isInSequence(Sequence<? extends Item> seq) {
 		return (m_sequence_reference == seq);
 	}
 
+    /**
+     * Get the current item sequence
+     *
+     * @return the item sequence
+     */
 	public Sequence<? extends Item> getSequence() {
 		return m_sequence_reference;
 	}
--- marytts-common/src/main/java/marytts/data/item/linguistic/Paragraph.java
+++ marytts-common/src/main/java/marytts/data/item/linguistic/Paragraph.java
@@ -5,27 +5,50 @@ import java.util.ArrayList;
 import marytts.data.item.Item;
 
 /**
- *
+ * A class representing a pararaph. A paragraph is just composed by its text
  *
  * @author <a href="mailto:slemaguer@coli.uni-saarland.de">Sébastien Le
  *         Maguer</a>
  */
 public class Paragraph extends Item {
+    /** the text of the paragraph */
 	private String m_text;
 
+    /**
+     * Constructor of a paragraph
+     *
+     * @param text the text of the paragraph
+     */
 	public Paragraph(String text) {
 		super();
 		setText(text);
 	}
 
+    /**
+     * Accessor to get the text of the paragraph
+     *
+     * @return the text of the paragraph
+     */
 	public String getText() {
 		return m_text;
 	}
 
+    /**
+     * Accessor to set the text of the paragraph
+     *
+     * @param text the text of the paragraph
+     */
 	protected void setText(String text) {
 		m_text = text;
 	}
 
+	/**
+	 * Method to compare an object to the current paragraph
+	 *
+	 * @param obj
+	 *            the object to compare
+	 * @return true if obj is a paragraph and the paragraph are equals, false else
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (!(obj instanceof Paragraph))
