074b04e0eb5f4eda6c56d6284265e16f4a2baab4
Sebastien Le Maguer <slemaguer@coli.uni-saarland.de>
2017-05-13 16:19:20 +0200

    Javadoc SupportedSequenceType

--- marytts-common/src/main/java/marytts/data/SupportedSequenceType.java
+++ marytts-common/src/main/java/marytts/data/SupportedSequenceType.java
@@ -1,5 +1,8 @@
 package marytts.data;
 
+/**
+ * The type to label the kinds of sequence which are currently supported
+ */
 public enum SupportedSequenceType {
 	PARAGRAPH, SENTENCE, PHRASE, WORD, SYLLABLE, PHONE, NSS, SEGMENT, FEATURES
 };
