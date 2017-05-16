58f2c61c6dd90b358ef5fbbdcee4937c77d2cb70
Sebastien Le Maguer <slemaguer@coli.uni-saarland.de>
2017-05-16 16:15:06 +0200

    javadoc context processor ok

--- marytts-common/src/main/java/marytts/features/contextprocessor/NextNext.java
+++ marytts-common/src/main/java/marytts/features/contextprocessor/NextNext.java
@@ -6,17 +6,27 @@ import marytts.data.Sequence;
 import marytts.features.ContextProcessor;
 
 /**
+ * Context processor to get the 2nd next item
  *
  *
  * @author <a href="mailto:slemaguer@coli.uni-saarland.de">Sébastien Le
  *         Maguer</a>
  */
 public class NextNext implements ContextProcessor {
+
+    /**
+     * Return the 2nd next item of the given item in the sequence
+     *
+     * @param item the given item
+     * @return the 2nd next item of the given item or null if there is no such things.
+     * @throws Exception (actually NotInSequenceException) if the given item is not in a sequence
+     */
 	public Item get(Item item) throws Exception {
 		Sequence<? extends Item> seq = item.getSequence();
+
+        // FIXME: Should be replace by a "notinsequence" exception
 		if (seq == null)
-			throw new Exception(); // FIXME: Should be replace by a
-									// "notinsequence" exception
+			throw new Exception();
 
 		int idx = seq.indexOf(item);
 		if (idx >= (seq.size() - 2))
