5ff2c6b7f3414679f8ac8e383b56f82fed366093
Sebastien Le Maguer <slemaguer@coli.uni-saarland.de>
2017-07-17 09:40:28 +0200

    Add minimal voice class

---
+++ marytts-common/src/main/java/marytts/Voice.java
@@ -0,0 +1,28 @@
+package marytts;
+
+
+/**
+ *
+ *
+ * @author <a href="mailto:slemaguer@coli.uni-saarland.de">Sébastien Le Maguer</a>
+ */
+public abstract class Voice
+{
+    private String name;
+
+    public Voice(String name)
+    {
+	setName(name);
+    }
+
+    public String getName() {
+	return name;
+    }
+
+    public void setName(String name) {
+	this.name = name;
+    }
+}
+
+
+/* Voice.java ends here */
