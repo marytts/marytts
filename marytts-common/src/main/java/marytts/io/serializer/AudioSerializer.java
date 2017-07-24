a12838da231cccb9b0bfa7b6d315bc00a78d1f31
Sebastien Le Maguer <slemaguer@coli.uni-saarland.de>
2017-07-25 00:11:13 +0200

    Fix audio serialization into a JSON string

--- marytts-common/src/main/java/marytts/io/serializer/AudioSerializer.java
+++ marytts-common/src/main/java/marytts/io/serializer/AudioSerializer.java
@@ -1,8 +1,5 @@
+
 package marytts.io.serializer;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-
 import marytts.io.serializer.Serializer;
 import marytts.io.MaryIOException;
 
@@ -11,14 +8,6 @@ import marytts.data.Sequence;
 import marytts.data.SupportedSequenceType;
 import marytts.data.item.acoustic.AudioItem;
 
-
-import javax.sound.sampled.AudioFileFormat;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.UnsupportedAudioFileException;
-
-
-import java.util.Base64;
 
 
 /**
@@ -43,28 +32,20 @@ public class AudioSerializer implements Serializer
      *             if anything is going wrong
      */
     public Object export(Utterance utt) throws MaryIOException {
-	Sequence<AudioItem> seq_au = (Sequence<AudioItem>) utt.getSequence(SupportedSequenceType.AUDIO);
-
-	if (seq_au == null)
-	    throw new MaryIOException("There is no audio to serialize (no sequence available)");
-
-	if (seq_au.size() == 0)
-	    throw new MaryIOException("There is no audio to serialize (sequence is empty)");
-
-	// FIXME: what to do with multiple audio, merge them
-	AudioInputStream ais = ((AudioItem) seq_au.get(0)).getAudio();
-	ByteArrayOutputStream baos = new ByteArrayOutputStream();
-
 	try {
-	    AudioSystem.write(ais
-			      ,AudioFileFormat.Type.WAVE
-			      ,baos);
-	} catch (IOException ex) {
+	    Sequence<AudioItem> seq_au = (Sequence<AudioItem>) utt.getSequence(SupportedSequenceType.AUDIO);
+	    if (seq_au == null)
+		throw new MaryIOException("There is no audio to serialize (no sequence available)");
+
+	    if (seq_au.size() == 0)
+		throw new MaryIOException("There is no audio to serialize (sequence is empty)");
+
+	    AudioItem audio_item = (AudioItem) seq_au.get(0);
+
+	    return "{ \"audio\": \"" + audio_item.getAudioStringEncoded() + "\"}";
+	} catch (Exception ex) {
 	    throw new MaryIOException("Cannot serialize utterance", ex);
 	}
-
-	return "{ \"audio\": \"" + Base64.getEncoder().encodeToString(baos.toByteArray()) + "\"}";
-
     }
 
 
