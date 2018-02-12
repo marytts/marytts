9fc25b45adf4989db243eedfc536ca3b5cf295a8
Sebastien Le Maguer <slemaguer@coli.uni-saarland.de>
2018-02-12 11:21:07 +0100

    config: Sanitize the json loader (add object instantiation + cleaning)

--- marytts-common/src/main/java/marytts/config/JSONMaryConfigLoader.java
+++ marytts-common/src/main/java/marytts/config/JSONMaryConfigLoader.java
@@ -19,43 +19,59 @@
  */
 package marytts.config;
 
+// Regexp
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.io.File;
-import java.io.FileInputStream;
+
+// IO
 import java.io.InputStream;
 import java.io.InputStreamReader;
+
+// Collections
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Locale;
-import java.util.Map;
 import java.util.Set;
+
+/* Reflection */
+import java.lang.reflect.Method;
+import java.lang.reflect.Constructor;
 
 // JSON part
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.*;
 
-import org.apache.commons.lang.StringUtils;
-
+// Exceptions
 import marytts.exceptions.MaryConfigurationException;
 import java.io.FileNotFoundException;
 
 /**
- * This class is designed to load configuration stored in "extended" java properties.
+ * Json configuration loader
  *
  *
- * @author marc
- *
+ * @author <a href="mailto:slemaguer@coli.uni-saarland.de">Sébastien Le Maguer</a>
  */
 public class JSONMaryConfigLoader extends MaryConfigLoader {
 
-    // ////////// Non-/ base class methods //////////////
+    /** Class keyword used for object representation */
+    public static final String CLASS_KEY = "class";
+
+    /** Arguments keyword used for object representation */
+    public static final String ARG_KEY = "arguments";
+
+    /** Argument class keyword used for object representation */
+    public static final String ARG_CLASS_KEY = "class";
+
+    /** Argument value keyword used for object representation */
+    public static final String ARG_VALUE_KEY = "value";
 
 
+    /**
+     *  Default constructor which is just instantiate the default configuration
+     *
+     *  @throws MaryConfigurationException if the instanciation failed
+     */
     public JSONMaryConfigLoader() throws MaryConfigurationException {
 	super();
 	InputStream input_stream = this.getClass().getResourceAsStream(MaryConfigurationFactory.DEFAULT_KEY + ".json");
@@ -63,6 +79,13 @@ public class JSONMaryConfigLoader extends MaryConfigLoader {
     }
 
 
+    /**
+     *  Method to explicitely load a configuration stored in a given stream
+     *
+     *  @param input_stream the stream in which the configuration is stored
+     *  @return the loaded MaryConfiguration object
+     *  @throws MaryConfigurationException if the loading fails
+     */
     public MaryConfiguration loadConfiguration(InputStream input_stream) throws MaryConfigurationException {
 	MaryConfiguration mc = new MaryConfiguration();
         try {
@@ -88,24 +111,124 @@ public class JSONMaryConfigLoader extends MaryConfigLoader {
 			val = jsonObjectToHash((JSONObject) val);
 		    }
 
-		    // Check if method exists
+		    // Check if the setter method exists
 		    prop_name = adaptPropertyName(prop_name);
-		    assessMethod(class_name, prop_name, val.getClass());
+		    assertSetter(class_name, prop_name, val.getClass());
 
 		    mc.addConfigurationValueProperty(class_name, prop_name, val);
 		}
 
 	    }
 
+	    return mc;
         } catch (Exception e) {
             throw new MaryConfigurationException("cannot load json configuration", e);
         }
-
-	return mc;
     }
 
 
-    public Object jsonObjectToHash(JSONObject hash) throws MaryConfigurationException {
+    /**
+     *  Internal method to instanciate an object stored in the json format
+     *
+     *  an object is composed by the following description:
+     *
+     *  {
+     *     "class": "class of the object",
+     *     "arguments": [
+     *         {
+     *             "class": "class of the argument",
+     *             "value": "value of the argument"
+     *         },
+     *         ....
+     *     ]
+     *  }
+     *
+     *  the value of the argument is store in a string and is optional.
+     *
+     *  @param hash the jsonobject representing the object to instanciate
+     *  @return the instanciated objects
+     *  @throws MaryConfigurationException if anything fails during the instanciation
+     */
+    protected Object jsonObjectInstantiate(JSONObject hash) throws MaryConfigurationException {
+
+	try {
+	    Class<?> clazz = Class.forName((String) hash.get(CLASS_KEY));
+
+	    JSONArray json_args = (JSONArray) hash.get(ARG_KEY);
+	    Object[] args = null;
+	    Constructor<?> ctor = null;
+
+	    if (args == null) { // Default constructor
+		args = new Object[] {};
+		ctor = clazz.getConstructor();
+	    } else { // With args constructor
+		args = new Object[json_args.size()];
+		Class<?>[] arg_classes = new Class<?>[json_args.size()];
+
+		for (int i=0; i<json_args.size(); i++) {
+		    JSONObject arg = (JSONObject) json_args.get(i);
+
+		    // Get class
+		    Class<?> cur_clazz = Class.forName((String) arg.get(ARG_CLASS_KEY));
+		    arg_classes[i] = cur_clazz;
+
+		    // Check argument (at most 1 for now and String mandatory)
+		    Object[] arg_val = null;
+		    Class<?>[] class_ctor = null;
+		    if (arg.containsKey(ARG_VALUE_KEY)) {
+
+			// Prepare arguments
+			class_ctor = new Class<?>[1];
+			class_ctor[0] = Class.forName("String");
+
+			// Get value argument
+			arg_val = new Object[1];
+			String value = arg.get(ARG_VALUE_KEY).toString();
+			arg_val[0] = value;
+
+		    } else {
+			// Prepare arguments
+			class_ctor = new Class<?>[]{};
+
+			// Get value argument
+			arg_val = new Object[] {};
+		    }
+
+		    // Ok now instanciate
+		    Constructor<?> cur_ctor = cur_clazz.getConstructor(class_ctor);
+		    args[i] = cur_ctor.newInstance(arg_val);
+		}
+
+		ctor = clazz.getConstructor(arg_classes);
+	    }
+
+
+	    return ctor.newInstance(args);
+	} catch (Exception ex) {
+	    throw new MaryConfigurationException("Couldn't instantiate object of class \"" +
+						 hash.get(CLASS_KEY).toString()	 + "\"",  ex);
+	}
+    }
+
+
+    /**
+     *  Internal method to load a json hash
+     *
+     *  it can be a classic dictionnary or an object description. The last case is detected if a key
+     *  "class" is present in the map.
+     *
+     *  @param hash the hash to load
+     *  @return the object instanciated or the loaded dictionnary
+     *  @throws MaryConfigurationException if the loading fails
+     */
+    protected Object jsonObjectToHash(JSONObject hash) throws MaryConfigurationException {
+
+	// Is the hash is actually an object description
+	Set<Object> key_set = hash.keySet();
+	if (key_set.contains(CLASS_KEY))
+	    return jsonObjectInstantiate(hash);
+
+	// Load the dictionary
 	HashMap<String, Object> output_hash = new HashMap<String, Object>();
 	for (Object k_ob: hash.keySet()) {
 	    String k = k_ob.toString();
@@ -123,7 +246,14 @@ public class JSONMaryConfigLoader extends MaryConfigLoader {
 	return output_hash;
     }
 
-    public Object jsonArrayToList(JSONArray array) throws MaryConfigurationException {
+    /**
+     *  Convert the json array to an arraylist
+     *
+     *  @param array the json array
+     *  @return the arraylist of objects extracted from the json array
+     *  @throws MaryConfigurationException if anything fails
+     */
+    protected ArrayList<Object> jsonArrayToList(JSONArray array) throws MaryConfigurationException {
 	if (array.size() == 0)
 	    throw new MaryConfigurationException("It is forbiddent to have an empty array in the configuration");
 
@@ -143,7 +273,15 @@ public class JSONMaryConfigLoader extends MaryConfigLoader {
 	return ar;
     }
 
-    public void assessMethod(String class_name, String property_name, Class<?> class_arg) throws MaryConfigurationException {
+    /**
+     *  Assert if the setter method for a given property is available in the given class
+     *
+     *  @param class_name the name of the class
+     *  @param property_name the name of the property
+     *  @param class_arg the class of the argument
+     *  @throws MaryConfigurationException if the setter doesn't exist
+     */
+    protected void assertSetter(String class_name, String property_name, Class<?> class_arg) throws MaryConfigurationException {
 	Class cls = null;
 	try {
 	    cls =  Class.forName(class_name, false, this.getClass().getClassLoader());
@@ -158,8 +296,20 @@ public class JSONMaryConfigLoader extends MaryConfigLoader {
 	}
     }
 
-    public String adaptPropertyName(String property_name) {
+    /**
+     *  Internal method to transform a property name to its setter name
+     *
+     *  By convention the first charatecter is always capitalized as all characters prefixed by
+     *  underscores. For example, prop_name is going to be transformed in setPropName.
+     *
+     *  @param property_name the name of the property for which we want the setter method
+     *  @return the setter method name
+     */
+    protected String adaptPropertyName(String property_name) {
+	// Upper case the first letter
 	property_name = property_name.substring(0,1).toUpperCase() + property_name.substring(1).toLowerCase();
+
+	// Upper case the character prefixed by underscores
 	StringBuffer result = new StringBuffer();
 	Matcher m = Pattern.compile("_(\\w)").matcher(property_name);
 	while (m.find()) {
@@ -167,7 +317,7 @@ public class JSONMaryConfigLoader extends MaryConfigLoader {
 				m.group(1).toUpperCase());
 	}
 	m.appendTail(result);
-	return result.toString();
 
+	return result.toString();
     }
 }
