ae6490567a44833e3c0a2b3cf281026fc26f88fe
Sebastien Le Maguer <slemaguer@coli.uni-saarland.de>
2018-03-03 17:27:32 +0100

    phonetic: add sampa alphabet, adapt jphonemiser to produce ipa symbols, adapt the HTS serialisation

--- marytts-common/src/main/java/marytts/features/featureprocessor/ArpaLabel.java
+++ marytts-common/src/main/java/marytts/features/featureprocessor/ArpaLabel.java
@@ -7,6 +7,9 @@ import marytts.data.Utterance;
 import marytts.data.item.Item;
 import marytts.data.item.linguistic.Word;
 
+import marytts.phonetic.AlphabetFactory;
+import marytts.phonetic.converter.Alphabet;
+import marytts.MaryException;
 import marytts.features.Feature;
 import marytts.features.FeatureProcessor;
 
@@ -17,78 +20,17 @@ import marytts.features.FeatureProcessor;
  *         Maguer</a>
  */
 public class ArpaLabel implements FeatureProcessor {
-    protected static Hashtable<String, String> alphabet_converter = null;
 
-    public ArpaLabel() {
-        if (alphabet_converter == null) {
-            initPhConverter();
-        }
+    Alphabet ipa2arp;
+    public ArpaLabel() throws MaryException {
+	ipa2arp = AlphabetFactory.getAlphabet("arpabet");
     }
 
-    protected void initPhConverter() {
-        alphabet_converter = new Hashtable<String, String>();
+    protected String convertPh(String ph) throws MaryException{
+	if (ph.equals("sil") || ph.equals("_"))
+	    return "pau";
 
-        alphabet_converter.put("A", "aa");
-        alphabet_converter.put("O", "ao");
-        alphabet_converter.put("u", "uw");
-        alphabet_converter.put("i", "iy");
-
-        alphabet_converter.put("{", "ae");
-        alphabet_converter.put("V", "ah");
-        alphabet_converter.put("E", "eh");
-        alphabet_converter.put("I", "ih");
-        alphabet_converter.put("U", "uh");
-
-        alphabet_converter.put("@", "ah");
-        alphabet_converter.put("r=", "er"); // FIXME: what is this one ?
-
-
-        alphabet_converter.put("aU", "aw");
-        alphabet_converter.put("OI", "oy");
-        alphabet_converter.put("@U", "ah");
-        alphabet_converter.put("EI", "ey");
-        alphabet_converter.put("AI", "ay");
-
-        alphabet_converter.put("p", "p");
-        alphabet_converter.put("t", "t");
-        alphabet_converter.put("k", "k");
-        alphabet_converter.put("b", "b");
-        alphabet_converter.put("d", "d");
-        alphabet_converter.put("g", "g");
-
-        alphabet_converter.put("tS", "ch");
-        alphabet_converter.put("dZ", "jh");
-
-        alphabet_converter.put("f", "f");
-        alphabet_converter.put("v", "v");
-        alphabet_converter.put("T", "th");
-        alphabet_converter.put("D", "dh");
-        alphabet_converter.put("s", "s");
-        alphabet_converter.put("z", "z");
-        alphabet_converter.put("S", "sh");
-        alphabet_converter.put("Z", "zh");
-        alphabet_converter.put("h", "h");
-
-        alphabet_converter.put("l", "l");
-        alphabet_converter.put("m", "m");
-        alphabet_converter.put("n", "n");
-        alphabet_converter.put("N", "ng");
-        alphabet_converter.put("r", "r"); // FIXME: what is this one ?
-        alphabet_converter.put("w", "w");
-        alphabet_converter.put("j", "y");
-
-        alphabet_converter.put("sil", "pau");
-        alphabet_converter.put("_", "pau");
-    }
-
-    protected String convertPh(String ph) {
-        String fest_ph = alphabet_converter.get(ph);
-        if (fest_ph != null) {
-            return fest_ph;
-        }
-        System.out.println(ph + " is not converted");
-
-        return ph;
+        return ipa2arp.getLabelFromIPA(ph);
     }
 
     public Feature generate(Utterance utt, Item item) throws MaryException {
--- marytts-common/src/main/java/marytts/phonetic/converter/Sampa.java
+++ marytts-common/src/main/java/marytts/phonetic/converter/Sampa.java
@@ -14,7 +14,76 @@ import java.util.ArrayList;
  */
 public class Sampa extends Alphabet
 {
+    public Sampa() {
+	super();
+
+	// consonants
+	this.addIpaCorrespondance("b", "b");
+	this.addIpaCorrespondance("d", "d");
+	this.addIpaCorrespondance("f", "f");
+	this.addIpaCorrespondance("g", "ɡ");
+	this.addIpaCorrespondance("k", "k");
+	this.addIpaCorrespondance("l", "l");
+	this.addIpaCorrespondance("m", "m");
+	this.addIpaCorrespondance("n", "n");
+	this.addIpaCorrespondance("S", "ʃ");
+	this.addIpaCorrespondance("s", "s");
+	this.addIpaCorrespondance("p", "p");
+	this.addIpaCorrespondance("t", "t");
+	this.addIpaCorrespondance("T", "θ");
+	this.addIpaCorrespondance("v", "v");
+	this.addIpaCorrespondance("w", "w");
+	this.addIpaCorrespondance("z", "z");
+	this.addIpaCorrespondance("Z", "ʒ");
+
+	// affricates
+	this.addIpaCorrespondance("dS", "tʃ");
+	this.addIpaCorrespondance("dZ", "dʒ");
+
+	// Long vowels
+	this.addIpaCorrespondance("A", "ɑ");
+	this.addIpaCorrespondance("i", "i");
+	this.addIpaCorrespondance("u", "u");
+	this.addIpaCorrespondance("O", "ɔ");
+	this.addIpaCorrespondance("AXR", "ɚ");
+
+	// Short vowels
+	this.addIpaCorrespondance("{", "æ");
+	this.addIpaCorrespondance("V", "ʌ");
+	this.addIpaCorrespondance("@", "ʌ"); // FIXME check
+	this.addIpaCorrespondance("@U", "ʌ"); // FIXME check
+	this.addIpaCorrespondance("AX", "ə");
+	this.addIpaCorrespondance("E", "ɛ");
+	this.addIpaCorrespondance("r=", "ɚ"); // FIXME: what is this one
+	this.addIpaCorrespondance("I", "ɪ");
+	this.addIpaCorrespondance("IX", "ɨ");
+	this.addIpaCorrespondance("U", "ʊ");
+	this.addIpaCorrespondance("UX", "ʉ");
+	this.addIpaCorrespondance("D", "ð");
+	this.addIpaCorrespondance("DX", "ɾ");
+
+	// Stressed
+	this.addIpaCorrespondance("EL", "l̩");
+	this.addIpaCorrespondance("EM", "m̩");
+	this.addIpaCorrespondance("EN", "n̩");
+
+
+	// dipthongs
+	this.addIpaCorrespondance("EI", "eɪ");
+	this.addIpaCorrespondance("AI", "aɪ");
+	this.addIpaCorrespondance("aU", "aʊ");
+	this.addIpaCorrespondance("OW", "oʊ");
+	this.addIpaCorrespondance("OI", "ɔɪ");
+
+
+	// FIXME: Not sorted (yet)
+	this.addIpaCorrespondance("h", "h");
+	this.addIpaCorrespondance("N", "ŋ");
+	this.addIpaCorrespondance("NX", "ɾ̃");
+	this.addIpaCorrespondance("Q", "ʔ");
+	this.addIpaCorrespondance("r", "ɹ");
+	this.addIpaCorrespondance("WH", "ʍ");
+	this.addIpaCorrespondance("j", "j");
+
+    }
 }
-
-
-/* Sampa.java ends here */
