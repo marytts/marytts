aef7ee88dc2252043035b7da5caf76798a15f4af
Sebastien Le Maguer <slemaguer@coli.uni-saarland.de>
2018-04-05 00:06:27 +0200

    serializer: add identity serializer

---
+++ marytts-common/src/main/java/marytts/io/serializer/UtteranceSerializer.java
@@ -0,0 +1,39 @@
+package marytts.io.serializer;
+
+import marytts.data.Utterance;
+import marytts.io.MaryIOException;
+
+
+/**
+ *
+ * @author <a href="mailto:slemaguer@coli.uni-saarland.de">Sébastien Le
+ *         Maguer</a>
+ */
+public class UtteranceSerializer implements Serializer {
+
+    /**
+     * Constructor
+     *
+     */
+    public UtteranceSerializer() {
+    }
+
+    public Object export(Utterance utt) throws MaryIOException {
+	return utt;
+    }
+
+    /**
+     * Unsupported operation ! We can't import from a TSV formatted input.
+     *
+     * @param content
+     *            unused
+     * @return nothing
+     * @throws MaryIOException
+     *             never done
+     */
+    public Utterance load(String content) throws MaryIOException {
+        throw new UnsupportedOperationException();
+    }
+}
+
+/* HTSLabelSerializer.java ends here */
