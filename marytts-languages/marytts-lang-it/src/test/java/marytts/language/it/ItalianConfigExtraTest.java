1e6adfcdbb50c512bb4c936e900f3af336ed1ef1
Ingmar Steiner <steiner@coli.uni-saarland.de>
2018-07-28 22:53:45 +0200

    refactor old unit test

--- marytts-languages/marytts-lang-it/src/test/java/marytts/language/it/ItalianConfigExtraTest.java
+++ marytts-languages/marytts-lang-it/src/test/java/marytts/language/it/ItalianConfigExtraTest.java
@@ -1,21 +1,20 @@
 /**
  * Copyright 2011 DFKI GmbH.
  * All Rights Reserved.  Use is subject to license terms.
- *
+ * <p>
  * This file is part of MARY TTS.
- *
+ * <p>
  * MARY TTS is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, version 3 of the License.
- *
+ * <p>
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- *
+ * <p>
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
  */
 package marytts.language.it;
 
@@ -30,26 +29,25 @@ import org.testng.annotations.Test;
 
 /**
  * @author marc
- *
  */
-public class ItalianConfigTest {
+public class ItalianConfigExtraTest {
 
-	@Test
-	public void isNotMainConfig() throws MaryConfigurationException {
-		MaryConfig m = new ItalianConfig();
-		Assert.assertFalse(m.isMainConfig());
-	}
+    @Test
+    public void isNotMainConfig() throws MaryConfigurationException {
+        MaryConfig m = new ItalianConfig();
+        Assert.assertFalse(m.isMainConfig());
+    }
 
-	@Test
-	public void canGet() {
-		MaryConfig m = MaryConfig.getLanguageConfig(Locale.ITALIAN);
-		Assert.assertNotNull(m);
-		Assert.assertTrue(((LanguageConfig) m).getLocales().contains(Locale.ITALIAN));
-	}
+    @Test
+    public void canGet() {
+        MaryConfig m = MaryConfig.getLanguageConfig(Locale.ITALIAN);
+        Assert.assertNotNull(m);
+        Assert.assertTrue(((LanguageConfig) m).getLocales().contains(Locale.ITALIAN));
+    }
 
-	@Test
-	public void hasItalianLocale() throws MaryConfigurationException {
-		LanguageConfig e = new ItalianConfig();
-		Assert.assertTrue(e.getLocales().contains(Locale.ITALIAN));
-	}
+    @Test
+    public void hasItalianLocale() throws MaryConfigurationException {
+        LanguageConfig e = new ItalianConfig();
+        Assert.assertTrue(e.getLocales().contains(Locale.ITALIAN));
+    }
 }
