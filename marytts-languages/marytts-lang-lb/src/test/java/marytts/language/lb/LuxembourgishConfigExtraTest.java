5bbdfc3cf2f72f60709fb3731b0b66244206be54
Ingmar Steiner <steiner@coli.uni-saarland.de>
2018-07-29 19:31:00 +0200

    refactor old unit test

--- marytts-languages/marytts-lang-lb/src/test/java/marytts/language/lb/LuxembourgishConfigExtraTest.java
+++ marytts-languages/marytts-lang-lb/src/test/java/marytts/language/lb/LuxembourgishConfigExtraTest.java
@@ -1,21 +1,20 @@
 /**
  * Copyright 2011 DFKI GmbH.
  * All Rights Reserved.  Use is subject to license terms.
- *
+ * <p>
  * This file is part of MARY TTS.
- *
+ * <p>
  * MARY TTS is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, version 3 of the License.
- *
+ * <p>
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- *
+ * <p>
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
  */
 package marytts.language.lb;
 
@@ -31,27 +30,20 @@ import org.testng.annotations.*;
 
 /**
  * @author marc
- *
  */
-public class LuxembourgishConfigTest {
-	private static final Locale LUXEMBOURGISH = new Locale("lb");
+public class LuxembourgishConfigExtraTest {
+    private static final Locale LUXEMBOURGISH = new Locale("lb");
 
-	@Test
-	public void isNotMainConfig() throws MaryConfigurationException {
-		MaryConfig m = new LuxembourgishConfig();
-		assertFalse(m.isMainConfig());
-	}
+    @Test
+    public void canGet() {
+        MaryConfig m = MaryConfig.getLanguageConfig(LUXEMBOURGISH);
+        assertNotNull(m);
+        assertTrue(((LanguageConfig) m).getLocales().contains(LUXEMBOURGISH));
+    }
 
-	@Test
-	public void canGet() {
-		MaryConfig m = MaryConfig.getLanguageConfig(LUXEMBOURGISH);
-		assertNotNull(m);
-		assertTrue(((LanguageConfig) m).getLocales().contains(LUXEMBOURGISH));
-	}
-
-	@Test
-	public void hasLuxembourgishLocale() throws MaryConfigurationException {
-		LanguageConfig e = new LuxembourgishConfig();
-		assertTrue(e.getLocales().contains(LUXEMBOURGISH));
-	}
+    @Test
+    public void hasLuxembourgishLocale() throws MaryConfigurationException {
+        LanguageConfig e = new LuxembourgishConfig();
+        assertTrue(e.getLocales().contains(LUXEMBOURGISH));
+    }
 }
