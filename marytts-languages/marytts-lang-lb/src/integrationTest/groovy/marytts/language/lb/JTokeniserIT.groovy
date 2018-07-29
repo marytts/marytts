a5d3a34815708ebac44829395cfb725af33cc77b
Ingmar Steiner <steiner@coli.uni-saarland.de>
2018-07-29 19:37:02 +0200

    reformat, parameterize IT

--- marytts-languages/marytts-lang-lb/src/integrationTest/groovy/marytts/language/lb/JTokeniserIT.groovy
+++ marytts-languages/marytts-lang-lb/src/integrationTest/groovy/marytts/language/lb/JTokeniserIT.groovy
@@ -1,53 +1,42 @@
 package marytts.language.lb
 
-import java.util.Locale
-
-import org.testng.annotations.Test
-
 import marytts.modules.JTokeniser
 import marytts.modules.ModuleRegistry
 import marytts.tests.modules.MaryModuleTestCase
+import org.testng.annotations.DataProvider
+import org.testng.annotations.Test
 
 class JTokeniserIT extends MaryModuleTestCase {
 
-	def locale
+    Locale locale
 
-	JTokeniserIT() throws Exception {
-		super(true)
-		module = ModuleRegistry.getModule(JTokeniser.class)
-		locale = Locale.forLanguageTag('lb')
-	}
+    JTokeniserIT() throws Exception {
+        super(true)
+        module = ModuleRegistry.getModule(JTokeniser.class)
+        locale = Locale.forLanguageTag('lb')
+    }
 
-	String inputEnding() {
-		'txt'
-	}
+    String inputEnding() {
+        'txt'
+    }
 
-	String outputEnding() {
-		'tokenised'
-	}
+    String outputEnding() {
+        'tokenised'
+    }
 
-	@Test
-	void testDots1() {
-		processAndCompare 'dots1', locale
-	}
+    @DataProvider
+    Object[][] basenames() {
+        [
+                ['dots1'],
+                ['dots2'],
+                ['dots3'],
+                ['exclam'],
+                ['quest']
+        ]
+    }
 
-	@Test
-	void testDots2() {
-		processAndCompare 'dots2', locale
-	}
-
-	@Test
-	void testDots3() {
-		processAndCompare 'dots3', locale
-	}
-
-	@Test
-	void testExclam() {
-		processAndCompare 'exclam', locale
-	}
-
-	@Test
-	void testQuest() {
-		processAndCompare 'quest', locale
-	}
+    @Test(dataProvider = 'basenames')
+    void testCase(String basename) {
+        processAndCompare basename, locale
+    }
 }
