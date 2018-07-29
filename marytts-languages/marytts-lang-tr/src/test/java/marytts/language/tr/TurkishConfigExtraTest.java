a1906a1d555b1cfc12c802a9d8a0bba75a7384c9
Ingmar Steiner <steiner@coli.uni-saarland.de>
2018-07-29 23:11:04 +0200

    refactor old unit test

--- marytts-languages/marytts-lang-tr/src/test/java/marytts/language/tr/TurkishConfigExtraTest.java
+++ marytts-languages/marytts-lang-tr/src/test/java/marytts/language/tr/TurkishConfigExtraTest.java
@@ -1,27 +1,22 @@
 /**
  * Copyright 2011 DFKI GmbH.
  * All Rights Reserved.  Use is subject to license terms.
- *
+ * <p>
  * This file is part of MARY TTS.
- *
+ * <p>
  * MARY TTS is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, version 3 of the License.
- *
+ * <p>
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- *
+ * <p>
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
  */
 package marytts.language.tr;
-
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 
 import java.util.Locale;
 
@@ -29,31 +24,26 @@ import marytts.config.LanguageConfig;
 import marytts.config.MaryConfig;
 import marytts.exceptions.MaryConfigurationException;
 
-import org.junit.Test;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 /**
  * @author marc
- *
  */
-public class TurkishConfigTest {
-	private static final Locale TURKISH = new Locale("tr");
+public class TurkishConfigExtraTest {
 
-	@Test
-	public void isNotMainConfig() throws MaryConfigurationException {
-		MaryConfig m = new TurkishConfig();
-		assertFalse(m.isMainConfig());
-	}
+    private static final Locale TURKISH = new Locale("tr");
 
-	@Test
-	public void canGet() {
-		MaryConfig m = MaryConfig.getLanguageConfig(TURKISH);
-		assertNotNull(m);
-		assertTrue(((LanguageConfig) m).getLocales().contains(TURKISH));
-	}
+    @Test
+    public void canGet() {
+        MaryConfig m = MaryConfig.getLanguageConfig(TURKISH);
+        Assert.assertNotNull(m);
+        Assert.assertTrue(((LanguageConfig) m).getLocales().contains(TURKISH));
+    }
 
-	@Test
-	public void hasRussianLocale() throws MaryConfigurationException {
-		LanguageConfig e = new TurkishConfig();
-		assertTrue(e.getLocales().contains(TURKISH));
-	}
+    @Test
+    public void hasTurkishLocale() throws MaryConfigurationException {
+        LanguageConfig e = new TurkishConfig();
+        Assert.assertTrue(e.getLocales().contains(TURKISH));
+    }
 }
