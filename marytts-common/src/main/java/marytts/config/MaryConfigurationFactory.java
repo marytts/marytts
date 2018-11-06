80f3ecc29a51bfe35145c94e744930ce011af630
Sebastien Le Maguer <lemagues@tcd.ie>
2018-11-06 17:48:57 +0000

    configuration: add multiple configuration instance support per "set"

--- marytts-common/src/main/java/marytts/config/MaryConfigurationFactory.java
+++ marytts-common/src/main/java/marytts/config/MaryConfigurationFactory.java
@@ -1,5 +1,6 @@
 package marytts.config;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import marytts.exceptions.MaryConfigurationException;
 
@@ -15,7 +16,13 @@ public class MaryConfigurationFactory
     public static final String DEFAULT_KEY = "default";
 
     /** The map containing the configuration */
-    private static HashMap<String, MaryConfiguration> configuration_map  = new HashMap<String, MaryConfiguration>();
+    private static HashMap<String, ArrayList<MaryConfiguration>> configuration_map;
+    static {
+        configuration_map = new HashMap<String, ArrayList<MaryConfiguration>>();
+        ArrayList<MaryConfiguration> list_conf = new ArrayList<MaryConfiguration>();
+        list_conf.add(new MaryConfiguration());
+        configuration_map.put(DEFAULT_KEY, list_conf);
+    }
 
     /**
      *  Method to add a configuration to a set. A set can be a locale or anything else
@@ -24,23 +31,77 @@ public class MaryConfigurationFactory
      *  @param configuration the configuration to add
      *  @fixme maybe avoid overriding default?
      */
-    //
-    public synchronized static void addConfiguration(String set, MaryConfiguration configuration) {
-	if (configuration_map.containsKey(set)) {
-	    configuration_map.get(set).merge(configuration);
-	}
-	configuration_map.put(set, configuration);
+    public synchronized static void pushConfiguration(String set, MaryConfiguration configuration) throws MaryConfigurationException {
+        if (set.equals(DEFAULT_KEY)) {
+            throw new MaryConfigurationException("It is forbidden to have multiple default configurations");
+        }
+        int i = 0;
+        if (configuration_map.get(set) != null) {
+            i = configuration_map.get(set).size();
+        }
+        addConfiguration(set, configuration, i);
     }
 
     /**
-     *  Method to get the configuration knowning the set
+     *  Method to add a configuration to a set. A set can be a locale or anything else
+     *
+     *  @param set the set
+     *  @param configuration the configuration to add
+     *  @fixme maybe avoid overriding default?
+     */
+    public synchronized static void addConfiguration(String set, MaryConfiguration configuration) throws MaryConfigurationException {
+        addConfiguration(set, configuration, 0);
+    }
+
+    /**
+     *  Method to add a configuration to a set. A set can be a locale or anything else
+     *
+     *  @param set the set
+     *  @param configuration the configuration to add
+     *  @fixme maybe avoid overriding default?
+     */
+    public synchronized static void addConfiguration(String set, MaryConfiguration configuration, int i) throws MaryConfigurationException {
+	if (configuration_map.containsKey(set)) {
+            if (i < configuration_map.get(set).size()) {
+                configuration_map.get(set).get(i).merge(configuration);
+            } else if (i == configuration_map.get(set).size()) {
+                configuration_map.get(set).add(i, configuration);
+            } else {
+                throw new MaryConfigurationException(String.format("The index \"%d\" of the configuration is illogical as the set \"%s\" contains only \"%d\" available configurations",
+                                                                   i, set, configuration_map.get(set).size()));
+            }
+	} else if (i == 0) {
+            ArrayList<MaryConfiguration> tmp = new ArrayList<MaryConfiguration>();
+            tmp.add(configuration);
+            configuration_map.put(set, tmp);
+        } else {
+            throw new MaryConfigurationException(String.format("Index \"%d\" is invalid as no configurations for the set \"set\" has yet been found",
+                                                               i, set));
+        }
+    }
+
+
+    /**
+     *  Method to get the first configuration knowning the set
      *
      *  @param set the set
      *  @return the configuration
      *  @fixme see for cloning
      */
     public synchronized static MaryConfiguration getConfiguration(String set) {
-	return configuration_map.get(set);
+        return getConfiguration(set, 0);
+    }
+
+    /**
+     *  Method to get the configuration knowning the set
+     *
+     *  @param set the set
+     *  @param i the index of the configuration (for a module called multiple times)
+     *  @return the configuration
+     *  @fixme see for cloning
+     */
+    public synchronized static MaryConfiguration getConfiguration(String set, int i) {
+	return configuration_map.get(set).get(i);
     }
 
     /**
@@ -49,7 +110,7 @@ public class MaryConfigurationFactory
      *  @return the default configuration
      */
     public synchronized static MaryConfiguration getDefaultConfiguration() {
-	return configuration_map.get(MaryConfigurationFactory.DEFAULT_KEY);
+	return configuration_map.get(MaryConfigurationFactory.DEFAULT_KEY).get(0);
     }
 
     public synchronized static void applyDefaultConfiguration(Object obj) throws MaryConfigurationException {
@@ -57,14 +118,15 @@ public class MaryConfigurationFactory
     }
 
     public synchronized static String dump() {
-	String result = "";
+	String result = "{";
 	for (String key: configuration_map.keySet()) {
-	    result += key + ":\n";
-	    result += "\t" + configuration_map.get(key).toString().replaceAll("\\\n", "\\\n\\\t");
+	    result += key + ": \n";
+            for (MaryConfiguration conf: configuration_map.get(key)) {
+                result += "\t - " + conf.toString().replaceAll("\\\n", "\\\n\\\t\\\t");
+                result += "\n";
+            }
 	    result += "\n";
 	}
 	return result;
     }
 }
-
-
