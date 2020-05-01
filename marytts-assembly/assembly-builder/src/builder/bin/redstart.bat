cd1651d185ae3c797d4bbe962ba07aca795ab2f1
Sebastien Le Maguer <lemagues@tcd.ie>
2020-05-01 13:58:24 +0100

    Get rid of the builder

--- marytts-assembly/assembly-builder/src/builder/bin/redstart.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set BINDIR=%~dp0
-call :RESOLVE "%BINDIR%\.." MARY_BASE
-java -showversion "%*" -cp ".;%MARY_BASE%/lib/*" marytts.tools.redstart.Redstart
-goto :EOF
-
-:RESOLVE
-set %2=%~f1
-goto :EOF
--- marytts-assembly/assembly-builder/src/builder/bin/transcription.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set BINDIR=%~dp0
-call :RESOLVE "%BINDIR%\.." MARY_BASE
-java -showversion "%*" -cp ".;%MARY_BASE%\lib\*" marytts.tools.transcription.TranscriptionGUI
-goto :EOF
-
-:RESOLVE
-set %2=%~f1
-goto :EOF
