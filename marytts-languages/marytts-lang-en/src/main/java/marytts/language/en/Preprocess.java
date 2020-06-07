092fa81e272768cb48cf6401e13db4df3cb357c1
Wayne Holder <wayne.holder@gmail.com>
2020-06-07 15:05:20 -0700

    Revised and simplified expandDate()

--- marytts-languages/marytts-lang-en/src/main/java/marytts/language/en/Preprocess.java
+++ marytts-languages/marytts-lang-en/src/main/java/marytts/language/en/Preprocess.java
@@ -582,26 +582,13 @@ public class Preprocess extends InternalModule {
 
     protected String expandDate(String date) throws ParseException {
         // Fix date in case format not completely correct
-        String[] dateParts = date.split("[./]");
-        for (int i = 0; i < dateParts.length - 1; i++) {
-            if (dateParts[i].length() == 1) {
-                dateParts[i] = "0" + dateParts[i];
-            }
-        }
-        String processed_date = String.join("/", dateParts);
-        if (Integer.parseInt(processed_date.substring(0, 2)) > 12) {
-            // UK formatted date
-            Date humanDate = DateFormat.getPatternInstance("dd.MM.yyyy", ULocale.UK).parse(processed_date);
-            dateParts = df.format(humanDate).replaceAll(",", "").split("\\s");
-            dateParts[1] = expandOrdinal(Double.parseDouble(dateParts[1]));
-            dateParts[2] = expandYear(Double.parseDouble(dateParts[2]));
-        } else {
-            // US formatted date
-            Date humanDate = DateFormat.getPatternInstance("MM.dd.yyyy", ULocale.US).parse(processed_date);
-            dateParts = df.format(humanDate).replaceAll(",", "").split("\\s");
-            dateParts[1] = expandOrdinal(Double.parseDouble(dateParts[1]));
-            dateParts[2] = expandYear(Double.parseDouble(dateParts[2]));
-        }
+        date = date.replace('.', '/');
+        String[] dateParts = date.split("/");
+        ULocale locale = Integer.parseInt(dateParts[0]) > 12 ? ULocale.UK : ULocale.US;
+        Date humanDate = DateFormat.getPatternInstance("dd.MM.yyyy", locale).parse(date);
+        dateParts = df.format(humanDate).replaceAll(",", "").split("\\s");
+        dateParts[1] = expandOrdinal(Double.parseDouble(dateParts[1]));
+        dateParts[2] = expandYear(Double.parseDouble(dateParts[2]));
         return Arrays.toString(dateParts).replaceAll("[,\\]\\[]", "");
     }
 
