a606328198eb1eebdb8a2e29cf95b4d4f8af2547
NoodleGodz <baybobi4@gmail.com>
2022-06-01 10:55:03 +0700

    Fix function name

--- marytts-signalproc/src/test/java/marytts/util/math/MathUtilsSumTest.java
+++ marytts-signalproc/src/test/java/marytts/util/math/MathUtilsSumTest.java
@@ -5,27 +5,27 @@ import org.junit.Test;
 
 public class MathUtilsSumTest {
     @Test
-    public void TestSumThreeElements(){
+    public void testSumThreeElements(){
         float[] a = new float[] {1, 0, 3};
         Assert.assertEquals((float)4.0, MathUtils.sum(a),0);
     }
     @Test
-    public void TestSumFiveElements(){
+    public void testSumFiveElements(){
         float[] v = new float[] {1, 0, 3, 5, 6};
         Assert.assertEquals((float)15.0, MathUtils.sum(v),0);
     }
     @Test
-    public void TestSumZeroElements(){    
+    public void testSumZeroElements(){    
         float[] f = new float[] {};
         Assert.assertEquals((float)0, MathUtils.sum(f),0);
     }
     @Test
-    public void TestSumNegativeElements(){   
+    public void testSumNegativeElements(){   
         float[] b = new float[] {-3, -4, -5};
         Assert.assertEquals((float)-12, MathUtils.sum(b),0);
     }
     @Test
-    public void TestRandomArray(){
+    public void testRandomArray(){
         float[] c = new float[] {(float) -3.4, 23,(float) 0.45, -5,(float) 0.33, 0};
         Assert.assertEquals((float)15.38, MathUtils.sum(c),0.001);   
     }
