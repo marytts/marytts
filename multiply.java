1e20bcc154a3e2df5cc02e4a6a1f69dce24b2875
KimThanh-BA10-058 <101244551+KimThanh-BA10-058@users.noreply.github.com>
2022-06-08 11:05:45 +0700

    Update Partitioning Testing

--- multiply.java
+++ multiply.java
@@ -2,10 +2,12 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class multiply {
+	//Test Case 1: Input -> {3, 4}, Output -> 12
 	@Test
 	public void multiplyTest() {
 		Assert.assertEquals(12, MathUtils.multiply(new float [] {3, 4}, 0));
 	}
+	//Test Case 2: Input -> {0, 9} or {9, 0}, Output -> 0
 	@Test
 	public void MuliplyTestwithZero() {
 		Assert.assertEquals(0, MathUtils.multiply(new float [] {0, 9}, 0));
